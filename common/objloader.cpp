fa1f2077f1f934bf28d034d5d5c206a11b31745f
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-02-14 18:17:29 +0200

    Lisätty kommentteja ja siivottu koodia.
    Ei toiminnallisia muutoksia.
    
    	modified:   common/objloader.cpp

--- common/objloader.cpp
+++ common/objloader.cpp
@@ -19,7 +19,7 @@
 // - More secure. Change another line and you can inject code.
 // - Loading from memory, stream, etc
 
-bool loadOBJ(const char * path,
+bool loadOBJ(const char *path,
         std::vector<glm::vec3> & out_vertices,
         std::vector<glm::vec2> & out_uvs,
         std::vector<glm::vec3> & out_normals)
@@ -31,7 +31,7 @@ bool loadOBJ(const char * path,
     std::vector<glm::vec2> temp_uvs;
     std::vector<glm::vec3> temp_normals;
 
-    FILE * file = fopen(path, "r");
+    FILE *file = fopen(path, "r");
     if (file == NULL)
     {
         printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
@@ -53,12 +53,18 @@ bool loadOBJ(const char * path,
 
         if (strcmp(lineHeader, "v") == 0)
         {
+            // This line specifies a vertex.
+            // Example:
+            // v 1.000000 -1.000000 -1.000000
             glm::vec3 vertex;
             fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
             temp_vertices.push_back(vertex);
         }
         else if (strcmp(lineHeader, "vt") == 0)
         {
+            // This line specifies texture coordinate of one vertex.
+            // Example:
+            // vt 0.748573 0.750412
             glm::vec2 uv;
             fscanf(file, "%f %f\n", &uv.x, &uv.y);
             uv.y = -uv.y; // Invert V coordinate since we will only use DDS texture, which are inverted. Remove if you want to use TGA or BMP loaders.
@@ -66,15 +72,32 @@ bool loadOBJ(const char * path,
         }
         else if (strcmp(lineHeader, "vn") == 0)
         {
+            // This line specifies the normal of one vertex.
+            // Example:
+            // vn 0.000000 0.000000 -1.000000
             glm::vec3 normal;
             fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
             temp_normals.push_back(normal);
         }
         else if (strcmp(lineHeader, "f") == 0)
         {
+            // This line specifies a face.
+            // Example:
+            // f 5/1/1 1/2/1 4/3/1
             std::string vertex1, vertex2, vertex3;
             unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+            int matches = fscanf(
+                    file,
+                    "%d/%d/%d %d/%d/%d %d/%d/%d\n",
+                    &vertexIndex[0],
+                    &uvIndex[0],
+                    &normalIndex[0],
+                    &vertexIndex[1],
+                    &uvIndex[1],
+                    &normalIndex[1],
+                    &vertexIndex[2],
+                    &uvIndex[2],
+                    &normalIndex[2]);
             if (matches != 9)
             {
                 printf("File can't be read by our simple parser :-( Try exporting with other options\n");
@@ -96,22 +119,20 @@ bool loadOBJ(const char * path,
             char stupidBuffer[1000];
             fgets(stupidBuffer, 1000, file);
         }
-
     }
 
     // For each vertex of each triangle
-    for (unsigned int i=0; i<vertexIndices.size(); i++)
+    for (unsigned int i = 0; i < vertexIndices.size(); i++)
     {
-
         // Get the indices of its attributes
         unsigned int vertexIndex = vertexIndices[i];
         unsigned int uvIndex = uvIndices[i];
         unsigned int normalIndex = normalIndices[i];
 
         // Get the attributes thanks to the index
-        glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
-        glm::vec2 uv = temp_uvs[ uvIndex-1 ];
-        glm::vec3 normal = temp_normals[ normalIndex-1 ];
+        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
+        glm::vec2 uv = temp_uvs[uvIndex - 1];
+        glm::vec3 normal = temp_normals[normalIndex - 1];
 
         // Put the attributes in buffers
         out_vertices.push_back(vertex);
@@ -121,7 +142,6 @@ bool loadOBJ(const char * path,
     return true;
 }
 
-
 #ifdef USE_ASSIMP // don't use this #define, it's only for me (it AssImp fails to compile on your machine, at least all the other tutorials still work)
 
 // Include AssImp
@@ -130,7 +150,7 @@ bool loadOBJ(const char * path,
 #include <assimp/postprocess.h>     // Post processing flags
 
 bool loadAssImp(
-        const char * path,
+        const char *path,
         std::vector<unsigned short> & indices,
         std::vector<glm::vec3> & vertices,
         std::vector<glm::vec2> & uvs,
@@ -150,7 +170,7 @@ bool loadAssImp(
 
     // Fill vertices positions
     vertices.reserve(mesh->mNumVertices);
-    for(unsigned int i=0; i<mesh->mNumVertices; i++)
+    for(unsigned int i = 0; i < mesh->mNumVertices; i++)
     {
         aiVector3D pos = mesh->mVertices[i];
         vertices.push_back(glm::vec3(pos.x, pos.y, pos.z));
@@ -166,15 +186,15 @@ bool loadAssImp(
 
     // Fill vertices normals
     normals.reserve(mesh->mNumVertices);
-    for (unsigned int i=0; i<mesh->mNumVertices; i++)
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++)
     {
         aiVector3D n = mesh->mNormals[i];
         normals.push_back(glm::vec3(n.x, n.y, n.z));
     }
 
     // Fill face indices
-    indices.reserve(3*mesh->mNumFaces);
-    for (unsigned int i=0; i<mesh->mNumFaces; i++)
+    indices.reserve(3 * mesh->mNumFaces);
+    for (unsigned int i = 0; i < mesh->mNumFaces; i++)
     {
         // Assume the model has only triangles.
         indices.push_back(mesh->mFaces[i].mIndices[0]);
