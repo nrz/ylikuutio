3bcfbcbed931933c4aeb5637c554832840192302
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-02-17 18:52:55 +0200

    Nopeus pudotettu 10 yksikköön.
    	modified:   common/controls.cpp

--- common/controls.cpp
+++ common/controls.cpp
@@ -33,7 +33,7 @@ float verticalAngle = 0.0f;
 // Initial Field of View
 float initialFoV = 45.0f;
 
-float speed = 20.0f; // 3 units / second
+float speed = 10.0f; // 10 units / second
 float mouseSpeed = 0.005f;
 
 void computeMatricesFromInputs()
