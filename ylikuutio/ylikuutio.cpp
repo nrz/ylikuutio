3e0ae30ea012db3777cc55928d666df688f77817
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-07-04 16:34:50 +0300

    Muokattu `SRTM_terrain_species_struct.model_filename`.
    `SRTM_terrain_species_struct.model_filename` sisältää ainoastaan
    hakemistopolun, ei SRTM-tiedoston nimeä (tiedoston nimi muodostetaan
    koordinaattien perusteella).
    
    	modified:   ylikuutio/ylikuutio.cpp

--- ylikuutio/ylikuutio.cpp
+++ ylikuutio/ylikuutio.cpp
@@ -168,7 +168,7 @@ int main(void)
     SpeciesStruct SRTM_terrain_species_struct;
     SRTM_terrain_species_struct.shader_pointer = my_shader;
     SRTM_terrain_species_struct.model_file_format = "SRTM";
-    SRTM_terrain_species_struct.model_filename = "/media/laatikko_4TB/satelliittikuvat/srtm/version3/data/S17W069.hgt";
+    SRTM_terrain_species_struct.model_filename = "/media/laatikko_4TB/satelliittikuvat/srtm/version3/data/";
     SRTM_terrain_species_struct.texture_file_format = g_texture_file_format;
     SRTM_terrain_species_struct.color_channel = g_height_data_color_channel;
     SRTM_terrain_species_struct.texture_filename = g_texture_filename;
