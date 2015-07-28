0e16e0a00d3a58a8a8c75a26da1bc612c9d118c1
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-07-28 13:14:07 +0300

    Paikannettu bugi: `this->free_shaderID_queue.empty()` aiheuttaa segmentointivirheen (Segmentation fault), jos kääntäjänä on Clang. Virhe ilmenee ainakin Clangin versiolla 3.7.0.
    	modified:   common/model.cpp

--- common/model.cpp
+++ common/model.cpp
@@ -236,6 +236,8 @@ namespace model
     {
         GLuint shaderID;
 
+        // FIXME: `this->free_shaderID_queue.empty()` causes segmentation fault in Clang version 3.7.0!
+
         while (!this->free_shaderID_queue.empty())
         {
             // return the first (oldest) free shaderID.
