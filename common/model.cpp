8ae31522504e6b36205f31ad252fb176d9458983
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-07-30 12:13:46 +0300

    znear:ia muutettu suuremmaksi, koska float-tarkkuuden raja tuli vastaan.
    	modified:   common/model.cpp

--- common/model.cpp
+++ common/model.cpp
@@ -545,7 +545,7 @@ namespace model
         camera_position.y += 2.0f;
 
         // Projection matrix : 45° Field of View, aspect ratio, display range : 0.1 unit <-> 100 units
-        ProjectionMatrix = glm::perspective(FoV, ASPECT_RATIO, 0.0001f, 5000.0f + 2.0f * (GLfloat) earth_radius);
+        ProjectionMatrix = glm::perspective(FoV, ASPECT_RATIO, 0.001f, 5000.0f + 2.0f * (GLfloat) earth_radius);
         // Camera matrix
         ViewMatrix = glm::lookAt(
                 camera_position,           // Camera is here
