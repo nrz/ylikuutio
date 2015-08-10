382ddd0ef8400013272f0273452ae29c5698db43
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-08-10 08:38:24 +0300

    Poistettu tarpeeton `template<class T1> friend void render_this_object`.
    	modified:   cpp/common/texture.hpp

--- cpp/common/texture.hpp
+++ cpp/common/texture.hpp
@@ -37,8 +37,6 @@ namespace model
                 friend void bind_child_to_parent(T1 child_pointer, std::vector<void*> &child_pointer_vector, std::queue<GLuint> &free_childID_queue);
             template<class T1, class T2>
                 friend void bind_child_to_new_parent(T1 child_pointer, T2 new_parent_pointer, std::vector<void*> &old_child_pointer_vector, std::queue<GLuint> &old_free_childID_queue);
-            template<class T1>
-               friend void render_this_object(model::Object* object_pointer, model::Shader* shader_pointer);
 
         private:
             // this method renders all species using this texture.
