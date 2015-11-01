81e0ab53b1ad2e9952166e118a6106de1d801ea0
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-11-01 11:52:38 +0200

    `void printText2D`: Refaktorointia.
    	modified:   cpp/common/text2D.cpp

--- cpp/common/text2D.cpp
+++ cpp/common/text2D.cpp
@@ -183,24 +183,9 @@ namespace text2D
                 }
             }
 
-            if (strcmp(horizontal_alignment, "left") == 0)
-            {
-                vertex_up_left_x = vertex_down_left_x = current_left_x;
-                vertex_up_right_x = vertex_down_right_x = current_left_x + text_size;
-                current_left_x += text_size;
-            }
-            else if (strcmp(horizontal_alignment, "right") == 0)
-            {
-                vertex_up_left_x = vertex_down_left_x = current_left_x;
-                vertex_up_right_x = vertex_down_right_x = current_left_x + text_size;
-                current_left_x += text_size;
-            }
-            else if (strcmp(horizontal_alignment, "center") == 0)
-            {
-                vertex_up_left_x = vertex_down_left_x = current_left_x;
-                vertex_up_right_x = vertex_down_right_x = current_left_x + text_size;
-                current_left_x += text_size;
-            }
+            vertex_up_left_x = vertex_down_left_x = current_left_x;
+            vertex_up_right_x = vertex_down_right_x = current_left_x + text_size;
+            current_left_x += text_size;
 
             if (strcmp(vertical_alignment, "bottom") == 0)
             {
