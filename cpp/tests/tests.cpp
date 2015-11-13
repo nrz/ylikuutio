c309d8a35db866dbd686d992ef82f7a648ea876d
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-11-13 19:04:48 +0200

    Testataan kaikki `globals.hpp`:n makrojen toimintaa.
    Poistettu tarpeeton `unsigned int factorial(unsigned int number)` ja
    siihen liittyvä testi (molemmat ovat Catchin tutoriaalista).
    
    Catch ei ilmeisesti toimi Clangin kanssa: kääntäessä tulee seuraavat
    virheet:
    
        Scanning dependencies of target do_tests
        [ 35%] Building CXX object CMakeFiles/do_tests.dir/cpp/tests/tests.cpp.o
        In file included from /home/antti/koodi/ylikuutio/cpp/tests/tests.cpp:7:
        /home/antti/koodi/ylikuutio/external/Catch/single_include/catch.hpp:1145:43: error:
              no type named 'underlying_type' in namespace 'std'
                        static_cast<typename std::underlying_type<T>::type>(v)
                                    ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~
        /home/antti/koodi/ylikuutio/external/Catch/single_include/catch.hpp:1145:37: error:
              expected '>'
                        static_cast<typename std::underlying_type<T>::type>(v)
                                            ^
                                            >
        /home/antti/koodi/ylikuutio/external/Catch/single_include/catch.hpp:1145:28: note:
              to match this '<'
                        static_cast<typename std::underlying_type<T>::type>(v)
                                   ^
        /home/antti/koodi/ylikuutio/external/Catch/single_include/catch.hpp:1145:59: error:
              'T' does not refer to a value
                        static_cast<typename std::underlying_type<T>::type>(v)
                                                                  ^
        /home/antti/koodi/ylikuutio/external/Catch/single_include/catch.hpp:1139:23: note:
              declared here
            template<typename T>
                              ^
        /home/antti/koodi/ylikuutio/external/Catch/single_include/catch.hpp:1145:63: error:
              no member named 'type' in the global namespace
                        static_cast<typename std::underlying_type<T>::type>(v)
                                                                    ~~^
        4 errors generated.
        make[2]: *** [CMakeFiles/do_tests.dir/cpp/tests/tests.cpp.o] Error 1
        make[1]: *** [CMakeFiles/do_tests.dir/all] Error 2
        make: *** [all] Error 2
    
    GCC:llä kaikki testit menevät läpi. Jatkossa testit käännetään vain
    GCC:llä.
    
    	modified:   cpp/tests/tests.cpp

--- cpp/tests/tests.cpp
+++ cpp/tests/tests.cpp
@@ -1,16 +1,73 @@
+#ifndef CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
+#endif
 
-unsigned int factorial(unsigned int number)
+#ifndef __CATCH_HPP_INCLUDED
+#define __CATCH_HPP_INCLUDED
+#include "catch.hpp"
+#endif
+
+#include "cpp/common/globals.hpp"
+#include "cpp/common/shader_loader.hpp"
+#include "cpp/common/texture_loader.hpp"
+#include "cpp/common/vboindexer.hpp"
+#include "cpp/common/text2D.hpp"
+#include "cpp/common/text3D.hpp"
+#include "cpp/common/world.hpp"
+#include "cpp/common/shader.hpp"
+#include "cpp/common/material.hpp"
+#include "cpp/common/font.hpp"
+#include "cpp/common/glyph.hpp"
+#include "cpp/common/species.hpp"
+#include "cpp/common/object.hpp"
+
+#include <cmath>
+
+TEST_CASE("parent pointer of OBJECTSTRUCT must be null", "[OBJECTSTRUCT]")
 {
-    return number <= 1 ? number : factorial(number - 1) * number;
+    SHADERSTRUCT(test_shader_struct);
+    REQUIRE(test_shader_struct.parent_pointer == NULL);
 }
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("parent pointer of MATERIALSTRUCT must be null", "[MATERIALSTRUCT]")
 {
-    REQUIRE(factorial(0) == 1);
-    REQUIRE(factorial(1) == 1);
-    REQUIRE(factorial(2) == 2);
-    REQUIRE(factorial(3) == 6);
-    REQUIRE(factorial(10) == 3628800);
+    MATERIALSTRUCT(test_material_struct);
+    REQUIRE(test_material_struct.parent_pointer == NULL);
+}
+
+TEST_CASE("parent pointer of NODESTRUCT must be null", "[NODESTRUCT]")
+{
+    NODESTRUCT(test_node_struct);
+    REQUIRE(test_node_struct.parent_pointer == NULL);
+}
+
+TEST_CASE("OBJECTSTRUCT must be initialized appropriately", "[OBJECTSTRUCT]")
+{
+    OBJECTSTRUCT(test_object_struct);
+    REQUIRE(test_object_struct.species_parent_pointer == NULL);
+    REQUIRE(test_object_struct.glyph_parent_pointer == NULL);
+    REQUIRE(test_object_struct.original_scale_vector == glm::vec3(1.0f, 1.0f, 1.0f));
+    REQUIRE(std::isnan(test_object_struct.rotate_angle));
+    REQUIRE(test_object_struct.is_character == false);
+}
+
+TEST_CASE("SPECIESSTRUCT must be initialized appropriately", "[SPECIESSTRUCT]")
+{
+    SPECIESSTRUCT(test_species_struct);
+    REQUIRE(test_species_struct.parent_pointer == NULL);
+    REQUIRE(test_species_struct.is_world == false);
+    REQUIRE(std::isnan(test_species_struct.world_radius));
+}
+
+TEST_CASE("FONTSTRUCT must be initialized appropriately", "[FONTSTRUCT]")
+{
+    FONTSTRUCT(test_font_struct);
+    REQUIRE(test_font_struct.parent_pointer == NULL);
+    REQUIRE(test_font_struct.vertex_scaling_factor == DEFAULT_VERTEX_SCALING_FACTOR);
+}
+
+TEST_CASE("parent pointer of GLYPHSTRUCT must be null", "[GLYPHSTRUCT]")
+{
+    GLYPHSTRUCT(test_glyph_struct);
+    REQUIRE(test_glyph_struct.parent_pointer == NULL);
 }
