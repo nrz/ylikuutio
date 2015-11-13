81d2af75241b34461d816bf8a9e31a9514b627c0
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-11-13 10:58:36 +0200

    Esimerkkitestikoodin muotoilua.
    Ei toiminnallisia muutoksia.
    
    Kaikki testit menevät läpi.
    
    	modified:   cpp/tests/tests.cpp

--- cpp/tests/tests.cpp
+++ cpp/tests/tests.cpp
@@ -1,12 +1,13 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-unsigned int factorial( unsigned int number )
+unsigned int factorial(unsigned int number)
 {
     return number <= 1 ? number : factorial(number - 1) * number;
 }
 
-TEST_CASE("Factorials are computed", "[factorial]") {
+TEST_CASE("Factorials are computed", "[factorial]")
+{
     REQUIRE(factorial(1) == 1);
     REQUIRE(factorial(2) == 2);
     REQUIRE(factorial(3) == 6);
