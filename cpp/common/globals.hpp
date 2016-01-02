ba2adc21b1a0b3dc1a7c1c8b6c60f55b361562da
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-01-02 10:13:04 +0200

    Muokattu `typedef struct SphericalWorldStruct`.
    Muokattu `typedef struct SphericalWorldStruct`: lisätty muuttujat
    `double SRTM_latitude_step_in_degrees` &
    `double SRTM_longitude_step_in_degrees` ja luotu oletuskonstruktori,
    joka asettaa em. muuttujien oletusarvoksi 1.0f/1200.0f.
    
    	modified:   cpp/common/globals.hpp

--- cpp/common/globals.hpp
+++ cpp/common/globals.hpp
@@ -239,12 +239,19 @@ typedef struct
     double phi;
 } SphericalCoordinatesStruct;
 
-typedef struct
+typedef struct SphericalWorldStruct
 {
+    SphericalWorldStruct()
+        :SRTM_latitude_step_in_degrees(1.0f/1200.0f), SRTM_longitude_step_in_degrees(1.0f/1200.0f)
+    {
+        // constructor.
+    }
     double southern_latitude;
     double northern_latitude;
     double western_longitude;
     double eastern_longitude;
+    double SRTM_latitude_step_in_degrees;
+    double SRTM_longitude_step_in_degrees;
 } SphericalWorldStruct;
 
 typedef struct TriangulateQuadsStruct
