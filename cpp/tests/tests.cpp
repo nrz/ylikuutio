3534f5ed2e0f5d91e0ea23a48d1c58302ac89cea
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-01-03 19:42:12 +0200

    Muokattu ja täydennetty `"[triangulate_2x2_world_bilinear]"`.
    Kolmiointi näyttäisi toimivan oikein.
    
    Kaikki testit menevät läpi.
    
    	modified:   cpp/tests/tests.cpp

--- cpp/tests/tests.cpp
+++ cpp/tests/tests.cpp
@@ -118,19 +118,21 @@ TEST_CASE("2x2 world must be triangulated appropriately using bilinear interpola
     vertex_data = new GLuint [world_size];
     GLuint* vertex_pointer = vertex_data;
     // x, z: height (y).
-#define SOUTHWEST_HEIGHT 1
-#define SOUTHEAST_HEIGHT 2
-#define NORTHWEST_HEIGHT 4
-#define NORTHEAST_HEIGHT 8
+    GLuint southwest_height = 1;
+    GLuint southeast_height = 2;
+    GLuint northwest_height = 4;
+    GLuint northeast_height = 8;
+    GLfloat center_x = 0.5f;
+    GLfloat center_z = 0.5f;
 
     // 0, 0: 1.
-    *vertex_data++ = SOUTHWEST_HEIGHT;
+    *vertex_pointer++ = southwest_height;
     // 1, 0: 2.
-    *vertex_data++ = SOUTHEAST_HEIGHT;
+    *vertex_pointer++ = southeast_height;
     // 0, 1: 4.
-    *vertex_data++ = NORTHWEST_HEIGHT;
+    *vertex_pointer++ = northwest_height;
     // 1, 1: 8.
-    *vertex_data++ = NORTHEAST_HEIGHT;
+    *vertex_pointer++ = northeast_height;
 
     std::vector<glm::vec3> vertices; // vertices of the object.
     std::vector<glm::vec2> UVs;      // UVs of the object.
@@ -149,6 +151,79 @@ TEST_CASE("2x2 world must be triangulated appropriately using bilinear interpola
     REQUIRE(vertices.size() == 12);
     REQUIRE(UVs.size() == 12);
     REQUIRE(normals.size() == 12);
+
+    std::cout << " 0: " << vertices[0].x << ", " << vertices[0].y << ", " << vertices[0].z << "\n";
+    std::cout << " 1: " << vertices[1].x << ", " << vertices[1].y << ", " << vertices[1].z << "\n";
+    std::cout << " 2: " << vertices[2].x << ", " << vertices[2].y << ", " << vertices[2].z << "\n";
+    std::cout << " 3: " << vertices[3].x << ", " << vertices[3].y << ", " << vertices[3].z << "\n";
+    std::cout << " 4: " << vertices[4].x << ", " << vertices[4].y << ", " << vertices[4].z << "\n";
+    std::cout << " 5: " << vertices[5].x << ", " << vertices[5].y << ", " << vertices[5].z << "\n";
+    std::cout << " 6: " << vertices[6].x << ", " << vertices[6].y << ", " << vertices[6].z << "\n";
+    std::cout << " 7: " << vertices[7].x << ", " << vertices[7].y << ", " << vertices[7].z << "\n";
+    std::cout << " 8: " << vertices[8].x << ", " << vertices[8].y << ", " << vertices[8].z << "\n";
+    std::cout << " 9: " << vertices[9].x << ", " << vertices[9].y << ", " << vertices[9].z << "\n";
+    std::cout << "10: " << vertices[10].x << ", " << vertices[10].y << ", " << vertices[10].z << "\n";
+    std::cout << "11: " << vertices[11].x << ", " << vertices[11].y << ", " << vertices[11].z << "\n";
+
+    // 1st vertex is the center vertex of the 1st triangle.
+    REQUIRE(vertices[0].x == center_x);
+    REQUIRE(vertices[0].z == center_z);
+    REQUIRE(vertices[0].y == (GLfloat) (southwest_height + southeast_height + northwest_height + northeast_height) / 4);
+
+    // 2nd vertex is the southwest vertex of the 1st triangle.
+    REQUIRE(vertices[1].x == 1.0f);
+    REQUIRE(vertices[1].z == 0.0f);
+    REQUIRE(vertices[1].y == southeast_height);
+
+    // 3rd vertex is the northwest vertex of the 1st triangle.
+    REQUIRE(vertices[2].x == 0.0f);
+    REQUIRE(vertices[2].z == 0.0f);
+    REQUIRE(vertices[2].y == southwest_height);
+
+    // 4th vertex is the center vertex of the 2nd triangle.
+    REQUIRE(vertices[3].x == center_x);
+    REQUIRE(vertices[3].z == center_z);
+    REQUIRE(vertices[3].y == (GLfloat) (southwest_height + southeast_height + northwest_height + northeast_height) / 4);
+
+    // 5th vertex is the southwest vertex of the 2nd triangle.
+    REQUIRE(vertices[4].x == 0.0f);
+    REQUIRE(vertices[4].z == 0.0f);
+    REQUIRE(vertices[4].y == southwest_height);
+
+    // 6th vertex is the northwest vertex of the 2nd triangle.
+    REQUIRE(vertices[5].x == 0.0f);
+    REQUIRE(vertices[5].z == 1.0f);
+    REQUIRE(vertices[5].y == northwest_height);
+
+    // 7th vertex is the center vertex of the 3rd triangle.
+    REQUIRE(vertices[6].x == center_x);
+    REQUIRE(vertices[6].z == center_z);
+    REQUIRE(vertices[6].y == (GLfloat) (southwest_height + southeast_height + northwest_height + northeast_height) / 4);
+
+    // 8th vertex is the northwest vertex of the 3rd triangle.
+    REQUIRE(vertices[7].x == 0.0f);
+    REQUIRE(vertices[7].z == 1.0f);
+    REQUIRE(vertices[7].y == northwest_height);
+
+    // 9th vertex is the northeast vertex of the 3rd triangle.
+    REQUIRE(vertices[8].x == 1.0f);
+    REQUIRE(vertices[8].z == 1.0f);
+    REQUIRE(vertices[8].y == northeast_height);
+
+    // 10th vertex is the center vertex of the 4th triangle.
+    REQUIRE(vertices[9].x == center_x);
+    REQUIRE(vertices[9].z == center_z);
+    REQUIRE(vertices[9].y == (GLfloat) (southwest_height + southeast_height + northwest_height + northeast_height) / 4);
+
+    // 11th vertex is the northeast vertex of the 4th triangle.
+    REQUIRE(vertices[10].x == 1.0f);
+    REQUIRE(vertices[10].z == 1.0f);
+    REQUIRE(vertices[10].y == northeast_height);
+
+    // 12th vertex is the southeast vertex of the 4th triangle.
+    REQUIRE(vertices[11].x == 1.0f);
+    REQUIRE(vertices[11].z == 0.0f);
+    REQUIRE(vertices[11].y == southeast_height);
 }
 
 TEST_CASE("3x3 BMP world must be loaded appropriately", "[load_3x3_BMP_world]")
