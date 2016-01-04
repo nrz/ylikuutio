05176c98d30c7fe01d8aee4ae9a4735aad2d0fd8
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-01-04 12:31:12 +0200

    Refaktorointia: `interpolate_vertices_using_bilinear_interpolation`.
    Refaktorointia: siirretty koodia funktiosta `bool triangulate_quads`
    uuteen funktioon
    `void interpolate_vertices_using_bilinear_interpolation`.
    
    Ei toiminnallisia muutoksia.
    
    	modified:   cpp/common/triangulation.cpp

--- cpp/common/triangulation.cpp
+++ cpp/common/triangulation.cpp
@@ -153,6 +153,59 @@ namespace geometry
         return *vertex_pointer;
     }
 
+    void interpolate_vertices_using_bilinear_interpolation(
+            BilinearInterpolationStruct bilinear_interpolation_struct,
+            std::vector<glm::vec3> &temp_vertices,
+            std::vector<glm::vec2> &temp_UVs)
+    {
+        GLuint* input_vertex_pointer = bilinear_interpolation_struct.input_vertex_pointer;
+        uint32_t image_width = bilinear_interpolation_struct.image_width;
+        uint32_t image_height = bilinear_interpolation_struct.image_height;
+
+        std::cout << "interpolating center vertices.\n";
+
+        // Then, define the faces in a double loop.
+        // Begin from index 1.
+        for (uint32_t z = 1; z < image_height; z++)
+        {
+            // Begin from index 1.
+            for (uint32_t x = 1; x < image_width; x++)
+            {
+                // This corresponds to "f": specify a face (but here we specify 2 faces instead!).
+                // std::cout << "Processing coordinate (" << x << ", " << z << ").\n";
+
+                uint32_t current_vertex_i = image_width * z + x;
+
+                // Interpolate y coordinate (altitude).
+                GLfloat y = ((GLfloat) SOUTHWEST_Y + SOUTHEAST_Y + NORTHWEST_Y + NORTHEAST_Y) / 4;
+
+                // Create a new vertex using bilinear interpolation.
+                // This corresponds to "v": specify one vertex.
+                glm::vec3 vertex;
+                vertex.x = (GLfloat) x - 0.5f;
+                vertex.y = y;
+                vertex.z = (GLfloat) z - 0.5f;
+                temp_vertices.push_back(vertex);
+
+                // This corresponds to "vt": specify texture coordinates of one vertex.
+                glm::vec2 uv;
+
+                if (bilinear_interpolation_struct.should_ylikuutio_use_real_texture_coordinates)
+                {
+                    uv.x = 0.5f;
+                    uv.y = 0.5f;
+                }
+                else
+                {
+                    uv.x = y / 256;
+                    uv.y = 0.0f;
+                }
+
+                temp_UVs.push_back(uv);
+            }
+        }
+    }
+
     glm::vec3 transform_planar_world_vertex_into_cartesian_vertex(
             glm::vec3 planar_world_vertex,
             double sphere_radius)
@@ -325,48 +378,15 @@ namespace geometry
         // 2. Interpolate the vertices between, using bilinear interpolation, `push_back` to `temp_vertices`.
         if (is_bilinear_interpolation_in_use)
         {
-            std::cout << "interpolating center vertices.\n";
-
-            // Then, define the faces in a double loop.
-            // Begin from index 1.
-            for (uint32_t z = 1; z < image_height; z++)
-            {
-                // Begin from index 1.
-                for (uint32_t x = 1; x < image_width; x++)
-                {
-                    // This corresponds to "f": specify a face (but here we specify 2 faces instead!).
-                    // std::cout << "Processing coordinate (" << x << ", " << z << ").\n";
-
-                    uint32_t current_vertex_i = image_width * z + x;
-
-                    // Interpolate y coordinate (altitude).
-                    GLfloat y = ((GLfloat) SOUTHWEST_Y + SOUTHEAST_Y + NORTHWEST_Y + NORTHEAST_Y) / 4;
-
-                    // Create a new vertex using bilinear interpolation.
-                    // This corresponds to "v": specify one vertex.
-                    glm::vec3 vertex;
-                    vertex.x = (GLfloat) x - 0.5f;
-                    vertex.y = y;
-                    vertex.z = (GLfloat) z - 0.5f;
-                    temp_vertices.push_back(vertex);
-
-                    // This corresponds to "vt": specify texture coordinates of one vertex.
-                    glm::vec2 uv;
-
-                    if (triangulate_quads_struct.should_ylikuutio_use_real_texture_coordinates)
-                    {
-                        uv.x = 0.5f;
-                        uv.y = 0.5f;
-                    }
-                    else
-                    {
-                        uv.x = y / 256;
-                        uv.y = 0.0f;
-                    }
-
-                    temp_UVs.push_back(uv);
-                }
-            }
+            BilinearInterpolationStruct bilinear_interpolation_struct;
+            bilinear_interpolation_struct.input_vertex_pointer = input_vertex_pointer;
+            bilinear_interpolation_struct.image_width = image_width;
+            bilinear_interpolation_struct.image_height = image_height;
+            bilinear_interpolation_struct.should_ylikuutio_use_real_texture_coordinates = triangulate_quads_struct.should_ylikuutio_use_real_texture_coordinates;
+            geometry::interpolate_vertices_using_bilinear_interpolation(
+                    bilinear_interpolation_struct,
+                    temp_vertices,
+                    temp_UVs);
         }
         else if (is_southwest_northeast_edges_in_use || is_southeast_northwest_edges_in_use)
         {
