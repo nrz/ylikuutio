ff269c247b728a2be76d84e89d539e067f42a6e6
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-01-11 10:25:07 +0200

    Lisätty `typedef`-määrityksiä callback:ejä varten.
    	modified:   cpp/common/globals.hpp

--- cpp/common/globals.hpp
+++ cpp/common/globals.hpp
@@ -287,6 +287,24 @@ typedef struct
     SphericalWorldStruct spherical_world_struct;
 } TransformationStruct;
 
+// callback typedefs in alphabetical order.
+typedef bool (*BoolToBoolCallback)(bool);
+typedef float (*BoolToFloatCallback)(bool);
+typedef int (*BoolToIntCallback)(bool);
+typedef void (*BoolToVoidCallback)(bool);
+typedef bool (*FloatToBoolCallback)(float);
+typedef float (*FloatToFloatCallback)(float);
+typedef int (*FloatToIntCallback)(float);
+typedef void (*FloatToVoidCallback)(float);
+typedef bool (*IntToBoolCallback)(int);
+typedef float (*IntToFloatCallback)(int);
+typedef int (*IntToIntCallback)(int);
+typedef void (*IntToVoidCallback)(int);
+typedef bool (*VoidToBoolCallback)(void);
+typedef float (*VoidToFloatCallback)(void);
+typedef int (*VoidToIntCallback)(void);
+typedef void (*VoidToVoidCallback)(void);
+
 extern SphericalCoordinatesStruct spherical_position;
 
 #endif
