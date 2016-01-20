33f09aac9351c73d6b663f54b832aad8f89d7423
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-01-21 01:18:16 +0200

    `GLuint` -> `uint32_t`.
    `GLuint` & `GLfloat` ym. ovat aiheellisia vain näytönohjaimelle
    välitettävässä datassa, ei muissa muuttujissa.
    
    Muokattu myös `#include`-määrityksiä.
    
    	modified:   cpp/common/model_common_functions.cpp
    	modified:   cpp/common/model_common_functions.hpp

--- cpp/common/model_common_functions.cpp
+++ cpp/common/model_common_functions.cpp
@@ -1,31 +1,15 @@
 #include "model_common_functions.hpp"
 #include "species.hpp"
 
-// Include GLEW
-#ifndef __GL_GLEW_H_INCLUDED
-#define __GL_GLEW_H_INCLUDED
-#include <GL/glew.h> // GLfloat, GLuint etc.
-#endif
-
-// Include GLFW
-#ifndef __GLFW3_H_INCLUDED
-#define __GLFW3_H_INCLUDED
-#include <glfw3.h>
-#endif
-
-// Include GLM
-#ifndef __GLM_GLM_HPP_INCLUDED
-#define __GLM_GLM_HPP_INCLUDED
-#include <glm/glm.hpp> // glm
-#endif
-
 // Include standard headers
 #include <cmath>     // NAN
+#include <queue>     // std::queue
+#include <stdint.h>  // uint32_t etc.
 #include <vector>    // std::vector
 
 namespace model
 {
-    void set_child_pointer(GLuint childID, void* child_pointer, std::vector<void*> &child_pointer_vector, std::queue<GLuint> &free_childID_queue)
+    void set_child_pointer(uint32_t childID, void* child_pointer, std::vector<void*> &child_pointer_vector, std::queue<uint32_t> &free_childID_queue)
     {
         child_pointer_vector[childID] = child_pointer;
 
@@ -44,9 +28,9 @@ namespace model
         }
     }
 
-    GLuint get_childID(std::vector<void*> &child_pointer_vector, std::queue<GLuint> &free_childID_queue)
+    uint32_t get_childID(std::vector<void*> &child_pointer_vector, std::queue<uint32_t> &free_childID_queue)
     {
-        GLuint childID;
+        uint32_t childID;
 
         while (!free_childID_queue.empty())
         {
--- cpp/common/model_common_functions.hpp
+++ cpp/common/model_common_functions.hpp
@@ -1,32 +1,15 @@
 #ifndef __MODEL_COMMON_FUNCTIONS_HPP_INCLUDED
 #define __MODEL_COMMON_FUNCTIONS_HPP_INCLUDED
 
-// Include GLEW
-#ifndef __GL_GLEW_H_INCLUDED
-#define __GL_GLEW_H_INCLUDED
-#include <GL/glew.h> // GLfloat, GLuint etc.
-#endif
-
-// Include GLFW
-#ifndef __GLFW3_H_INCLUDED
-#define __GLFW3_H_INCLUDED
-#include <glfw3.h>
-#endif
-
-// Include GLM
-#ifndef __GLM_GLM_HPP_INCLUDED
-#define __GLM_GLM_HPP_INCLUDED
-#include <glm/glm.hpp> // glm
-#endif
-
 // Include standard headers
+#include <stdint.h>  // uint32_t etc.
 #include <vector>    // std::vector
 #include <queue>     // std::queue
 
 namespace model
 {
-    void set_child_pointer(GLuint childID, void* child_pointer, std::vector<void*> &child_pointer_vector, std::queue<GLuint> &free_childID_queue);
-    GLuint get_childID(std::vector<void*> &child_pointer_vector, std::queue<GLuint> &free_childID_queue);
+    void set_child_pointer(uint32_t childID, void* child_pointer, std::vector<void*> &child_pointer_vector, std::queue<uint32_t> &free_childID_queue);
+    uint32_t get_childID(std::vector<void*> &child_pointer_vector, std::queue<uint32_t> &free_childID_queue);
 }
 
 #endif
