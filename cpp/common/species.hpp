d1ea81d7808da41b97b0ec16186465923375bc15
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-01-20 23:54:54 +0200

    Lisätty puuttuva `#include <glm/glm.hpp>` jne.
    	modified:   cpp/common/species.hpp

--- cpp/common/species.hpp
+++ cpp/common/species.hpp
@@ -12,6 +12,12 @@
 #include <GL/glew.h> // GLfloat, GLuint etc.
 #endif
 
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
 // Include standard headers
 #include <string>    // std::string
 #include <vector>    // std::vector
