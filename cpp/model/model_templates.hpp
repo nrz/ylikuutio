547bb0dc38e7755898cffb61cf5af39d118d3708
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-04 13:27:40 +0200

    Poistettu ylimääräisiä `#include`-määrityksiä.
    	modified:   cpp/model/model_templates.hpp

--- cpp/model/model_templates.hpp
+++ cpp/model/model_templates.hpp
@@ -7,33 +7,6 @@
 #include <GL/glew.h> // GLfloat, GLuint etc.
 #endif
 
-// Include GLFW
-#ifndef __GLFW3_H_INCLUDED
-#define __GLFW3_H_INCLUDED
-#include <glfw3.h>
-#endif
-
-// Include GLM
-#ifndef __GLM_GLM_HPP_INCLUDED
-#define __GLM_GLM_HPP_INCLUDED
-#include <glm/glm.hpp> // glm
-#endif
-
-#ifndef __GLM_GTC_MATRIX_TRANSFORM_HPP_INCLUDED
-#define __GLM_GTC_MATRIX_TRANSFORM_HPP_INCLUDED
-#include <glm/gtc/matrix_transform.hpp>
-#endif
-
-#ifndef __GLM_GTC_QUATERNION_HPP_INCLUDED
-#define __GLM_GTC_QUATERNION_HPP_INCLUDED
-#include <glm/gtc/quaternion.hpp> // glm::quat
-#endif
-
-#ifndef __GLM_GTX_QUATERNION_HPP_INCLUDED
-#define __GLM_GTX_QUATERNION_HPP_INCLUDED
-#include <glm/gtx/quaternion.hpp> // glm::toMat4
-#endif
-
 #include "cpp/common/hierarchy.hpp"
 
 // Include standard headers
