9567fae45e2a9f9253ff112abcf3df1a7331f84b
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-07 23:56:59 +0200

    Lisätty FIXME-kommentti: virheellinen pointterin arvo.
    	modified:   cpp/ylikuutio/model/font_loader.cpp

--- cpp/ylikuutio/model/font_loader.cpp
+++ cpp/ylikuutio/model/font_loader.cpp
@@ -72,9 +72,8 @@ namespace model
         char char_number_buffer[1024];
         char* dest_mem_pointer;
         dest_mem_pointer = char_number_buffer;
-
         model::extract_string_with_several_endings(dest_mem_pointer, vertex_data_pointer, char_end_string);
-        uint32_t value = atoi(dest_mem_pointer);
+        uint32_t value = atoi(dest_mem_pointer); // FIXME: sometimes the value of `dest_mem_pointer` is invalid and causes segmentation fault here.
         printf("%s: %d\n", description, value);
         return value;
     }
