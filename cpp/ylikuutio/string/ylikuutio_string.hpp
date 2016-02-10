0db6582d0407ba468f967c993ff4ff6c6be84158
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-11 00:58:49 +0200

    Poistettu ylimääräinen `#include <glm/glm.hpp>` (ja 'include guard').
    	modified:   cpp/ylikuutio/string/ylikuutio_string.hpp

--- cpp/ylikuutio/string/ylikuutio_string.hpp
+++ cpp/ylikuutio/string/ylikuutio_string.hpp
@@ -5,12 +5,6 @@
 #include <string>    // std::string
 #include <vector>    // std::vector
 
-// Include GLM
-#ifndef __GLM_GLM_HPP_INCLUDED
-#define __GLM_GLM_HPP_INCLUDED
-#include <glm/glm.hpp> // glm
-#endif
-
 namespace string
 {
     bool check_and_report_if_some_string_matches(const char* SVG_base_pointer, char* SVG_data_pointer, std::vector<std::string> identifier_strings_vector);
