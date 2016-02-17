bdccb96df386e95efae9ad77f0d3fe5c7430f5a8
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-17 23:41:32 +0200

    `TEST(ObjectStruct_must_be_initialized_appropriately, ObjectStruct)`.
    Kaikki testit menevät läpi.
    
    	modified:   cpp/ylikuutio/tests/test.cpp

--- cpp/ylikuutio/tests/test.cpp
+++ cpp/ylikuutio/tests/test.cpp
@@ -37,3 +37,16 @@ TEST(NodeStruct_must_be_initialized_appropriately, NodeStruct)
     ASSERT_EQ(test_node_struct.coordinate_vector, glm::vec3(0.0f, 0.0f, 0.0f));
     ASSERT_EQ(test_node_struct.neighbor_nodeIDs.size(), 0);
 }
+
+TEST(ObjectStruct_must_be_initialized_appropriately, ObjectStruct)
+{
+    ObjectStruct test_object_struct;
+    ASSERT_EQ(test_object_struct.species_parent_pointer, nullptr);
+    ASSERT_EQ(test_object_struct.glyph_parent_pointer, nullptr);
+    ASSERT_EQ(test_object_struct.original_scale_vector, glm::vec3(1.0f, 1.0f, 1.0f));
+    ASSERT_TRUE(std::isnan(test_object_struct.rotate_angle));
+    ASSERT_FALSE(test_object_struct.is_character);
+    ASSERT_EQ(test_object_struct.coordinate_vector, glm::vec3(0.0f, 0.0f, 0.0f));
+    ASSERT_EQ(test_object_struct.rotate_vector, glm::vec3(0.0f, 0.0f, 0.0f));
+    ASSERT_EQ(test_object_struct.translate_vector, glm::vec3(0.0f, 0.0f, 0.0f));
+}
