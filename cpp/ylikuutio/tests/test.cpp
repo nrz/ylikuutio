2162e84df31ed78ac7d940ac1d4c6d52f182721a
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-17 23:22:20 +0200

    `TEST(ShaderStruct_must_be_initialized_appropriately, ShaderStruct)`.
    Kaikki testit menevät läpi.
    
    	modified:   cpp/ylikuutio/tests/test.cpp

--- cpp/ylikuutio/tests/test.cpp
+++ cpp/ylikuutio/tests/test.cpp
@@ -1,7 +1,16 @@
 #include "gtest/gtest.h"
 #include "testable.hpp"
+#include "cpp/ylikuutio/common/globals.hpp"
 
 TEST(GetTwoTest, Two)
 {
     EXPECT_EQ(2, getTwo());
+}
+
+TEST(ShaderStruct_must_be_initialized_appropriately, ShaderStruct)
+{
+    ShaderStruct test_shader_struct;
+    ASSERT_EQ(test_shader_struct.parent_pointer, nullptr);
+    ASSERT_TRUE(test_shader_struct.vertex_shader.empty());
+    ASSERT_TRUE(test_shader_struct.fragment_shader.empty());
 }
