27f9b95cf7c43d7a94362a5b95c1ccf32a1bd24f
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-17 23:24:29 +0200

    `TEST(MaterialStruct_must_be_initialized_appropriately` ...
    Kaikki testit menevät läpi.
    
    	modified:   cpp/ylikuutio/tests/test.cpp

--- cpp/ylikuutio/tests/test.cpp
+++ cpp/ylikuutio/tests/test.cpp
@@ -14,3 +14,12 @@ TEST(ShaderStruct_must_be_initialized_appropriately, ShaderStruct)
     ASSERT_TRUE(test_shader_struct.vertex_shader.empty());
     ASSERT_TRUE(test_shader_struct.fragment_shader.empty());
 }
+
+TEST(MaterialStruct_must_be_initialized_appropriately, MaterialStruct)
+{
+    MaterialStruct test_material_struct;
+    ASSERT_EQ(test_material_struct.parent_pointer, nullptr);
+    ASSERT_TRUE(test_material_struct.texture_file_format.empty());
+    ASSERT_TRUE(test_material_struct.texture_filename.empty());
+    ASSERT_TRUE(test_material_struct.image_path.empty());
+}
