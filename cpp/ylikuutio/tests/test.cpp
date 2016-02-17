3e5339c7c72577ee63410e0e9b6d3e78f7208c83
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-17 23:35:39 +0200

    `TEST(NodeStruct_must_be_initialized_appropriately, NodeStruct)`.
    Kaikki testit menevät läpi.
    
    	modified:   cpp/ylikuutio/tests/test.cpp

--- cpp/ylikuutio/tests/test.cpp
+++ cpp/ylikuutio/tests/test.cpp
@@ -2,6 +2,12 @@
 #include "testable.hpp"
 #include "cpp/ylikuutio/common/globals.hpp"
 
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
 TEST(GetTwoTest, Two)
 {
     EXPECT_EQ(2, getTwo());
@@ -22,4 +28,12 @@ TEST(MaterialStruct_must_be_initialized_appropriately, MaterialStruct)
     ASSERT_TRUE(test_material_struct.texture_file_format.empty());
     ASSERT_TRUE(test_material_struct.texture_filename.empty());
     ASSERT_TRUE(test_material_struct.image_path.empty());
+}
+
+TEST(NodeStruct_must_be_initialized_appropriately, NodeStruct)
+{
+    NodeStruct test_node_struct;
+    ASSERT_EQ(test_node_struct.parent_pointer, nullptr);
+    ASSERT_EQ(test_node_struct.coordinate_vector, glm::vec3(0.0f, 0.0f, 0.0f));
+    ASSERT_EQ(test_node_struct.neighbor_nodeIDs.size(), 0);
 }
