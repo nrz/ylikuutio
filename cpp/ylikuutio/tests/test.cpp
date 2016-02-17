dbf256d5e7b0dc0adc3548d9747bf384b4512178
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-18 01:13:39 +0200

    `TEST(a_256x256_BMP_world_must_be_loaded_appropriately` ...
    Kaikki testit menevät läpi.
    
    	modified:   cpp/ylikuutio/tests/test.cpp

--- cpp/ylikuutio/tests/test.cpp
+++ cpp/ylikuutio/tests/test.cpp
@@ -326,3 +326,34 @@ TEST(a_3x3_BMP_world_must_be_loaded_appropriately, load_3x3_BMP_world)
     ASSERT_EQ(out_UVs.size(), n_vertices_for_face * n_faces_for_bilinear_triangulation * (n_width_of_image_file - 1) * (n_height_of_image_file - 1));
     ASSERT_EQ(out_normals.size(), n_vertices_for_face * n_faces_for_bilinear_triangulation * (n_width_of_image_file - 1) * (n_height_of_image_file - 1));
 }
+
+TEST(a_256x256_BMP_world_must_be_loaded_appropriately, load_256x256_BMP_world)
+{
+    std::string image_path = "noise256x256.bmp";
+    std::vector<glm::vec3> out_vertices;
+    std::vector<glm::vec2> out_UVs;
+    std::vector<glm::vec3> out_normals;
+    GLuint image_width = 0;
+    GLuint image_height = 0;
+    std::string color_channel = "mean";
+    std::string triangulation_type = "bilinear_interpolation";
+
+    bool model_loading_result = model::load_BMP_world(
+            image_path,
+            *&out_vertices,
+            *&out_UVs,
+            *&out_normals,
+            *&image_width,
+            *&image_height,
+            color_channel,
+            triangulation_type);
+
+    const uint32_t n_vertices_for_face = 3;
+    const uint32_t n_faces_for_bilinear_triangulation = 4;
+    const uint32_t n_width_of_image_file = 256;
+    const uint32_t n_height_of_image_file = 256;
+
+    ASSERT_EQ(out_vertices.size(), n_vertices_for_face * n_faces_for_bilinear_triangulation * (n_width_of_image_file - 1) * (n_height_of_image_file - 1));
+    ASSERT_EQ(out_UVs.size(), n_vertices_for_face * n_faces_for_bilinear_triangulation * (n_width_of_image_file - 1) * (n_height_of_image_file - 1));
+    ASSERT_EQ(out_normals.size(), n_vertices_for_face * n_faces_for_bilinear_triangulation * (n_width_of_image_file - 1) * (n_height_of_image_file - 1));
+}
