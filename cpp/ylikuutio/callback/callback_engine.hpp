43588329c9864f7e372144981f9d65637054f5d3
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-02-24 23:38:51 +0200

    Poistettu ylimääräisiä takaisinkutsu-typedef-määrityksiä.
    	modified:   cpp/ylikuutio/callback/callback_engine.hpp

--- cpp/ylikuutio/callback/callback_engine.hpp
+++ cpp/ylikuutio/callback/callback_engine.hpp
@@ -16,23 +16,7 @@ namespace model
 }
 
 // callback typedefs in alphabetical order.
-typedef bool (*BoolToBoolCallback)(bool);
-typedef float (*BoolToFloatCallback)(bool);
-typedef int (*BoolToIntCallback)(bool);
-typedef void (*BoolToVoidCallback)(bool);
-typedef bool (*FloatToBoolCallback)(float);
-typedef float (*FloatToFloatCallback)(float);
-typedef int (*FloatToIntCallback)(float);
-typedef void (*FloatToVoidCallback)(float);
-typedef bool (*IntToBoolCallback)(int);
-typedef float (*IntToFloatCallback)(int);
-typedef int (*IntToIntCallback)(int);
-typedef void (*IntToVoidCallback)(int);
-typedef bool (*VoidToBoolCallback)(void);
-typedef float (*VoidToFloatCallback)(void);
-typedef int (*VoidToIntCallback)(void);
 typedef void (*VoidToVoidCallback)(void);
-typedef void (*WorldToVoidCallback)(model::World*);
 typedef void (*AnyValueToVoidCallback)(AnyValue);
 typedef AnyValue (*VoidToAnyValueCallback)(void);
 typedef AnyValue (*AnyValueToAnyValueCallback)(AnyValue);
