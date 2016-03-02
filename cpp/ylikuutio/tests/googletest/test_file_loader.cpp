8037fb11958382fbfc21967d7372a64c78e65149
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-03-02 02:06:58 +0200

    `TEST(file_must_be_slurped_appropriately`: Tarkistetaan myös rivi 10.
    Kaikki testit menevät läpi.
    
    	modified:   cpp/ylikuutio/tests/googletest/test_file_loader.cpp

--- cpp/ylikuutio/tests/googletest/test_file_loader.cpp
+++ cpp/ylikuutio/tests/googletest/test_file_loader.cpp
@@ -9,4 +9,9 @@ TEST(file_must_be_slurped_appropriately, kongtext_svg)
     const char* first_line_of_kongtext_svg = "<?xml version=\"1.0\" standalone=\"no\"?>";
     printf("%.*s\n", strlen(first_line_of_kongtext_svg), file_contents_char);
     ASSERT_EQ(strncmp(file_contents_char, first_line_of_kongtext_svg, strlen(first_line_of_kongtext_svg)), 0);
+
+    int offset_of_first_character_of_tenth_line = 0x126;
+    const char* tenth_line_of_kongtext_svg = "<font id=\"KongtextRegular\" horiz-adv-x=\"1024\" >";
+    printf("%.*s\n", strlen(tenth_line_of_kongtext_svg), file_contents_char + offset_of_first_character_of_tenth_line);
+    ASSERT_EQ(strncmp(file_contents_char + offset_of_first_character_of_tenth_line, tenth_line_of_kongtext_svg, strlen(tenth_line_of_kongtext_svg)), 0);
 }
