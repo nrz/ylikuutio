7ffca9b9a8abffd81e69530c7dafda8921b33cfa
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-03-13 16:49:52 +0200

    `callback_system::CallbackObject *parent_pointer`.
    All tests pass.
    
    	modified:   cpp/ylikuutio/callback/callback_parameter.hpp

--- cpp/ylikuutio/callback/callback_parameter.hpp
+++ cpp/ylikuutio/callback/callback_parameter.hpp
@@ -36,6 +36,8 @@ namespace callback_system
 
             uint32_t childID;                      // callback parameter ID, returned by `callback_system::CallbackObject->get_callback_parameterID()`.
 
+            callback_system::CallbackObject *parent_pointer; // pointer to the callback object.
+
             std::string name;
             AnyValue any_value;
             bool is_reference; // if true, the value is read from the hashmap. if false, then the value is read from the union.
