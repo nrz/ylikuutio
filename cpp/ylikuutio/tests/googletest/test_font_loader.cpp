6d8a868b988d6760697f10e9fc5de81f9604a593
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-03-20 23:54:09 +0200

    More assertions (`ASSERT_EQ`) to `font_loader.*` test.
    All tests pass.
    
    	modified:   cpp/ylikuutio/tests/googletest/test_font_loader.cpp

--- cpp/ylikuutio/tests/googletest/test_font_loader.cpp
+++ cpp/ylikuutio/tests/googletest/test_font_loader.cpp
@@ -14,8 +14,9 @@
 #endif
 
 // Include standard headers
-#include <string>   // std::string
-#include <vector>   // std::vector
+#include <cstring> // std::memcmp, std::strcmp, std::strlen, std::strncmp
+#include <string>  // std::string
+#include <vector>  // std::vector
 
 TEST(font_must_be_loaded_appropriately, kongtext_svg)
 {
@@ -38,4 +39,25 @@ TEST(font_must_be_loaded_appropriately, kongtext_svg)
             vertex_scaling_factor);
     ASSERT_EQ(glyph_names.size(), 217);
     ASSERT_EQ(glyph_vertex_data.size(), 217);
+    ASSERT_EQ(std::strcmp(glyph_names.at(0).c_str(), "nonmarkingreturn"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(1).c_str(), "space"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(2).c_str(), "exclam"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(3).c_str(), "quotedbl"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(4).c_str(), "numbersign"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(5).c_str(), "dollar"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(6).c_str(), "percent"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(7).c_str(), "ampersand"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(8).c_str(), "quotesingle"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(9).c_str(), "parenleft"), 0);
+
+    ASSERT_EQ(std::strcmp(glyph_names.at(207).c_str(), "quotedblbase"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(208).c_str(), "dagger"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(209).c_str(), "daggerdbl"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(210).c_str(), "bullet"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(211).c_str(), "ellipsis"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(212).c_str(), "perthousand"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(213).c_str(), "guilsinglleft"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(214).c_str(), "guilsinglright"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(215).c_str(), "Euro"), 0);
+    ASSERT_EQ(std::strcmp(glyph_names.at(216).c_str(), "trademark"), 0);
 }
