468eb858b092b0de1f7e9750fb656301c5431bf3
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-03-26 23:51:23 +0200

    Function declarations. Some code, compiles but does nothing useful.
    No tests for these functions yet.
    
    All tests pass.
    
    	modified:   CMakeLists.txt
    	new file:   cpp/ylikuutio/geometry/line_line_intersection.cpp
    	new file:   cpp/ylikuutio/geometry/line_line_intersection.hpp
    	new file:   cpp/ylikuutio/geometry/line_segment_intersection.cpp
    	new file:   cpp/ylikuutio/geometry/line_segment_intersection.hpp

---
+++ cpp/ylikuutio/geometry/line_line_intersection.cpp
@@ -0,0 +1,16 @@
+#include "line_line_intersection.hpp"
+
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
+namespace geometry
+{
+    glm::vec2 do_lines_intersect(glm::vec2 line_1_vertex_1, glm::vec2 line_1_vertex_2, glm::vec2 line_2_vertex_1, glm::vec2 line_2_vertex_2)
+    {
+        glm::vec2 intersection_point;
+        return intersection_point;
+    }
+}
