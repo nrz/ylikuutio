2f71da9482813cc66ac0644e944051daefce54dd
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-03-28 22:34:48 +0300

    `class Line`.
    Not much code yet.
    
    All tests pass.
    
    	modified:   CMakeLists.txt
    	new file:   cpp/ylikuutio/geometry/line.cpp
    	new file:   cpp/ylikuutio/geometry/line.hpp

---
+++ cpp/ylikuutio/geometry/line.cpp
@@ -0,0 +1,17 @@
+#include "line.hpp"
+
+// Include standard headers
+#include <string>   // std::string
+
+namespace geometry
+{
+    Line::Line()
+    {
+    }
+
+    std::string Line::get_equation()
+    {
+        std::string line_equation;
+        return line_equation;
+    }
+}
---
+++ cpp/ylikuutio/geometry/line.hpp
@@ -0,0 +1,18 @@
+#ifndef __LINE_HPP_INCLUDED
+#define __LINE_HPP_INCLUDED
+
+// Include standard headers
+#include <string>   // std::string
+
+namespace geometry
+{
+    class Line
+    {
+        // constructor.
+        Line();
+
+        std::string get_equation();
+    };
+}
+
+#endif
