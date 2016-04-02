6d6b975ff88b851391c1df95d8cfae592f5b0c4e
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-04-02 23:56:46 +0300

    Modified `class Line` (not complete yet).
    All tests pass.
    
    	modified:   cpp/ylikuutio/geometry/line.cpp
    	modified:   cpp/ylikuutio/geometry/line.hpp

--- cpp/ylikuutio/geometry/line.cpp
+++ cpp/ylikuutio/geometry/line.cpp
@@ -5,11 +5,17 @@
 
 namespace geometry
 {
-    Line::Line()
+    Line::Line(std::vector<float> point1, std::vector<float> point2)
     {
     }
 
-    std::string Line::get_equation()
+    Line::Line(std::vector<float> general_form_coefficients, float general_form_constant)
+    {
+        this->general_form_coefficients = general_form_coefficients;
+        this->general_form_constant = general_form_constant;
+    }
+
+    std::string Line::get_general_form_equation()
     {
         std::string line_equation;
         return line_equation;
--- cpp/ylikuutio/geometry/line.hpp
+++ cpp/ylikuutio/geometry/line.hpp
@@ -3,15 +3,24 @@
 
 // Include standard headers
 #include <string>   // std::string
+#include <vector>   // std::vector
 
 namespace geometry
 {
     class Line
     {
-        // constructor.
-        Line();
+        public:
+            // constructor.
+            Line(std::vector<float> point1, std::vector<float> point2);
 
-        std::string get_equation();
+            // constructor.
+            Line(std::vector<float> general_form_coefficients, float general_form_constant);
+
+            std::string get_general_form_equation();
+
+        protected:
+            std::vector<float> general_form_coefficients;
+            float general_form_constant;
     };
 }
 
