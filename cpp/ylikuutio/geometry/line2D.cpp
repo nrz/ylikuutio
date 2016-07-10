b2e2bb9bda658e881b1c46920671503235a060aa
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-10 12:38:36 +0300

    Added commented-out code for `Line2D` constructors.
    	modified:   cpp/ylikuutio/geometry/line2D.cpp

--- cpp/ylikuutio/geometry/line2D.cpp
+++ cpp/ylikuutio/geometry/line2D.cpp
@@ -1,8 +1,24 @@
 #include "line2D.hpp"
 
 // Include standard headers
+#include <iostream> // std::cout, std::cin, std::cerr
 #include <string>   // std::string
+#include <vector>   // std::vector
 
 namespace geometry
 {
+    /*
+    Line2D::Line2D(std::vector<float> point1, std::vector<float> point2)
+    {
+        Line(std::vector<float> point1, std::vector<float> point2);
+    }
+    */
+
+    /*
+    Line2D::Line2D(std::vector<float> general_form_coefficients, float general_form_constant)
+    {
+        std::cout << "Hello from Line2D constructor!\n";
+        Line2D(std::vector<float> general_form_coefficients, float general_form_constant);
+    }
+    */
 }
