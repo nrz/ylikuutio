bbb7d545af32be7d2beee7a93fdf2f67e2ff0143
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-10 12:48:26 +0300

    `class LineSegment3D`. Inherits `class LineSegment`.
    All tests pass.
    
    	modified:   CMakeLists.txt
    	new file:   cpp/ylikuutio/geometry/line_segment3D.cpp
    	new file:   cpp/ylikuutio/geometry/line_segment3D.hpp

---
+++ cpp/ylikuutio/geometry/line_segment3D.cpp
@@ -0,0 +1,16 @@
+#include "line_segment3D.hpp"
+
+// Include standard headers
+#include <iostream> // std::cout, std::cin, std::cerr
+#include <string>   // std::string
+#include <vector>   // std::vector
+
+namespace geometry
+{
+    /*
+    LineSegment3D::LineSegment3D(std::vector<float> point1, std::vector<float> point2)
+    {
+        std::cout << "Hello from LineSegment3D constructor!\n";
+    }
+    */
+}
