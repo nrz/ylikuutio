af76b233189bbccdfd291f3e18c17d8bb971c759
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-19 00:26:55 +0300

    Revert "Removed most getters and setters of `class CustomNamespace`."
    This reverts commit d7c6fa0d3197b8a143f3d61f9ce4ccacf9852133.
    
    However, `datatypes::AnyValue* get_any_value` still returns a pointer.
    
    All tests pass.
    
    	modified:   cpp/ylikuutio/callback/callback_object.cpp
    	modified:   cpp/ylikuutio/callback/custom_namespace.cpp
    	modified:   cpp/ylikuutio/callback/custom_namespace.hpp
    
    Conflicts:
    	cpp/ylikuutio/callback/custom_namespace.cpp
    	cpp/ylikuutio/callback/custom_namespace.hpp

--- cpp/ylikuutio/callback/custom_namespace.cpp
+++ cpp/ylikuutio/callback/custom_namespace.cpp
@@ -7,15 +7,71 @@
 
 namespace callback_system
 {
-    // getter function for callbacks and callback objects.
+    // getter functions for callbacks and callback objects.
     datatypes::AnyValue* CustomNamespace::get_any_value(std::string name)
     {
         return &this->anyvalue_hashmap[name];
     }
 
-    // setter function for callbacks and callback objects.
-    void CustomNamespace::set_any_value(std::string name, datatypes::AnyValue any_value)
+    bool CustomNamespace::get_bool(std::string name)
     {
-        this->anyvalue_hashmap[name] = any_value;
+        return this->anyvalue_hashmap[name].bool_value;
+    }
+
+    float CustomNamespace::get_float(std::string name)
+    {
+        return this->anyvalue_hashmap[name].float_value;
+    }
+
+    double CustomNamespace::get_double(std::string name)
+    {
+        return this->anyvalue_hashmap[name].double_value;
+    }
+
+    int32_t CustomNamespace::get_int32_t(std::string name)
+    {
+        return this->anyvalue_hashmap[name].int32_t_value;
+    }
+
+    uint32_t CustomNamespace::get_uint32_t(std::string name)
+    {
+        return this->anyvalue_hashmap[name].uint32_t_value;
+    }
+
+    void* CustomNamespace::get_void_pointer(std::string name)
+    {
+        return this->anyvalue_hashmap[name].void_pointer;
+    }
+
+    // setter functions for callbacks and callback objects.
+
+    void CustomNamespace::set_bool(std::string name, bool value)
+    {
+        this->anyvalue_hashmap[name] = datatypes::AnyValue(value);
+    }
+
+    void CustomNamespace::set_float(std::string name, float value)
+    {
+        this->anyvalue_hashmap[name] = datatypes::AnyValue(value);
+    }
+
+    void CustomNamespace::set_double(std::string name, double value)
+    {
+        this->anyvalue_hashmap[name] = datatypes::AnyValue(value);
+    }
+
+    void CustomNamespace::set_int32_t(std::string name, int32_t value)
+    {
+        this->anyvalue_hashmap[name] = datatypes::AnyValue(value);
+    }
+
+    void CustomNamespace::set_uint32_t(std::string name, uint32_t value)
+    {
+        this->anyvalue_hashmap[name] = datatypes::AnyValue(value);
+    }
+
+    void CustomNamespace::set_void_pointer(std::string name, void* value)
+    {
+        this->anyvalue_hashmap[name] = datatypes::AnyValue(value);
     }
 }
--- cpp/ylikuutio/callback/custom_namespace.hpp
+++ cpp/ylikuutio/callback/custom_namespace.hpp
@@ -15,9 +15,20 @@ namespace callback_system
         protected:
             // getter functions for callbacks and callback objects.
             datatypes::AnyValue* get_any_value(std::string name);
+            bool get_bool(std::string name);
+            float get_float(std::string name);
+            double get_double(std::string name);
+            int32_t get_int32_t(std::string name);
+            uint32_t get_uint32_t(std::string name);
+            void* get_void_pointer(std::string name);
 
             // setter functions for callbacks and callback objects.
-            void set_any_value(std::string name, datatypes::AnyValue any_value);
+            void set_bool(std::string name, bool value);
+            void set_float(std::string name, float value);
+            void set_double(std::string name, double value);
+            void set_int32_t(std::string name, int32_t value);
+            void set_uint32_t(std::string name, uint32_t value);
+            void set_void_pointer(std::string name, void* value);
 
             // A hash map used to store variables.
             std::unordered_map<std::string, datatypes::AnyValue> anyvalue_hashmap;
