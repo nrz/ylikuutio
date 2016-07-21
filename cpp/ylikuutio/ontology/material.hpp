ba7ca1b448feedf1e19f1c64faa198c6ebd1c775
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-21 10:51:32 +0300

    `void Material::render()` is `private` again.
    All tests pass.
    
    	modified:   cpp/ylikuutio/ontology/material.hpp

--- cpp/ylikuutio/ontology/material.hpp
+++ cpp/ylikuutio/ontology/material.hpp
@@ -43,16 +43,16 @@ namespace ontology
             friend class Species;
             friend class Object;
             template<class T1>
-                friend void render_children(std::vector<void*> &child_pointer_vector);
+                friend void render_children(std::vector<T1> &child_pointer_vector);
             template<class T1>
                 friend void hierarchy::bind_child_to_parent(T1 child_pointer, std::vector<T1> &child_pointer_vector, std::queue<uint32_t> &free_childID_queue);
             template<class T1, class T2>
                 friend void hierarchy::bind_child_to_new_parent(T1 child_pointer, T2 new_parent_pointer, std::vector<T1> &old_child_pointer_vector, std::queue<uint32_t> &old_free_childID_queue);
 
+        private:
             // this method renders all species using this texture.
             void render();
 
-        private:
             // this method sets `Species` pointer.
             void set_species_pointer(uint32_t childID, ontology::Species* child_pointer);
 
