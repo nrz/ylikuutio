d68b9331e5a37712f49cc4a88809d80cf18d0857
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-22 18:05:56 +0300

    Bugfix: Removed invalid extra qualifications.
    	modified:   cpp/ylikuutio/console/console.hpp

--- cpp/ylikuutio/console/console.hpp
+++ cpp/ylikuutio/console/console.hpp
@@ -19,14 +19,14 @@ namespace console
             void add_character(char character);
             void backspace(char character);
             void delete_character(char character);
-            void Console::move_cursor_left();
-            void Console::move_cursor_right();
-            void Console::move_cursor_to_start_of_line();
-            void Console::move_cursor_to_end_of_line();
-            void Console::page_up();
-            void Console::page_down();
-            void Console::home();
-            void Console::end();
+            void move_cursor_left();
+            void move_cursor_right();
+            void move_cursor_to_start_of_line();
+            void move_cursor_to_end_of_line();
+            void page_up();
+            void page_down();
+            void home();
+            void end();
 
         private:
             std::list<char> current_input;
