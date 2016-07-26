644f471061054ac8295cf3b93f6ef79e31e57d47
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-26 21:42:08 +0300

    `#define EXIT_PROGRAM_MAGIC_NUMBER 0x0c0ea1ca`.
    	modified:   cpp/ylikuutio/callback_system/callback_magic_numbers.hpp

--- cpp/ylikuutio/callback_system/callback_magic_numbers.hpp
+++ cpp/ylikuutio/callback_system/callback_magic_numbers.hpp
@@ -5,5 +5,6 @@
 #define EXIT_CONSOLE_MAGIC_NUMBER 0xaa110cc0  // "aallocco" ~ "aallokko" in standard Finnish ortography, means "swell of the sea/lake" in Finnish
 #define PAUSE_MAGIC_NUMBER 0x1c1a1ca          // "iciaica" ~ "ikiaika" in standard Finnish ortography, means "eternity" in Finnish
 #define CONTINUE_MAGIC_NUMBER 0xede11e        // "edelle", means "ahead" or "before" in Finnish
+#define EXIT_PROGRAM_MAGIC_NUMBER 0x0c0ea1ca  // "coeaica" ~ "koeaika", means "trial period" or "probation" or "parole" in Finnish
 
 #endif
