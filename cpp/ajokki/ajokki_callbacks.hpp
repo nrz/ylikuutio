84854ef62a7a0891d80307382f08f65e3c688b0a
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-07-27 09:33:59 +0300

    Cleaning up: Added empty lines between function declarations.
    	modified:   cpp/ajokki/ajokki_callbacks.hpp

--- cpp/ajokki/ajokki_callbacks.hpp
+++ cpp/ajokki/ajokki_callbacks.hpp
@@ -12,26 +12,32 @@ namespace ajokki
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
             std::vector<callback_system::CallbackParameter*> input_parameters);
+
     datatypes::AnyValue* full_cleanup(
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
             std::vector<callback_system::CallbackParameter*> input_parameters);
+
     datatypes::AnyValue* exit_program(
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
             std::vector<callback_system::CallbackParameter*>);
+
     datatypes::AnyValue* exit_console(
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
             std::vector<callback_system::CallbackParameter*> input_parameters);
+
     datatypes::AnyValue* delete_suzanne_species(
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
             std::vector<callback_system::CallbackParameter*> input_parameters);
+
     datatypes::AnyValue* switch_to_new_material(
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
             std::vector<callback_system::CallbackParameter*> input_parameters);
+
     datatypes::AnyValue* transform_into_new_species(
             callback_system::CallbackEngine*,
             callback_system::CallbackObject*,
