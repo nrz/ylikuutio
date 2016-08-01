2953262082ebae021b4b4a154ab92af8cc15a857
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-01 21:33:15 +0300

    Cleaning up: deleted whitespace.
    	modified:   cpp/ylikuutio/ontology/objloader.cpp

--- cpp/ylikuutio/ontology/objloader.cpp
+++ cpp/ylikuutio/ontology/objloader.cpp
@@ -147,9 +147,9 @@ namespace ontology
                 vertexIndices.push_back(vertexIndex[0]);
                 vertexIndices.push_back(vertexIndex[1]);
                 vertexIndices.push_back(vertexIndex[2]);
-                uvIndices    .push_back(uvIndex[0]);
-                uvIndices    .push_back(uvIndex[1]);
-                uvIndices    .push_back(uvIndex[2]);
+                uvIndices.push_back(uvIndex[0]);
+                uvIndices.push_back(uvIndex[1]);
+                uvIndices.push_back(uvIndex[2]);
                 normalIndices.push_back(normalIndex[0]);
                 normalIndices.push_back(normalIndex[1]);
                 normalIndices.push_back(normalIndex[2]);
