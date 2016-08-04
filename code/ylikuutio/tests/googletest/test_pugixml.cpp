f17f869d1406f4d321b6f7194369418e88c92580
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-04 21:55:41 +0300

    pugixml test stubs.
    All tests pass.
    
    	modified:   CMakeLists.txt
    	new file:   code/ylikuutio/tests/googletest/test_pugixml.cpp

---
+++ code/ylikuutio/tests/googletest/test_pugixml.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+// Include Eigen
+#include "pugixml.hpp"
+
+// Include standard headers
+#include <iostream> // std::cout, std::cin, std::cerr
+
+TEST(xml_must_be_loaded_as_expected, exactum_and_physicum)
+{
+}
+
+TEST(xml_must_be_loaded_as_expected, hofinkatu_and_isafjordinkatu)
+{
+}
