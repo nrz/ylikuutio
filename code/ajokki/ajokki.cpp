f491be87a2eb116284a322760942df1a206b7dcd
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-06 11:40:18 +0300

    If `testing_spherical_world_in_use`, then flight mode is on at start.
    This is make debugging and development of spherical world code easier.
    
    All tests pass.
    
    	modified:   code/ajokki/ajokki.cpp

--- code/ajokki/ajokki.cpp
+++ code/ajokki/ajokki.cpp
@@ -98,6 +98,8 @@ int main(void)
     // position = glm::vec3(100.0f, 5000.0f, 100.0f);
     if (testing_spherical_world_in_use)
     {
+        is_flight_mode_in_use = true;
+
         // position = glm::vec3(100.0f, 5000.0f + EARTH_RADIUS, 100.0f);
         // position = glm::vec3(100.0f, 5000.0f, 100.0f);
         // position = glm::vec3(-5658223.00f, -1700825.12f, 2322764.00f);
