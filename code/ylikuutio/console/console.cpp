94696a00698c40e68ad90ec7ab2e2a55f7c26e5e
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-09 22:40:22 +0300

    Reordered function definitions to match the order in `console.hpp`.
    Added comments, edited some whitespace.
    
    All tests pass.
    
    	modified:   code/ylikuutio/console/console.cpp

--- code/ylikuutio/console/console.cpp
+++ code/ylikuutio/console/console.cpp
@@ -60,28 +60,53 @@ namespace console
         this->exit_console();
     }
 
-    // callbacks.
-
-    // Keep these variable types as this is according to GLFW documentation!
-    // So: `unsigned int codepoint`, `int mods`.
-    void Console::charmods_callback(GLFWwindow* window, unsigned int codepoint, int mods)
+    void Console::set_my_keypress_callback_engine_vector_pointer(std::vector<KeyAndCallbackStruct>* my_keypress_callback_engine_vector_pointer)
     {
-        // `int mods` values:
-        // No modificators:       0x00
-        // Shift (left or right): 0x01
-        // Alt (not AltGr):       0x04
-        // Shift + Alt:           0x05
-        if ((mods == 0 || mods == 1) &&
-                global_console_pointer->in_console &&
-                !global_console_pointer->is_left_control_pressed &&
-                !global_console_pointer->is_right_control_pressed &&
-                !global_console_pointer->is_left_alt_pressed)
+        this->my_keypress_callback_engine_vector_pointer = my_keypress_callback_engine_vector_pointer;
+    }
+
+    void Console::set_my_keyrelease_callback_engine_vector_pointer(std::vector<KeyAndCallbackStruct>* my_keyrelease_callback_engine_vector_pointer)
+    {
+        this->my_keyrelease_callback_engine_vector_pointer = my_keyrelease_callback_engine_vector_pointer;
+    }
+
+    void Console::draw_console()
+    {
+        if (this->in_console)
         {
-            global_console_pointer->cursor_it = global_console_pointer->current_input.insert(global_console_pointer->cursor_it, static_cast<char>(codepoint));
-            global_console_pointer->cursor_it++;
-            global_console_pointer->cursor_index++;
+            // Convert current input into std::string.
+            uint32_t characters_for_line = window_width / text_size;
+
+            // Draw the console to screen using `font2D::printText2D`.
+            PrintingStruct printing_struct;
+            printing_struct.screen_width = (GLuint) window_width;
+            printing_struct.screen_height = (GLuint) window_height;
+            printing_struct.text_size = text_size;
+            printing_struct.font_size = font_size;
+            printing_struct.char_font_texture_file_format = "bmp";
+
+            printing_struct.x = 0;
+            printing_struct.y = window_height - (2 * text_size);
+            printing_struct.horizontal_alignment = "left";
+            printing_struct.vertical_alignment = "top";
+            printing_struct.text =
+                "Welcome! Please write \"help\"\\n"
+                "for more information.\\n";
+
+            for (uint32_t historical_input_i = 0; historical_input_i < this->command_history.size(); historical_input_i++)
+            {
+                std::list<char> historical_input = this->command_history.at(historical_input_i);
+                printing_struct.text += "$ " + string::convert_std_list_char_to_std_string(historical_input, characters_for_line - 2, characters_for_line) + "\\n";
+            }
+            printing_struct.text += "$ " + string::convert_std_list_char_to_std_string(this->current_input, characters_for_line - 2, characters_for_line);
+
+            this->text2D_pointer->printText2D(printing_struct);
         }
     }
+
+    // Public callbacks.
+
+    // Action mode keyrelease callbacks begin here.
 
     datatypes::AnyValue* Console::enable_enter_console(
             callback_system::CallbackEngine*,
@@ -95,6 +120,59 @@ namespace console
         }
         return nullptr;
     }
+
+    // Action mode keypress callbacks begin here.
+
+    datatypes::AnyValue* Console::enter_console(
+            callback_system::CallbackEngine*,
+            callback_system::CallbackObject*,
+            std::vector<callback_system::CallbackParameter*>&,
+            console::Console* console)
+    {
+        if (!console->in_console &&
+                console->can_enter_console &&
+                console->my_keypress_callback_engine_vector_pointer != nullptr &&
+                console->my_keyrelease_callback_engine_vector_pointer != nullptr &&
+                console->current_keypress_callback_engine_vector_pointer_pointer != nullptr &&
+                console->current_keyrelease_callback_engine_vector_pointer_pointer != nullptr)
+        {
+            // Store previous keypress callback engine vector pointer.
+            console->previous_keypress_callback_engine_vector_pointer =
+                *console->current_keypress_callback_engine_vector_pointer_pointer;
+
+            // Set new keypress callback engine vector pointer.
+            *console->current_keypress_callback_engine_vector_pointer_pointer =
+                console->my_keypress_callback_engine_vector_pointer;
+
+            // Store previous keyrelease callback engine vector pointer.
+            console->previous_keyrelease_callback_engine_vector_pointer =
+                *console->current_keyrelease_callback_engine_vector_pointer_pointer;
+
+            // Set new keyrelease callback engine vector pointer.
+            *console->current_keyrelease_callback_engine_vector_pointer_pointer =
+                console->my_keyrelease_callback_engine_vector_pointer;
+
+            glfwSetCharModsCallback(window, Console::charmods_callback);
+
+            // Mark that we're in console.
+            console->in_console = true;
+            console->in_historical_input = false;
+
+            // Usually key release is required to enable enter console.
+            console->can_exit_console = false;
+            console->can_enter_console = false;
+
+            // Signal to caller that we have entered the console.
+            uint32_t enter_console_magic_number = ENTER_CONSOLE_MAGIC_NUMBER;
+            datatypes::AnyValue* any_value_magic_number = new datatypes::AnyValue(enter_console_magic_number);
+            return any_value_magic_number;
+        }
+
+        // We did not enter the console.
+        return nullptr;
+    }
+
+    // Console mode keyrelease callbacks begin here.
 
     datatypes::AnyValue* Console::enable_exit_console(
             callback_system::CallbackEngine*,
@@ -252,54 +330,7 @@ namespace console
         return nullptr;
     }
 
-    datatypes::AnyValue* Console::enter_console(
-            callback_system::CallbackEngine*,
-            callback_system::CallbackObject*,
-            std::vector<callback_system::CallbackParameter*>&,
-            console::Console* console)
-    {
-        if (!console->in_console &&
-                console->can_enter_console &&
-                console->my_keypress_callback_engine_vector_pointer != nullptr &&
-                console->my_keyrelease_callback_engine_vector_pointer != nullptr &&
-                console->current_keypress_callback_engine_vector_pointer_pointer != nullptr &&
-                console->current_keyrelease_callback_engine_vector_pointer_pointer != nullptr)
-        {
-            // Store previous keypress callback engine vector pointer.
-            console->previous_keypress_callback_engine_vector_pointer =
-                *console->current_keypress_callback_engine_vector_pointer_pointer;
-
-            // Set new keypress callback engine vector pointer.
-            *console->current_keypress_callback_engine_vector_pointer_pointer =
-                console->my_keypress_callback_engine_vector_pointer;
-
-            // Store previous keyrelease callback engine vector pointer.
-            console->previous_keyrelease_callback_engine_vector_pointer =
-                *console->current_keyrelease_callback_engine_vector_pointer_pointer;
-
-            // Set new keyrelease callback engine vector pointer.
-            *console->current_keyrelease_callback_engine_vector_pointer_pointer =
-                console->my_keyrelease_callback_engine_vector_pointer;
-
-            glfwSetCharModsCallback(window, Console::charmods_callback);
-
-            // Mark that we're in console.
-            console->in_console = true;
-            console->in_historical_input = false;
-
-            // Usually key release is required to enable enter console.
-            console->can_exit_console = false;
-            console->can_enter_console = false;
-
-            // Signal to caller that we have entered the console.
-            uint32_t enter_console_magic_number = ENTER_CONSOLE_MAGIC_NUMBER;
-            datatypes::AnyValue* any_value_magic_number = new datatypes::AnyValue(enter_console_magic_number);
-            return any_value_magic_number;
-        }
-
-        // We did not enter the console.
-        return nullptr;
-    }
+    // Console mode keypress callbacks begin here.
 
     datatypes::AnyValue* Console::exit_console(
             callback_system::CallbackEngine*,
@@ -316,46 +347,6 @@ namespace console
         }
 
         // We did not exit the console.
-        return nullptr;
-    }
-
-    datatypes::AnyValue* Console::move_to_previous_input(
-            callback_system::CallbackEngine*,
-            callback_system::CallbackObject*,
-            std::vector<callback_system::CallbackParameter*>&,
-            console::Console* console)
-    {
-        if (console->in_console && console->can_move_to_previous_input)
-        {
-            if (!console->in_historical_input && console->command_history.size() > 0)
-            {
-                // OK, we moved from the new input to the last historical input.
-                console->in_historical_input = true;
-                console->historical_input_i = console->command_history.size() - 1;
-
-                // Copy the new input into temp input.
-                console->temp_input.clear();
-                std::copy(console->current_input.begin(), console->current_input.end(), std::back_inserter(console->temp_input));
-
-                // Copy the historical input into current input.
-                console->current_input.clear();
-                console->copy_historical_input_into_current_input();
-
-                console->move_cursor_to_end_of_line();
-                console->can_move_to_previous_input = false;
-            }
-            else if (console->in_historical_input && console->historical_input_i > 0)
-            {
-                // OK, we moved to the previous historical input.
-                console->historical_input_i--;
-
-                // Copy the historical input into current input.
-                console->copy_historical_input_into_current_input();
-
-                console->move_cursor_to_end_of_line();
-                console->can_move_to_previous_input = false;
-            }
-        }
         return nullptr;
     }
 
@@ -433,6 +424,46 @@ namespace console
         if (console->in_console)
         {
             console->is_right_shift_pressed = true;
+        }
+        return nullptr;
+    }
+
+    datatypes::AnyValue* Console::move_to_previous_input(
+            callback_system::CallbackEngine*,
+            callback_system::CallbackObject*,
+            std::vector<callback_system::CallbackParameter*>&,
+            console::Console* console)
+    {
+        if (console->in_console && console->can_move_to_previous_input)
+        {
+            if (!console->in_historical_input && console->command_history.size() > 0)
+            {
+                // OK, we moved from the new input to the last historical input.
+                console->in_historical_input = true;
+                console->historical_input_i = console->command_history.size() - 1;
+
+                // Copy the new input into temp input.
+                console->temp_input.clear();
+                std::copy(console->current_input.begin(), console->current_input.end(), std::back_inserter(console->temp_input));
+
+                // Copy the historical input into current input.
+                console->current_input.clear();
+                console->copy_historical_input_into_current_input();
+
+                console->move_cursor_to_end_of_line();
+                console->can_move_to_previous_input = false;
+            }
+            else if (console->in_historical_input && console->historical_input_i > 0)
+            {
+                // OK, we moved to the previous historical input.
+                console->historical_input_i--;
+
+                // Copy the historical input into current input.
+                console->copy_historical_input_into_current_input();
+
+                console->move_cursor_to_end_of_line();
+                console->can_move_to_previous_input = false;
+            }
         }
         return nullptr;
     }
@@ -529,49 +560,30 @@ namespace console
         return nullptr;
     }
 
-    void Console::set_my_keypress_callback_engine_vector_pointer(std::vector<KeyAndCallbackStruct>* my_keypress_callback_engine_vector_pointer)
-    {
-        this->my_keypress_callback_engine_vector_pointer = my_keypress_callback_engine_vector_pointer;
-    }
+    // Public callbacks end here.
 
-    void Console::set_my_keyrelease_callback_engine_vector_pointer(std::vector<KeyAndCallbackStruct>* my_keyrelease_callback_engine_vector_pointer)
+    // Keep these variable types as this is according to GLFW documentation!
+    // So: `unsigned int codepoint`, `int mods`.
+    void Console::charmods_callback(GLFWwindow* window, unsigned int codepoint, int mods)
     {
-        this->my_keyrelease_callback_engine_vector_pointer = my_keyrelease_callback_engine_vector_pointer;
-    }
-
-    void Console::draw_console()
-    {
-        if (this->in_console)
+        // `int mods` values:
+        // No modificators:       0x00
+        // Shift (left or right): 0x01
+        // Alt (not AltGr):       0x04
+        // Shift + Alt:           0x05
+        if ((mods == 0 || mods == 1) &&
+                global_console_pointer->in_console &&
+                !global_console_pointer->is_left_control_pressed &&
+                !global_console_pointer->is_right_control_pressed &&
+                !global_console_pointer->is_left_alt_pressed)
         {
-            // Convert current input into std::string.
-            uint32_t characters_for_line = window_width / text_size;
-
-            // Draw the console to screen using `font2D::printText2D`.
-            PrintingStruct printing_struct;
-            printing_struct.screen_width = (GLuint) window_width;
-            printing_struct.screen_height = (GLuint) window_height;
-            printing_struct.text_size = text_size;
-            printing_struct.font_size = font_size;
-            printing_struct.char_font_texture_file_format = "bmp";
-
-            printing_struct.x = 0;
-            printing_struct.y = window_height - (2 * text_size);
-            printing_struct.horizontal_alignment = "left";
-            printing_struct.vertical_alignment = "top";
-            printing_struct.text =
-                "Welcome! Please write \"help\"\\n"
-                "for more information.\\n";
-
-            for (uint32_t historical_input_i = 0; historical_input_i < this->command_history.size(); historical_input_i++)
-            {
-                std::list<char> historical_input = this->command_history.at(historical_input_i);
-                printing_struct.text += "$ " + string::convert_std_list_char_to_std_string(historical_input, characters_for_line - 2, characters_for_line) + "\\n";
-            }
-            printing_struct.text += "$ " + string::convert_std_list_char_to_std_string(this->current_input, characters_for_line - 2, characters_for_line);
-
-            this->text2D_pointer->printText2D(printing_struct);
+            global_console_pointer->cursor_it = global_console_pointer->current_input.insert(global_console_pointer->cursor_it, static_cast<char>(codepoint));
+            global_console_pointer->cursor_it++;
+            global_console_pointer->cursor_index++;
         }
     }
+
+    // Callbacks end here.
 
     void Console::copy_historical_input_into_current_input()
     {
@@ -610,6 +622,7 @@ namespace console
         }
         return false;
     }
+
     void Console::delete_character()
     {
         if (this->in_console)
