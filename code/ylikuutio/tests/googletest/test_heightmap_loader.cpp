3e526f70994bd6a80dceb526395de40ec2c4a024
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-11 18:29:14 +0300

    Assertions for face normals using `"bilinear_interpolation"`.
    More assertions for predefined vertices and interpolated vertices.
    
    All tests pass.
    
    	modified:   code/ylikuutio/tests/googletest/test_heightmap_loader.cpp

--- code/ylikuutio/tests/googletest/test_heightmap_loader.cpp
+++ code/ylikuutio/tests/googletest/test_heightmap_loader.cpp
@@ -160,6 +160,68 @@ TEST(vertices_must_be_defined_and_interpolated_appropriately, a_3x3_world)
     ASSERT_EQ(temp_vertices[12].y, 54.0f);
     ASSERT_EQ(temp_UVs[12].x, 0.5f);
     ASSERT_EQ(temp_UVs[12].y, 0.5f);
+
+    std::vector<glm::vec3> face_normal_vector_vec3;
+    bool is_bilinear_interpolation_in_use = true;
+    bool is_southwest_northeast_edges_in_use = false;
+    bool is_southeast_northwest_edges_in_use = false;
+
+    ASSERT_TRUE(geometry::compute_face_normals(
+                temp_vertices,
+                face_normal_vector_vec3,
+                image_width,
+                image_height,
+                is_bilinear_interpolation_in_use,
+                is_southwest_northeast_edges_in_use,
+                is_southeast_northwest_edges_in_use));
+
+    // Predefined vertices.
+    glm::vec3 i0_x0_z0 = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 i1_x1_z0 = glm::vec3(1.0f, 1.0f, 0.0f);
+    glm::vec3 i2_x2_z0 = glm::vec3(2.0f, 2.0f, 0.0f);
+    glm::vec3 i3_x0_z1 = glm::vec3(0.0f, 4.0f, 1.0f);
+    glm::vec3 i4_x1_z1 = glm::vec3(1.0f, 8.0f, 1.0f);
+    glm::vec3 i5_x2_z1 = glm::vec3(2.0f, 16.0f, 1.0f);
+    glm::vec3 i6_x0_z2 = glm::vec3(0.0f, 32.0f, 2.0f);
+    glm::vec3 i7_x1_z2 = glm::vec3(1.0f, 64.0f, 2.0f);
+    glm::vec3 i8_x2_z2 = glm::vec3(2.0f, 128.0f, 2.0f);
+
+    // Interpolated vertices.
+    glm::vec3 i9_x0_5_z0_5 = glm::vec3(0.5f, 3.25f, 0.5f);
+    glm::vec3 i10_x1_5_z0_5 = glm::vec3(1.5f, 6.75f, 0.5f);
+    glm::vec3 i11_x0_5_z1_5 = glm::vec3(0.5f, 27.0f, 1.5f);
+    glm::vec3 i12_x1_5_z1_5 = glm::vec3(1.5f, 54.0f, 1.5f);
+
+    // Predefined vertices.
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[0], i0_x0_z0)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[1], i1_x1_z0)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[2], i2_x2_z0)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[3], i3_x0_z1)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[4], i4_x1_z1)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[5], i5_x2_z1)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[6], i6_x0_z2)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[7], i7_x1_z2)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[8], i8_x2_z2)));
+
+    // Interpolated vertices.
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[9], i9_x0_5_z0_5)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[10], i10_x1_5_z0_5)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[11], i11_x0_5_z1_5)));
+    ASSERT_TRUE(glm::all(glm::equal(temp_vertices[12], i12_x1_5_z1_5)));
+
+    // Face normals.
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[0], glm::normalize(glm::cross(i1_x1_z0 - i9_x0_5_z0_5, i0_x0_z0 - i9_x0_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[1], glm::normalize(glm::cross(i0_x0_z0 - i9_x0_5_z0_5, i3_x0_z1 - i9_x0_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[2], glm::normalize(glm::cross(i3_x0_z1 - i9_x0_5_z0_5, i4_x1_z1 - i9_x0_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[3], glm::normalize(glm::cross(i4_x1_z1 - i9_x0_5_z0_5, i1_x1_z0 - i9_x0_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[4], glm::normalize(glm::cross(i2_x2_z0 - i10_x1_5_z0_5, i1_x1_z0 - i10_x1_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[5], glm::normalize(glm::cross(i1_x1_z0 - i10_x1_5_z0_5, i4_x1_z1 - i10_x1_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[6], glm::normalize(glm::cross(i4_x1_z1 - i10_x1_5_z0_5, i5_x2_z1 - i10_x1_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[7], glm::normalize(glm::cross(i5_x2_z1 - i10_x1_5_z0_5, i2_x2_z0 - i10_x1_5_z0_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[8], glm::normalize(glm::cross(i4_x1_z1 - i11_x0_5_z1_5, i3_x0_z1 - i11_x0_5_z1_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[9], glm::normalize(glm::cross(i3_x0_z1 - i11_x0_5_z1_5, i6_x0_z2 - i11_x0_5_z1_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[10], glm::normalize(glm::cross(i6_x0_z2 - i11_x0_5_z1_5, i7_x1_z2 - i11_x0_5_z1_5)))));
+    ASSERT_TRUE(glm::all(glm::equal(face_normal_vector_vec3[11], glm::normalize(glm::cross(i7_x1_z2 - i11_x0_5_z1_5, i4_x1_z1 - i11_x0_5_z1_5)))));
 }
 TEST(face_indices_must_be_computed_appropriately, a_4x4_world)
 {
