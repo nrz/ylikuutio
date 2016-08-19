1bcdd9500d69c8c9fbf621a014444e1314a86af3
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-19 10:10:06 +0300

    `class Chunk` inherits `ontology::Model`.
    All tests pass.
    
    	modified:   code/ylikuutio/space_partition/chunk.hpp

--- code/ylikuutio/space_partition/chunk.hpp
+++ code/ylikuutio/space_partition/chunk.hpp
@@ -1,9 +1,11 @@
 #ifndef __CHUNK_HPP_INCLUDED
 #define __CHUNK_HPP_INCLUDED
 
+#include "code/ylikuutio/ontology/model.hpp"
+
 namespace space_partition
 {
-    class Chunk
+    class Chunk: ontology::Model
     {
         public:
             // constructor.
