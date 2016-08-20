5a0475a22ca1410b987c86e2da2cceb1dcc08e25
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-20 09:53:34 +0300

    Operator overload for `bool operator==` for `Matrix` objects.
    	modified:   code/ylikuutio/linear_algebra/matrix.cpp
    	modified:   code/ylikuutio/linear_algebra/matrix.hpp

--- code/ylikuutio/linear_algebra/matrix.cpp
+++ code/ylikuutio/linear_algebra/matrix.cpp
@@ -26,4 +26,75 @@ namespace linear_algebra
     Matrix::~Matrix()
     {
     }
+
+    bool operator==(const Matrix& lhs, const Matrix& rhs)
+    {
+        // compare if matrices are equal.
+        if (lhs.width != rhs.width ||
+                lhs.height != rhs.height)
+        {
+            // Matrices are not equal, if they have different sizes.
+            return false;
+        }
+
+        if (!lhs.is_square || !rhs.is_square)
+        {
+            // TODO: implement non-square matrices, if needed.
+            return false;
+        }
+
+        if (lhs.square_size != rhs.square_size)
+        {
+            // If square sizes are different, square matrices are different.
+            return false;
+        }
+
+        switch (lhs.square_size)
+        {
+            case 1:
+                // Scalar (1x1 matrix).
+                return (lhs.array_1x1[0][0] == rhs.array_1x1[0][0]);
+            case 2:
+                // 2x2 matrix.
+                for (uint32_t y = 0; y < lhs.height; y++)
+                {
+                    for (uint32_t x = 0; y < lhs.height; y++)
+                    {
+                        if (lhs.array_2x2[y][x] != rhs.array_2x2[y][x])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            case 3:
+                // 3x3 matrix.
+                for (uint32_t y = 0; y < lhs.height; y++)
+                {
+                    for (uint32_t x = 0; y < lhs.height; y++)
+                    {
+                        if (lhs.array_3x3[y][x] != rhs.array_3x3[y][x])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            case 4:
+                // 4x4 matrix.
+                for (uint32_t y = 0; y < lhs.height; y++)
+                {
+                    for (uint32_t x = 0; y < lhs.height; y++)
+                    {
+                        if (lhs.array_4x4[y][x] != rhs.array_4x4[y][x])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
 }
--- code/ylikuutio/linear_algebra/matrix.hpp
+++ code/ylikuutio/linear_algebra/matrix.hpp
@@ -15,13 +15,21 @@ namespace linear_algebra
             // destructor.
             ~Matrix();
 
+            friend bool operator==(const Matrix& lhs, const Matrix& rhs);
+
         private:
             bool is_square;
             uint32_t square_size;
             uint32_t width;
             uint32_t height;
 
+            float array_1x1[1][1];
+            float array_2x2[2][2];
+            float array_3x3[3][3];
+            float array_4x4[4][4];
     };
+
+    bool operator==(const Matrix& lhs, const Matrix& rhs);
 }
 
 #endif
