0a27129f12d50960c32a37ae27b6368da1ba6f90
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-21 15:26:19 +0300

    Added member variable `ontology::Material* parent_pointer`.
    	modified:   code/ylikuutio/space_partition/triangle3D.hpp

--- code/ylikuutio/space_partition/triangle3D.hpp
+++ code/ylikuutio/space_partition/triangle3D.hpp
@@ -1,10 +1,15 @@
 #ifndef __TRIANGLE3D_HPP_INCLUDED
 #define __TRIANGLE3D_HPP_INCLUDED
 
+#include "code/ylikuutio/ontology/material.hpp"
+
 namespace space_partition
 {
     class Triangle3D
     {
+        public:
+        private:
+            ontology::Material* parent_pointer;      // pointer to the material.
     };
 }
 
