666da359da1a2ba635b170668c2bd97735114517
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-21 16:37:00 +0300

    Added a comment explaining `Chunk` and `Triangle` objects.
    	modified:   code/ylikuutio/space_partition/chunk.hpp

--- code/ylikuutio/space_partition/chunk.hpp
+++ code/ylikuutio/space_partition/chunk.hpp
@@ -3,6 +3,27 @@
 
 #include "code/ylikuutio/ontology/model.hpp"
 
+// Each `Chunk` contains `Triangle` objects.
+// The `Chunk` in which a given `Triangle` belongs to is based on either
+// on its center or its centroid. Each `Triangle` belongs to exactly one
+// `Chunk`. Moving the vertices of the `Triangle` may move it to some
+// other `Chunk` (to an adjacent `Chunk` or to a farther `Chunk`), as
+// moving vertices moves the centroid and the center of the `Triangle`.
+//
+// The content of any `Chunk` can be based either on some predefined
+// data (eg. height map data, laser scanning data etc.) or procedurally
+// (eg. Perlin noise or some other fractal). The parent of `Chunk` objects
+// is a `ChunkMaster` object, that has a callback for retrieving or
+// producing the content, and also the code for dividing the content into
+// appropriate `Chunk` objects.
+//
+// Centroid:
+//
+// Center:
+//
+// From rendering point of view, `Chunk` is like `Species`, with the
+// exception that `Chunk` normally contains only 1 `Object`.
+
 namespace space_partition
 {
     class Chunk: ontology::Model
