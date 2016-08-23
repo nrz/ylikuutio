6be669f0f8ff285fd6999a489a59e9f7447673b1
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-23 10:41:59 +0300

    Code for `class Triangle3D`.
    	modified:   code/ylikuutio/space_partition/triangle3D.cpp
    	modified:   code/ylikuutio/space_partition/triangle3D.hpp

--- code/ylikuutio/space_partition/triangle3D.cpp
+++ code/ylikuutio/space_partition/triangle3D.cpp
@@ -2,4 +2,18 @@
 
 namespace space_partition
 {
+    void Triangle3D::bind_to_parent()
+    {
+        hierarchy::bind_child_to_parent<space_partition::Triangle3D*>(this, this->parent_pointer->triangle3D_pointer_vector, this->parent_pointer->free_triangle3D_ID_queue);
+    }
+
+    Triangle3D::Triangle3D()
+    {
+        // constructor.
+    }
+
+    Triangle3D::~Triangle3D()
+    {
+        // destructor.
+    }
 }
