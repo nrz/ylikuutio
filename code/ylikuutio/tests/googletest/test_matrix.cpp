d960f9d08bfe284f611be2ec2c333bb315859af4
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-24 11:36:00 +0300

    Edited tests.
    All tests pass.
    
    	modified:   code/ylikuutio/tests/googletest/test_matrix.cpp

--- code/ylikuutio/tests/googletest/test_matrix.cpp
+++ code/ylikuutio/tests/googletest/test_matrix.cpp
@@ -50,7 +50,7 @@ TEST(matrices_must_function_as_expected, matrices)
     int32_t det_magic3x3 = magic3x3.det();
     ASSERT_EQ(det_magic3x3, -360);
 
-    // matrix equality.
+    // matrix equality, increment, and decrement.
 
     ASSERT_FALSE(zeros3x3 == ones3x3);
     zeros3x3++;
@@ -62,10 +62,62 @@ TEST(matrices_must_function_as_expected, matrices)
     ones3x3++;
     ASSERT_FALSE(zeros3x3 == ones3x3);
 
-    // matrix addition.
+    // copy constructor.
 
-    linear_algebra::Matrix magic3x3_plus_ones3x3(magic3x3);
-    magic3x3_plus_ones3x3 += ones3x3;
+    linear_algebra::Matrix magic3x3_plus_ones3x3_with_augmented_assignment(magic3x3);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[0][0], 8);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[1][0], 3);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[2][0], 4);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[0][1], 1);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[1][1], 5);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[2][1], 9);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[0][2], 6);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[1][2], 7);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[2][2], 2);
+
+    // copy constructor.
+
+    linear_algebra::Matrix magic3x3_plus_magic3x3_with_augmented_assignment(magic3x3);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[0][0], 8);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[1][0], 3);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[2][0], 4);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[0][1], 1);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[1][1], 5);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[2][1], 9);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[0][2], 6);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[1][2], 7);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[2][2], 2);
+
+    // matrix addition (augmented assignment).
+
+    magic3x3_plus_ones3x3_with_augmented_assignment += ones3x3;
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[0][0], 9);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[1][0], 4);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[2][0], 5);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[0][1], 2);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[1][1], 6);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[2][1], 10);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[0][2], 7);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[1][2], 8);
+    ASSERT_EQ(magic3x3_plus_ones3x3_with_augmented_assignment[2][2], 3);
+
+    // matrix addition (augmented assignment).
+
+    magic3x3_plus_magic3x3_with_augmented_assignment += magic3x3;
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[0][0], 16);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[1][0], 6);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[2][0], 8);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[0][1], 2);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[1][1], 10);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[2][1], 18);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[0][2], 12);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[1][2], 14);
+    ASSERT_EQ(magic3x3_plus_magic3x3_with_augmented_assignment[2][2], 4);
+
+    // matrix addition (binary addition).
+
+    linear_algebra::Matrix magic3x3_plus_ones3x3(3, 3);
+    magic3x3_plus_ones3x3 = magic3x3 + ones3x3;
     ASSERT_EQ(magic3x3_plus_ones3x3[0][0], 9);
     ASSERT_EQ(magic3x3_plus_ones3x3[1][0], 4);
     ASSERT_EQ(magic3x3_plus_ones3x3[2][0], 5);
@@ -76,8 +128,10 @@ TEST(matrices_must_function_as_expected, matrices)
     ASSERT_EQ(magic3x3_plus_ones3x3[1][2], 8);
     ASSERT_EQ(magic3x3_plus_ones3x3[2][2], 3);
 
-    linear_algebra::Matrix magic3x3_plus_magic3x3(magic3x3);
-    magic3x3_plus_magic3x3 += magic3x3;
+    // matrix addition (binary addition).
+
+    linear_algebra::Matrix magic3x3_plus_magic3x3(3, 3);
+    magic3x3_plus_magic3x3 = magic3x3 + magic3x3;
     ASSERT_EQ(magic3x3_plus_magic3x3[0][0], 16);
     ASSERT_EQ(magic3x3_plus_magic3x3[1][0], 6);
     ASSERT_EQ(magic3x3_plus_magic3x3[2][0], 8);
@@ -102,9 +156,10 @@ TEST(matrices_must_function_as_expected, matrices)
     ASSERT_EQ(ones3x3squared[1][2], 3);
     ASSERT_EQ(ones3x3squared[2][2], 3);
 
+    // matrix multiplication.
+
     linear_algebra::Matrix magic3x3squared(3, 3);
     magic3x3squared = magic3x3 * magic3x3;
-
     ASSERT_EQ(magic3x3squared[0][0], 91);
     ASSERT_EQ(magic3x3squared[1][0], 67);
     ASSERT_EQ(magic3x3squared[2][0], 67);
