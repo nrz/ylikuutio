68899f16f4f0f1ecfbd6a1d93b0c2dd5f1f5242b
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-25 09:30:44 +0300

    `Tensor3::Tensor3(uint32_t height, uint32_t width, uint32_t depth)`.
    Constructor is now implemented.
    
    All tests pass.
    
    	modified:   code/ylikuutio/linear_algebra/tensor3.cpp

--- code/ylikuutio/linear_algebra/tensor3.cpp
+++ code/ylikuutio/linear_algebra/tensor3.cpp
@@ -6,12 +6,43 @@
 
 namespace linear_algebra
 {
+    // `class Tensor3` uses the same coordinate order as MATLAB,
+    // to make testing easier. So: y, x, z.
+    // y = 0 is the uppermost slice
+    // x = 0 is the leftmost slice.
+    // z = 0 is the front slice.
+
     Tensor3::Tensor3(uint32_t height, uint32_t width, uint32_t depth)
     {
         // constructor.
         this->width = width;
         this->height = height;
         this->depth = depth;
+        this->array_of_arrays_of_arrays = new float**[this->height];
+
+        for (uint32_t y = 0; y < this->height; y++)
+        {
+            this->array_of_arrays_of_arrays[y] = new float*[this->width];
+
+            for (uint32_t x = 0; x < this->width; x++)
+            {
+                this->array_of_arrays_of_arrays[y][x] = new float[this->depth];
+            }
+        }
+
+        this->next_x_to_populate = 0;
+        this->next_y_to_populate = 0;
+        this->next_z_to_populate = 0;
+        this->is_fully_populated = false;
+
+        if (this->width == this->height && this->height == this->depth)
+        {
+            this->is_cube = true;
+        }
+        else
+        {
+            this->is_cube = false;
+        }
     }
 
     Tensor3::Tensor3(Tensor3& old_tensor3)
