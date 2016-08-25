0554fc2ff4bb23626957cd2d05cf1fac4ddb8dd7
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-08-25 09:35:22 +0300

    Bugfix: `void Tensor3::operator<<(const std::vector<float>& rhs)`.
    Populating `Tensor3` with a `std::vector<float>` should work now.
    
    All tests pass.
    
    	modified:   code/ylikuutio/linear_algebra/tensor3.cpp

--- code/ylikuutio/linear_algebra/tensor3.cpp
+++ code/ylikuutio/linear_algebra/tensor3.cpp
@@ -144,33 +144,29 @@ namespace linear_algebra
     {
         uint32_t rhs_i = 0;
 
-        if (this->is_fully_populated && rhs_i < rhs.size())
+        while (!this->is_fully_populated && rhs_i < rhs.size())
         {
-            // Array is already fully populated. Nothing to do.
-            return;
-        }
+            // First, get the slice.
+            float** my_array_of_arrays = this->array_of_arrays_of_arrays[this->next_y_to_populate];
+            float* my_array = my_array_of_arrays[this->next_x_to_populate];
 
-        // First, get the slice.
-        float** my_array_of_arrays = this->array_of_arrays_of_arrays[this->next_y_to_populate];
-        float* my_array = my_array_of_arrays[this->next_x_to_populate];
+            // Then store the value.
+            my_array[this->next_z_to_populate] = rhs.at(rhs_i++);
 
-        // Then store the value.
-        my_array[this->next_z_to_populate] = rhs.at(rhs_i++);
-
-        if (++this->next_x_to_populate >= this->width)
-        {
-            this->next_x_to_populate = 0;
-
-            if (++this->next_y_to_populate >= this->height)
+            if (++this->next_x_to_populate >= this->width)
             {
-                this->next_y_to_populate = 0;
+                this->next_x_to_populate = 0;
 
-                if (++this->next_z_to_populate >= this->depth)
+                if (++this->next_y_to_populate >= this->height)
                 {
-                    this->is_fully_populated = true;
+                    this->next_y_to_populate = 0;
+
+                    if (++this->next_z_to_populate >= this->depth)
+                    {
+                        this->is_fully_populated = true;
+                    }
                 }
             }
         }
     }
-
 }
