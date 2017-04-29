42d38f59cf7df6d0a774cdd3864f5cc3255974d0
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-04-29 19:19:30 +0300

    Renamed variables.
    `image_width` -> `actual_image_width`,
    `image_height` -> `actual_image_height`.
    
    All tests pass.
    
    	modified:   code/ylikuutio/triangulation/face_normals.cpp

--- code/ylikuutio/triangulation/face_normals.cpp
+++ code/ylikuutio/triangulation/face_normals.cpp
@@ -17,21 +17,21 @@ namespace geometry
     bool compute_face_normals(
             std::vector<glm::vec3>& temp_vertices,
             std::vector<glm::vec3>& face_normal_vector_vec3,
-            uint32_t image_width,
-            uint32_t image_height,
+            uint32_t actual_image_width,
+            uint32_t actual_image_height,
             uint32_t x_step,
             uint32_t z_step,
             bool is_bilinear_interpolation_in_use,
             bool is_southwest_northeast_edges_in_use,
             bool is_southeast_northwest_edges_in_use)
     {
-        uint32_t current_interpolated_vertex_i = image_width * image_height;
+        uint32_t current_interpolated_vertex_i = actual_image_width * actual_image_height;
 
-        for (uint32_t z = 1; z < image_height; z++)
+        for (uint32_t z = 1; z < actual_image_height; z++)
         {
-            for (uint32_t x = 1; x < image_width; x++)
+            for (uint32_t x = 1; x < actual_image_width; x++)
             {
-                uint32_t current_vertex_i = image_width * z + x;
+                uint32_t current_vertex_i = actual_image_width * z + x;
 
                 // Computing of face normals depends on triangulation type.
                 if (is_bilinear_interpolation_in_use)
@@ -41,26 +41,26 @@ namespace geometry
                     glm::vec3 face_normal;
 
                     // Compute the normal of S face.
-                    edge1 = temp_vertices[southeast(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
-                    edge2 = temp_vertices[southwest(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge1 = temp_vertices[southeast(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge2 = temp_vertices[southwest(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
 
                     // Compute the normal of W face.
-                    edge1 = temp_vertices[southwest(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
-                    edge2 = temp_vertices[northwest(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge1 = temp_vertices[southwest(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge2 = temp_vertices[northwest(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
 
                     // Compute the normal of N face.
-                    edge1 = temp_vertices[northwest(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
-                    edge2 = temp_vertices[northeast(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge1 = temp_vertices[northwest(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge2 = temp_vertices[northeast(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
 
                     // Compute the normal of E face.
-                    edge1 = temp_vertices[northeast(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
-                    edge2 = temp_vertices[southeast(current_vertex_i, image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge1 = temp_vertices[northeast(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
+                    edge2 = temp_vertices[southeast(current_vertex_i, actual_image_width)] - temp_vertices[current_interpolated_vertex_i];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
                 }
@@ -71,14 +71,14 @@ namespace geometry
                     glm::vec3 face_normal;
 
                     // Compute the normal of SE face.
-                    edge1 = temp_vertices[southwest(current_vertex_i, image_width)] - temp_vertices[southeast(current_vertex_i, image_width)];
-                    edge2 = temp_vertices[northeast(current_vertex_i, image_width)] - temp_vertices[southeast(current_vertex_i, image_width)];
+                    edge1 = temp_vertices[southwest(current_vertex_i, actual_image_width)] - temp_vertices[southeast(current_vertex_i, actual_image_width)];
+                    edge2 = temp_vertices[northeast(current_vertex_i, actual_image_width)] - temp_vertices[southeast(current_vertex_i, actual_image_width)];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
 
                     // Compute the normal of NW face.
-                    edge1 = temp_vertices[northeast(current_vertex_i, image_width)] - temp_vertices[northwest(current_vertex_i, image_width)];
-                    edge2 = temp_vertices[southwest(current_vertex_i, image_width)] - temp_vertices[northwest(current_vertex_i, image_width)];
+                    edge1 = temp_vertices[northeast(current_vertex_i, actual_image_width)] - temp_vertices[northwest(current_vertex_i, actual_image_width)];
+                    edge2 = temp_vertices[southwest(current_vertex_i, actual_image_width)] - temp_vertices[northwest(current_vertex_i, actual_image_width)];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
                 }
@@ -89,14 +89,14 @@ namespace geometry
                     glm::vec3 face_normal;
 
                     // Compute the normal of SW face.
-                    edge1 = temp_vertices[northwest(current_vertex_i, image_width)] - temp_vertices[southwest(current_vertex_i, image_width)];
-                    edge2 = temp_vertices[southeast(current_vertex_i, image_width)] - temp_vertices[southwest(current_vertex_i, image_width)];
+                    edge1 = temp_vertices[northwest(current_vertex_i, actual_image_width)] - temp_vertices[southwest(current_vertex_i, actual_image_width)];
+                    edge2 = temp_vertices[southeast(current_vertex_i, actual_image_width)] - temp_vertices[southwest(current_vertex_i, actual_image_width)];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
 
                     // Compute the normal of NE face.
-                    edge1 = temp_vertices[southeast(current_vertex_i, image_width)] - temp_vertices[northeast(current_vertex_i, image_width)];
-                    edge2 = temp_vertices[northwest(current_vertex_i, image_width)] - temp_vertices[northeast(current_vertex_i, image_width)];
+                    edge1 = temp_vertices[southeast(current_vertex_i, actual_image_width)] - temp_vertices[northeast(current_vertex_i, actual_image_width)];
+                    edge2 = temp_vertices[northwest(current_vertex_i, actual_image_width)] - temp_vertices[northeast(current_vertex_i, actual_image_width)];
                     face_normal = glm::normalize(glm::cross(edge1, edge2));
                     face_normal_vector_vec3.push_back(face_normal);
                 }
