53f09d4066bd0d244cf63e08cb142ce9096afe18
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-05-26 17:55:50 +0300

    `typedef struct InfoStruct {` ... `} InfoStruct;`.
    All tests pass.
    
    	modified:   CMakeLists.txt
    	new file:   code/ylikuutio/ontology/info_struct.hpp

---
+++ code/ylikuutio/ontology/info_struct.hpp
@@ -0,0 +1,34 @@
+#ifndef __INFO_STRUCT_HPP_INCLUDED
+#define __INFO_STRUCT_HPP_INCLUDED
+
+#include "code/ylikuutio/common/any_value.hpp"
+
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
+// Include standard headers
+#include <stdint.h> // uint32_t etc.
+#include <string>   // std::string
+
+namespace ontology
+{
+    class Scene;
+}
+
+typedef struct InfoStruct
+{
+    InfoStruct()
+        : memory_address(nullptr), childID(-1), name(""), coordinates(glm::vec3(NAN, NAN, NAN))
+    {
+        // constructor.
+    }
+    datatypes::AnyValue* memory_address;
+    uint32_t childID;
+    std::string name;
+    glm::vec3 coordinates;
+} InfoStruct;
+
+#endif
