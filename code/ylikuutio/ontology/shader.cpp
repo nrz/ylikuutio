e3a4747057c047d272cb07eb6489130592204673
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-06-14 00:21:47 +0300

    Bugfix: added missing `#include` lines.
    All tests pass.
    
    	modified:   code/ylikuutio/ontology/shader.cpp

--- code/ylikuutio/ontology/shader.cpp
+++ code/ylikuutio/ontology/shader.cpp
@@ -7,6 +7,18 @@
 #include "code/ylikuutio/hierarchy/hierarchy_templates.hpp"
 #include "code/ylikuutio/common/global_variables.hpp"
 
+// Include GLEW
+#ifndef __GL_GLEW_H_INCLUDED
+#define __GL_GLEW_H_INCLUDED
+#include <GL/glew.h> // GLfloat, GLuint etc.
+#endif
+
+// Include GLFW
+#ifndef __GLFW3_H_INCLUDED
+#define __GLFW3_H_INCLUDED
+#include <glfw3.h>
+#endif
+
 // Include standard headers
 #include <iostream> // std::cout, std::cin, std::cerr
 #include <stdint.h> // uint32_t etc.
