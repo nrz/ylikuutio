8a3ad2eb4df612ef9a4a39542d6ca66406f515d3
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-06-23 01:21:01 +0300

    `load_ASCII_grid`: `ASCII_grid_file_name` is `const std::string`.
    All tests pass.
    
    	modified:   code/ylikuutio/loaders/ascii_grid_loader.cpp
    	modified:   code/ylikuutio/loaders/ascii_grid_loader.hpp

--- code/ylikuutio/loaders/ascii_grid_loader.hpp
+++ code/ylikuutio/loaders/ascii_grid_loader.hpp
@@ -15,7 +15,7 @@
 namespace loaders
 {
     bool load_ASCII_grid(
-            std::string ASCII_grid_file_name,
+            const std::string ASCII_grid_file_name,
             std::vector<glm::vec3>& out_vertices,
             std::vector<glm::vec2>& out_UVs,
             std::vector<glm::vec3>& out_normals,
