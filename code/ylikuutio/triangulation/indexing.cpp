6fad3b39e4e7fab82ca709912bd0b1ad5e8a7ef6
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-06-22 21:06:46 +0300

    Converted variable types from `uint32_t` to `int32_t`.
    All tests pass.
    
    	modified:   code/ylikuutio/triangulation/indexing.cpp
    	modified:   code/ylikuutio/triangulation/indexing.hpp

--- code/ylikuutio/triangulation/indexing.cpp
+++ code/ylikuutio/triangulation/indexing.cpp
@@ -10,29 +10,29 @@ namespace geometry
 {
     // For bilinear interpolation, southeast-northwest edges, and southwest-northeast edges.
     // These functions return indices.
-    GLuint southwest(uint32_t current_vertex_i, uint32_t image_width)
+    GLuint southwest(int32_t current_vertex_i, int32_t image_width)
     {
         return current_vertex_i - image_width - 1;
     }
-    GLuint southeast(uint32_t current_vertex_i, uint32_t image_width)
+    GLuint southeast(int32_t current_vertex_i, int32_t image_width)
     {
         return current_vertex_i - image_width;
     }
-    GLuint northwest(uint32_t current_vertex_i, uint32_t image_width)
+    GLuint northwest(int32_t current_vertex_i, int32_t image_width)
     {
         return current_vertex_i - 1;
     }
-    GLuint northeast(uint32_t current_vertex_i, uint32_t image_width)
+    GLuint northeast(int32_t current_vertex_i, int32_t image_width)
     {
         return current_vertex_i;
     }
 
     // for bilinear interpolation.
     int32_t get_face_normal_i(
-            uint32_t x,
-            uint32_t z,
+            int32_t x,
+            int32_t z,
             BilinearDirections compass_point_code,
-            uint32_t actual_image_width)
+            int32_t actual_image_width)
     {
         // Face indices example for a 4x4 image file using bilinear interpolation.
         //
