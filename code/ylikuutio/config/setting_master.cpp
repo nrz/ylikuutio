nrz/ylikuutio

commit fbfafe6e0a3c289877f3cce5e1e5f9c427de3597

Coordinates `x`, `y`, `z` can now be `set` and `get` in console.
If a `read_callback` of `Setting` is not `nullptr`, then the `Setting`
will be read using that `read_callback`, otherwise directly from
`setting_value`. This is needed for variables that need to be updated
the fastest way possible, eg. `x`, `y`, and `z`.

All tests pass.

	modified:   CMakeLists.txt
	modified:   code/ajokki/ajokki.cpp
	new file:   code/ajokki/ajokki_debug.cpp
	new file:   code/ajokki/ajokki_debug.hpp
	modified:   code/ajokki/ajokki_keyboard_callbacks.cpp
	new file:   code/ajokki/ajokki_location_and_orientation.cpp
	new file:   code/ajokki/ajokki_location_and_orientation.hpp
	new file:   code/ajokki/ajokki_movement.cpp
	new file:   code/ajokki/ajokki_movement.hpp
	modified:   code/ylikuutio/config/setting_master.cpp
	modified:   code/ylikuutio/config/setting_master.hpp
	modified:   code/ylikuutio/ontology/ground_level.cpp
	modified:   code/ylikuutio/ontology/ground_level.hpp
	modified:   code/ylikuutio/ontology/model.hpp
	modified:   code/ylikuutio/ontology/scene.cpp
	modified:   code/ylikuutio/ontology/scene.hpp
	modified:   code/ylikuutio/ontology/species.hpp
	modified:   code/ylikuutio/ontology/universe.cpp
	modified:   code/ylikuutio/ontology/universe.hpp

code/ylikuutio/config/setting_master.cpp
@@ -79,16 +79,24 @@ namespace config
         return this->setting_pointer_map[setting_name];
     }
 
-    datatypes::AnyValue* SettingMaster::get_value(std::string& setting_name)
+    datatypes::AnyValue* SettingMaster::get_value(
+            ontology::Universe* const universe,
+            config::SettingMaster* const setting_master,
+            std::string& setting_name)
     {
-        config::Setting* setting = this->get(setting_name);
+        config::Setting* setting = setting_master->get(setting_name);
 
         if (setting == nullptr)
         {
             return nullptr;
         }
 
-        return setting->setting_value;
+        if (setting->read_callback == nullptr)
+        {
+            return new datatypes::AnyValue(setting->setting_value); // create a new `AnyValue`.
+        }
+
+        return setting->read_callback(universe, setting_master);
     }
 
     // public callbacks.
@@ -194,12 +202,13 @@ namespace config
             // Print valid values of the given variable.
             if (setting_master->is_setting(setting_name))
             {
-                config::Setting* setting = setting_master->setting_pointer_map[setting_name];
+                datatypes::AnyValue* setting_any_value = setting_master->get_value(universe, setting_master, setting_name);
 
-                if (setting != nullptr)
+                if (setting_any_value != nullptr)
                 {
                     // Print variable value.
-                    console->print_text(setting->setting_value->get_string());
+                    console->print_text(setting_any_value->get_string());
+                    delete setting_any_value;
                 }
             }
             else
@@ -308,6 +317,242 @@ namespace config
 
         glClearColor(red, green, blue, alpha);
         return nullptr;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_spherical_coordinates(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("spherical_coordinates") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* spherical_coordinates_any_value = setting_master_pointer->setting_pointer_map["spherical_coordinates"]->setting_value;
+
+        if (spherical_coordinates_any_value == nullptr || spherical_coordinates_any_value->type != datatypes::GLM_VEC3_POINTER)
+        {
+            return nullptr;
+        }
+
+        delete universe->spherical_coordinates; // delete the old `SphericalCoordinatesStruct`.
+        universe->spherical_coordinates = spherical_coordinates_any_value->spherical_coordinates_struct_pointer;
+        return nullptr;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_rho(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->spherical_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("spherical_coordinates") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* rho_any_value = setting_master_pointer->setting_pointer_map["rho"]->setting_value;
+
+        if (rho_any_value == nullptr || rho_any_value->type != datatypes::FLOAT)
+        {
+            return nullptr;
+        }
+
+        universe->spherical_coordinates->rho = rho_any_value->float_value;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_theta(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->spherical_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("spherical_coordinates") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* theta_any_value = setting_master_pointer->setting_pointer_map["theta"]->setting_value;
+
+        if (theta_any_value == nullptr || theta_any_value->type != datatypes::FLOAT)
+        {
+            return nullptr;
+        }
+
+        universe->spherical_coordinates->theta = theta_any_value->float_value;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_phi(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->spherical_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("spherical_coordinates") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* phi_any_value = setting_master_pointer->setting_pointer_map["phi"]->setting_value;
+
+        if (phi_any_value == nullptr || phi_any_value->type != datatypes::FLOAT)
+        {
+            return nullptr;
+        }
+
+        universe->spherical_coordinates->phi = phi_any_value->float_value;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_cartesian_coordinates(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("cartesian_coordinates") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* cartesian_coordinates_any_value = setting_master_pointer->setting_pointer_map["cartesian_coordinates"]->setting_value;
+
+        if (cartesian_coordinates_any_value == nullptr || cartesian_coordinates_any_value->type != datatypes::GLM_VEC3_POINTER)
+        {
+            return nullptr;
+        }
+
+        delete universe->cartesian_coordinates; // delete the old `glm::vec3`.
+        universe->cartesian_coordinates = cartesian_coordinates_any_value->glm_vec3_pointer;
+        return nullptr;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_x(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->cartesian_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("x") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* x_any_value = setting_master_pointer->setting_pointer_map["x"]->setting_value;
+
+        if (x_any_value == nullptr || x_any_value->type != datatypes::FLOAT)
+        {
+            return nullptr;
+        }
+
+        universe->cartesian_coordinates->x = x_any_value->float_value;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_y(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->cartesian_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("y") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* y_any_value = setting_master_pointer->setting_pointer_map["y"]->setting_value;
+
+        if (y_any_value == nullptr || y_any_value->type != datatypes::FLOAT)
+        {
+            return nullptr;
+        }
+
+        universe->cartesian_coordinates->y = y_any_value->float_value;
+    }
+
+    datatypes::AnyValue* SettingMaster::activate_z(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->cartesian_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        SettingMaster* setting_master_pointer = universe->setting_master_pointer;
+
+        if (setting_master_pointer == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (setting_master_pointer->setting_pointer_map.count("z") != 1)
+        {
+            return nullptr;
+        }
+
+        datatypes::AnyValue* z_any_value = setting_master_pointer->setting_pointer_map["z"]->setting_value;
+
+        if (z_any_value == nullptr || z_any_value->type != datatypes::FLOAT)
+        {
+            return nullptr;
+        }
+
+        universe->cartesian_coordinates->z = z_any_value->float_value;
     }
 
     datatypes::AnyValue* SettingMaster::activate_horizontal_angle(ontology::Universe* const universe, config::SettingMaster* const setting_master)
@@ -546,5 +791,65 @@ namespace config
         }
 
         universe->testing_spherical_world_in_use = testing_spherical_world_in_use_any_value->bool_value;
+    }
+
+    datatypes::AnyValue* SettingMaster::read_x(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->cartesian_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        return new datatypes::AnyValue(universe->cartesian_coordinates->x);
+    }
+
+    datatypes::AnyValue* SettingMaster::read_y(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->cartesian_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        return new datatypes::AnyValue(universe->cartesian_coordinates->y);
+    }
+
+    datatypes::AnyValue* SettingMaster::read_z(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->cartesian_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        return new datatypes::AnyValue(universe->cartesian_coordinates->z);
+    }
+
+    datatypes::AnyValue* SettingMaster::read_rho(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->spherical_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        return new datatypes::AnyValue(universe->spherical_coordinates->rho);
+    }
+
+    datatypes::AnyValue* SettingMaster::read_theta(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->spherical_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        return new datatypes::AnyValue(universe->spherical_coordinates->theta);
+    }
+
+    datatypes::AnyValue* SettingMaster::read_phi(ontology::Universe* const universe, config::SettingMaster* const setting_master)
+    {
+        if (universe == nullptr || universe->spherical_coordinates == nullptr)
+        {
+            return nullptr;
+        }
+
+        return new datatypes::AnyValue(universe->spherical_coordinates->phi);
     }
 }

CMakeLists.txt
@@ -627,8 +627,14 @@ add_executable(ajokki
     code/ajokki/ajokki_background_colors.hpp
     code/ajokki/ajokki_console_callbacks.cpp
     code/ajokki/ajokki_console_callbacks.hpp
+    code/ajokki/ajokki_debug.cpp
+    code/ajokki/ajokki_debug.hpp
     code/ajokki/ajokki_keyboard_callbacks.cpp
     code/ajokki/ajokki_keyboard_callbacks.hpp
+    code/ajokki/ajokki_location_and_orientation.cpp
+    code/ajokki/ajokki_location_and_orientation.hpp
+    code/ajokki/ajokki_movement.cpp
+    code/ajokki/ajokki_movement.hpp
     )
 add_dependencies(ajokki rescan)
 target_link_libraries(ajokki ylikuutio ${ALL_LIBS} glfw)

code/ajokki/ajokki.cpp
@@ -10,6 +10,9 @@
 #include "ajokki_background_colors.hpp"
 #include "ajokki_console_callbacks.hpp"
 #include "ajokki_keyboard_callbacks.hpp"
+#include "ajokki_debug.hpp"
+#include "ajokki_movement.hpp"
+#include "ajokki_location_and_orientation.hpp"
 #include "code/ylikuutio/callback_system/callback_parameter.hpp"
 #include "code/ylikuutio/callback_system/callback_object.hpp"
 #include "code/ylikuutio/callback_system/callback_engine.hpp"
@@ -121,25 +124,6 @@ int main(void)
     world_radius_setting_struct.should_ylikuutio_call_activate_callback_now = true;
     new config::Setting(world_radius_setting_struct);
 
-    // testing_spherical_world_in_use = true;
-
-    if (my_universe->testing_spherical_world_in_use)
-    {
-        my_universe->is_flight_mode_in_use = true;
-
-        my_universe->cartesian_coordinates = glm::vec3(-5682.32f, -1641.20f, 2376.45f);
-    }
-    else
-    {
-        my_universe->cartesian_coordinates = glm::vec3(100.0f, 100.0f, 100.0f);
-    }
-    // Initial horizontal angle : toward -Z
-    // horizontal_angle = 0.0f;
-    my_universe->horizontal_angle = 42.42f;
-    // Initial vertical angle : none
-    // vertical_angle = PI / 2;
-    my_universe->vertical_angle = 7.44f;
-
     callback_system::CallbackEngine* cleanup_callback_engine = new callback_system::CallbackEngine();
     callback_system::CallbackObject* cleanup_callback_object = new callback_system::CallbackObject(nullptr, cleanup_callback_engine);
 
@@ -193,6 +177,9 @@ int main(void)
     glEnable(GL_CULL_FACE);
 
     ajokki::set_background_colors(my_setting_master);
+    ajokki::set_movement(my_setting_master);
+    ajokki::set_location_and_orientation(my_setting_master);
+    ajokki::set_debug_variables(my_setting_master);
 
     ontology::Scene* my_scene = new ontology::Scene(my_universe);
 
@@ -971,96 +958,99 @@ int main(void)
             printing_struct.font_size = my_universe->get_font_size();
             printing_struct.char_font_texture_file_format = "bmp";
 
-            char angles_and_coordinates_text[256];
-            std::snprintf(
-                    angles_and_coordinates_text,
-                    sizeof(angles_and_coordinates_text),
-                    "%.2f,%.2f rad; %.2f,%.2f deg\\n(%.2f,%.2f,%.2f)",
-                    my_universe->horizontal_angle,
-                    my_universe->vertical_angle,
-                    RADIANS_TO_DEGREES(my_universe->horizontal_angle),
-                    RADIANS_TO_DEGREES(my_universe->vertical_angle),
-                    my_universe->cartesian_coordinates.x,
-                    my_universe->cartesian_coordinates.y,
-                    my_universe->cartesian_coordinates.z);
-
-            char time_text[256];
-            std::snprintf(time_text, sizeof(time_text), "%.2f sec", glfwGetTime());
-
-            char null_text[] = "";
-            char on_text[] = "on";
-            char off_text[] = "off";
-            char in_use_text[] = " (in use)";
-
-            char help_text_char[1024];
-            std::snprintf(
-                    help_text_char,
-                    sizeof(help_text_char),
-                    "Ajokki v. 0.0.1\\n"
-                    "\\n"
-                    "arrow keys\\n"
-                    "space jump\\n"
-                    "enter duck\\n"
-                    "F1 help mode\\n"
-                    "`  enter console\\n"
-                    "I  invert mouse (%s)\\n"
-                    "F  flight mode (%s)\\n"
-                    "Ctrl      turbo\\n"
-                    "Ctrl+Ctrl extra turbo\\n"
-                    "for debugging:\\n"
-                    "G  grass texture%s\\n"
-                    "U  uvmap texture%s\\n"
-                    "T  terrain species\\n"
-                    "A  suzanne species\\n",
-                    (my_universe->is_invert_mouse_in_use ? on_text : off_text),
-                    (my_universe->is_flight_mode_in_use ? on_text : off_text),
-                    (!does_suzanne_species_have_uvmap_texture ? in_use_text : null_text),
-                    (does_suzanne_species_have_uvmap_texture ? in_use_text : null_text));
-
-            char spherical_coordinates_text[256];
-
-            if (my_universe->testing_spherical_world_in_use)
+            if (my_universe != nullptr && my_universe->cartesian_coordinates != nullptr)
             {
-                std::snprintf(spherical_coordinates_text, sizeof(spherical_coordinates_text), "rho:%.2f theta:%.2f phi:%.2f", my_universe->spherical_coordinates->rho, my_universe->spherical_coordinates->theta, my_universe->spherical_coordinates->phi);
-            }
+                char angles_and_coordinates_text[256];
+                std::snprintf(
+                        angles_and_coordinates_text,
+                        sizeof(angles_and_coordinates_text),
+                        "%.2f,%.2f rad; %.2f,%.2f deg\\n(%.2f,%.2f,%.2f)",
+                        my_universe->horizontal_angle,
+                        my_universe->vertical_angle,
+                        RADIANS_TO_DEGREES(my_universe->horizontal_angle),
+                        RADIANS_TO_DEGREES(my_universe->vertical_angle),
+                        my_universe->cartesian_coordinates->x,
+                        my_universe->cartesian_coordinates->y,
+                        my_universe->cartesian_coordinates->z);
 
-            // print cartesian coordinates on bottom left corner.
-            printing_struct.x = 0;
-            printing_struct.y = 0;
-            printing_struct.text_char = angles_and_coordinates_text;
-            printing_struct.horizontal_alignment = "left";
-            printing_struct.vertical_alignment = "bottom";
-            my_font2D->printText2D(printing_struct);
+                char time_text[256];
+                std::snprintf(time_text, sizeof(time_text), "%.2f sec", glfwGetTime());
 
-            if (my_universe->in_help_mode && my_universe->can_display_help_screen)
-            {
-                // print help text.
+                char null_text[] = "";
+                char on_text[] = "on";
+                char off_text[] = "off";
+                char in_use_text[] = " (in use)";
+
+                char help_text_char[1024];
+                std::snprintf(
+                        help_text_char,
+                        sizeof(help_text_char),
+                        "Ajokki v. 0.0.1\\n"
+                        "\\n"
+                        "arrow keys\\n"
+                        "space jump\\n"
+                        "enter duck\\n"
+                        "F1 help mode\\n"
+                        "`  enter console\\n"
+                        "I  invert mouse (%s)\\n"
+                        "F  flight mode (%s)\\n"
+                        "Ctrl      turbo\\n"
+                        "Ctrl+Ctrl extra turbo\\n"
+                        "for debugging:\\n"
+                        "G  grass texture%s\\n"
+                        "U  uvmap texture%s\\n"
+                        "T  terrain species\\n"
+                        "A  suzanne species\\n",
+                        (my_universe->is_invert_mouse_in_use ? on_text : off_text),
+                        (my_universe->is_flight_mode_in_use ? on_text : off_text),
+                        (!does_suzanne_species_have_uvmap_texture ? in_use_text : null_text),
+                        (does_suzanne_species_have_uvmap_texture ? in_use_text : null_text));
+
+                char spherical_coordinates_text[256];
+
+                if (my_universe != nullptr && my_universe->testing_spherical_world_in_use)
+                {
+                    std::snprintf(spherical_coordinates_text, sizeof(spherical_coordinates_text), "rho:%.2f theta:%.2f phi:%.2f", my_universe->spherical_coordinates->rho, my_universe->spherical_coordinates->theta, my_universe->spherical_coordinates->phi);
+                }
+
+                // print cartesian coordinates on bottom left corner.
                 printing_struct.x = 0;
-                printing_struct.y = my_universe->get_window_height() - (3 * my_universe->get_text_size());
-                printing_struct.text_char = help_text_char;
+                printing_struct.y = 0;
+                printing_struct.text_char = angles_and_coordinates_text;
+                printing_struct.horizontal_alignment = "left";
+                printing_struct.vertical_alignment = "bottom";
+                my_font2D->printText2D(printing_struct);
+
+                if (my_universe != nullptr && my_universe->in_help_mode && my_universe->can_display_help_screen)
+                {
+                    // print help text.
+                    printing_struct.x = 0;
+                    printing_struct.y = my_universe->get_window_height() - (3 * my_universe->get_text_size());
+                    printing_struct.text_char = help_text_char;
+                    printing_struct.horizontal_alignment = "left";
+                    printing_struct.vertical_alignment = "top";
+                    my_font2D->printText2D(printing_struct);
+                }
+
+                if (my_universe != nullptr && my_universe->testing_spherical_world_in_use)
+                {
+                    // print spherical coordinates on bottom left corner.
+                    printing_struct.x = 0;
+                    printing_struct.y += 2 * my_universe->get_text_size();
+                    printing_struct.text_char = spherical_coordinates_text;
+                    printing_struct.horizontal_alignment = "left";
+                    printing_struct.vertical_alignment = "bottom";
+                    my_font2D->printText2D(printing_struct);
+                }
+
+                // print time data on top left corner.
+                printing_struct.x = 0;
+                printing_struct.y = static_cast<GLuint>(my_universe->get_window_height());
+                printing_struct.text_char = time_text;
                 printing_struct.horizontal_alignment = "left";
                 printing_struct.vertical_alignment = "top";
                 my_font2D->printText2D(printing_struct);
             }
-
-            if (my_universe->testing_spherical_world_in_use)
-            {
-                // print spherical coordinates on bottom left corner.
-                printing_struct.x = 0;
-                printing_struct.y += 2 * my_universe->get_text_size();
-                printing_struct.text_char = spherical_coordinates_text;
-                printing_struct.horizontal_alignment = "left";
-                printing_struct.vertical_alignment = "bottom";
-                my_font2D->printText2D(printing_struct);
-            }
-
-            // print time data on top left corner.
-            printing_struct.x = 0;
-            printing_struct.y = static_cast<GLuint>(my_universe->get_window_height());
-            printing_struct.text_char = time_text;
-            printing_struct.horizontal_alignment = "left";
-            printing_struct.vertical_alignment = "top";
-            my_font2D->printText2D(printing_struct);
 
             if (ms_frame_text_ready)
             {

code/ajokki/ajokki_debug.cpp
@@ -1,0 +1,30 @@
+#include "ajokki_debug.hpp"
+#include "code/ylikuutio/config/setting.hpp"
+#include "code/ylikuutio/config/setting_master.hpp"
+#include "code/ylikuutio/ontology/universe.hpp"
+#include "code/ylikuutio/common/any_value.hpp"
+#include "code/ylikuutio/common/globals.hpp"
+
+namespace ajokki
+{
+    void set_debug_variables(config::SettingMaster* setting_master)
+    {
+        bool is_flight_mode_in_use = true;
+        datatypes::AnyValue* any_value_is_flight_mode_in_use = new datatypes::AnyValue(is_flight_mode_in_use);
+        SettingStruct is_flight_mode_in_use_setting_struct(any_value_is_flight_mode_in_use);
+        is_flight_mode_in_use_setting_struct.name = "is_flight_mode_in_use";
+        is_flight_mode_in_use_setting_struct.setting_master_pointer = setting_master;
+        is_flight_mode_in_use_setting_struct.activate_callback = &config::SettingMaster::activate_is_flight_mode_in_use;
+        is_flight_mode_in_use_setting_struct.should_ylikuutio_call_activate_callback_now = false;
+        new config::Setting(is_flight_mode_in_use_setting_struct);
+
+        bool testing_spherical_world_in_use = true;
+        datatypes::AnyValue* any_value_testing_spherical_world_in_use = new datatypes::AnyValue(testing_spherical_world_in_use);
+        SettingStruct testing_spherical_world_in_use_setting_struct(any_value_testing_spherical_world_in_use);
+        testing_spherical_world_in_use_setting_struct.name = "testing_spherical_world_in_use";
+        testing_spherical_world_in_use_setting_struct.setting_master_pointer = setting_master;
+        testing_spherical_world_in_use_setting_struct.activate_callback =  &config::SettingMaster::activate_testing_spherical_world_in_use;
+        testing_spherical_world_in_use_setting_struct.should_ylikuutio_call_activate_callback_now = false;
+        new config::Setting(testing_spherical_world_in_use_setting_struct);
+    }
+}

commit 94ca038ea350a344c56b78eb9562b9a7d85674a7

Bugfix: reading values of `Setting`s without `read_callback` works.
This fixes a regression caused by commit
fbfafe6e0a3c289877f3cce5e1e5f9c427de3597 .

All tests pass.

	modified:   code/ylikuutio/config/setting_master.cpp

code/ylikuutio/config/setting_master.cpp
@@ -93,7 +93,12 @@ namespace config
 
         if (setting->read_callback == nullptr)
         {
-            return new datatypes::AnyValue(setting->setting_value); // create a new `AnyValue`.
+            if (setting_master->setting_pointer_map.count(setting_name) != 1)
+            {
+                return nullptr;
+            }
+
+            return new datatypes::AnyValue(setting_master->setting_pointer_map[setting_name]->setting_value); // create a new `AnyValue`.
         }
 
         return setting->read_callback(universe, setting_master);
@@ -166,7 +171,11 @@ namespace config
                 {
                     delete setting->setting_value; // delete the old value.
                     setting->setting_value = setting_any_value;
-                    setting->activate_callback(universe, setting_master);
+
+                    if (setting->activate_callback != nullptr)
+                    {
+                        setting->activate_callback(universe, setting_master);
+                    }
                 }
             }
             else
@@ -202,13 +211,22 @@ namespace config
             // Print valid values of the given variable.
             if (setting_master->is_setting(setting_name))
             {
-                datatypes::AnyValue* setting_any_value = setting_master->get_value(universe, setting_master, setting_name);
+                config::Setting* setting = setting_master->get(setting_name);
 
-                if (setting_any_value != nullptr)
+                if (setting != nullptr && setting->setting_value != nullptr && setting->read_callback == nullptr)
                 {
                     // Print variable value.
-                    console->print_text(setting_any_value->get_string());
-                    delete setting_any_value;
+                    console->print_text(setting->setting_value->get_string());
+                }
+                else if (setting != nullptr && setting->setting_value != nullptr)
+                {
+                    console->print_text(setting->read_callback(universe, setting_master)->get_string());
+                }
+                else
+                {
+                    // Invalid variable name.
+                    console->print_text("invalid variable name");
+                    console->print_text(setting_master->help());
                 }
             }
             else