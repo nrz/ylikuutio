ee660218ef1327ff1b63a3115706d95761808aa3
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-09-23 11:55:02 +0300

    Bugfix: added missing `const` modifiers.
    All tests pass with both GCC and Clang.
    
    	modified:   code/ylikuutio/common/any_value.cpp

--- code/ylikuutio/common/any_value.cpp
+++ code/ylikuutio/common/any_value.cpp
@@ -990,7 +990,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(float* float_pointer)
+    AnyValue::AnyValue(float* const float_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1010,7 +1010,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(double* double_pointer)
+    AnyValue::AnyValue(double* const double_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1030,7 +1030,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(int32_t* int32_t_pointer)
+    AnyValue::AnyValue(int32_t* const int32_t_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1050,7 +1050,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(uint32_t* uint32_t_pointer)
+    AnyValue::AnyValue(uint32_t* const uint32_t_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1070,7 +1070,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Universe* universe_pointer)
+    AnyValue::AnyValue(ontology::Universe* const universe_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1090,7 +1090,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Scene* scene_pointer)
+    AnyValue::AnyValue(ontology::Scene* const scene_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1110,7 +1110,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Shader* shader_pointer)
+    AnyValue::AnyValue(ontology::Shader* const shader_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1130,7 +1130,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Material* material_pointer)
+    AnyValue::AnyValue(ontology::Material* const material_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1150,7 +1150,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Species* species_pointer)
+    AnyValue::AnyValue(ontology::Species* const species_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1170,7 +1170,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Object* object_pointer)
+    AnyValue::AnyValue(ontology::Object* const object_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1190,7 +1190,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::VectorFont* vector_font_pointer)
+    AnyValue::AnyValue(ontology::VectorFont* const vector_font_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1210,7 +1210,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Glyph* glyph_pointer)
+    AnyValue::AnyValue(ontology::Glyph* const glyph_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1230,7 +1230,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Text3D* text3D_pointer)
+    AnyValue::AnyValue(ontology::Text3D* const text3D_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1250,7 +1250,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(ontology::Font2D* font2D_pointer)
+    AnyValue::AnyValue(ontology::Font2D* const font2D_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1270,7 +1270,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(console::Console* console_pointer)
+    AnyValue::AnyValue(console::Console* const console_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1290,7 +1290,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(SphericalCoordinatesStruct* spherical_coordinates_struct_pointer)
+    AnyValue::AnyValue(SphericalCoordinatesStruct* const spherical_coordinates_struct_pointer)
     {
         // constructor.
         this->set_default_values();
@@ -1310,7 +1310,7 @@ namespace datatypes
         }
     }
 
-    AnyValue::AnyValue(glm::vec3* glm_vec3_pointer)
+    AnyValue::AnyValue(glm::vec3* const glm_vec3_pointer)
     {
         // constructor.
         this->set_default_values();
