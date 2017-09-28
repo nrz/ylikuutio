6af6bd85a9f9e4b00ac2494e14577edc96842ea0
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-09-28 23:28:43 +0300

    Rewrite `TEST(file_must_be_slurped_appropriately, kongtext_svg)`.
    All tests pass with both GCC and Clang.
    
    	modified:   code/ylikuutio/tests/googletest/test_file_loader.cpp

--- code/ylikuutio/tests/googletest/test_file_loader.cpp
+++ code/ylikuutio/tests/googletest/test_file_loader.cpp
@@ -2,19 +2,18 @@
 #include "code/ylikuutio/file/file_loader.hpp"
 
 // Include standard headers
-#include <cstdio>  // std::FILE, std::fclose, std::fopen, std::fread, std::getchar, std::printf etc.
-#include <cstring> // std::memcmp, std::strcmp, std::strlen, std::strncmp
+#include <stdint.h> // uint32_t etc.
+#include <string>   // std::string
 
 TEST(file_must_be_slurped_appropriately, kongtext_svg)
 {
     std::string file_contents = file::slurp("kongtext.svg");
-    const char* file_contents_char = file_contents.c_str();
-    const char* first_line_of_kongtext_svg = "<?xml version=\"1.0\" standalone=\"no\"?>";
-    std::printf("%.*s\n", std::strlen(first_line_of_kongtext_svg), file_contents_char);
-    ASSERT_EQ(std::strncmp(file_contents_char, first_line_of_kongtext_svg, std::strlen(first_line_of_kongtext_svg)), 0);
+    std::string first_line_of_kongtext_svg = "<?xml version=\"1.0\" standalone=\"no\"?>";
+    uint32_t length_of_first_line_of_kongtext_svg = first_line_of_kongtext_svg.size();
+    ASSERT_EQ(file_contents.compare(0, length_of_first_line_of_kongtext_svg, first_line_of_kongtext_svg), 0);
 
-    int offset_of_first_character_of_tenth_line = 0x126;
-    const char* tenth_line_of_kongtext_svg = "<font id=\"KongtextRegular\" horiz-adv-x=\"1024\" >";
-    std::printf("%.*s\n", std::strlen(tenth_line_of_kongtext_svg), file_contents_char + offset_of_first_character_of_tenth_line);
-    ASSERT_EQ(std::strncmp(file_contents_char + offset_of_first_character_of_tenth_line, tenth_line_of_kongtext_svg, std::strlen(tenth_line_of_kongtext_svg)), 0);
+    uint32_t offset_of_first_character_of_tenth_line = 0x126;
+    std::string tenth_line_of_kongtext_svg = "<font id=\"KongtextRegular\" horiz-adv-x=\"1024\" >";
+    uint32_t length_of_tenth_line_of_kongtext_svg = tenth_line_of_kongtext_svg.size();
+    ASSERT_EQ(file_contents.compare(offset_of_first_character_of_tenth_line, length_of_tenth_line_of_kongtext_svg, tenth_line_of_kongtext_svg), 0);
 }
