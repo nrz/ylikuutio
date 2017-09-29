6f21caf1ae149601df79c428f54df75b022383de
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-09-29 20:49:09 +0300

    `Object::Object`: reordering code lines.
    All tests pass with both GCC and Clang.
    
    	modified:   code/ylikuutio/ontology/object.cpp

--- code/ylikuutio/ontology/object.cpp
+++ code/ylikuutio/ontology/object.cpp
@@ -56,10 +56,10 @@ namespace ontology
 
         if (this->is_character)
         {
-            this->species_parent_pointer = nullptr;
             this->glyph_parent_pointer   = object_struct.glyph_parent_pointer;
             this->text3D_parent_pointer  = object_struct.text3D_parent_pointer;
             this->universe_pointer       = this->text3D_parent_pointer->universe_pointer;
+            this->species_parent_pointer = nullptr;
         }
         else
         {
