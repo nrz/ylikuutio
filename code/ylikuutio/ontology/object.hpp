d22acf1427c68953664b56615d58556b4cab3638
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-10-14 00:22:35 +0300

    `class Object`: `get_parent()` is now `public`.
    	modified:   code/ylikuutio/ontology/object.hpp

--- code/ylikuutio/ontology/object.hpp
+++ code/ylikuutio/ontology/object.hpp
@@ -90,6 +90,8 @@ namespace ontology
             // destructor.
             virtual ~Object();
 
+            ontology::Entity* get_parent() override;
+
             // this method sets pointer to this `Object` to nullptr, sets `parent_pointer` according to the input,
             // and requests a new `childID` from the new `Species` or from the new `Glyph`.
             void bind_to_new_parent(void* const new_parent_pointer);
@@ -231,7 +233,6 @@ namespace ontology
             // this method renders this `Object`.
             void render();
 
-            ontology::Entity* get_parent() override;
             int32_t get_number_of_children() override;
             int32_t get_number_of_descendants() override;
 
