49aac438c7a6c69bd3f1050ab4dda1c64c1c8bea
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-11-04 21:01:00 +0200

    Cleaning up whitespace.
    	modified:   code/ylikuutio/ontology/shader.hpp

--- code/ylikuutio/ontology/shader.hpp
+++ code/ylikuutio/ontology/shader.hpp
@@ -61,8 +61,8 @@ namespace ontology
 
                 this->char_vertex_shader   = this->vertex_shader.c_str();
                 this->char_fragment_shader = this->fragment_shader.c_str();
-                this->parent       = shader_struct.parent;
-                this->universe     = this->parent->universe;
+                this->parent               = shader_struct.parent;
+                this->universe             = this->parent->universe;
 
                 this->terrain_species = nullptr;
 
