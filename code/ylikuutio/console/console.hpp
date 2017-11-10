a9252f1b21051a7e5c963d2f3bf4cea19ccb639a
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-11-10 23:57:15 +0200

    Refactoring `class Console`: callbacks moved to `console_callbacks.cpp`.
    All tests pass with both GCC and Clang.
    
    	modified:   CMakeLists.txt
    	modified:   code/ylikuutio/console/console.cpp
    	modified:   code/ylikuutio/console/console.hpp
    	new file:   code/ylikuutio/console/console_callbacks.cpp

--- code/ylikuutio/console/console.hpp
+++ code/ylikuutio/console/console.hpp
@@ -11,6 +11,12 @@
 #include "code/ylikuutio/ontology/font2D.hpp"
 #include "code/ylikuutio/common/globals.hpp"
 
+// Include GLEW
+#ifndef __GL_GLEW_H_INCLUDED
+#define __GL_GLEW_H_INCLUDED
+#include <GL/glew.h> // GLfloat, GLuint etc.
+#endif
+
 // Include GLFW
 #ifndef __GLFW3_H_INCLUDED
 #define __GLFW3_H_INCLUDED
