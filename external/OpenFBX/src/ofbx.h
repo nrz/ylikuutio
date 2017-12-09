8d1e4a16e0f0b9c9eed637a6c007d6ca818e19e8
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-12-09 19:56:24 +0200

    `cd external/OpenFBX && git pull`.
    All tests pass with both GCC and Clang.
    
    	modified:   external/OpenFBX/projects/genie.exe
    	modified:   external/OpenFBX/projects/genie.lua
    	new file:   external/OpenFBX/projects/genie_vs17.bat
    	modified:   external/OpenFBX/src/ofbx.cpp
    	modified:   external/OpenFBX/src/ofbx.h

--- external/OpenFBX/src/ofbx.h
+++ external/OpenFBX/src/ofbx.h
@@ -45,6 +45,12 @@ struct Quat
 };
 
 
+struct Color
+{
+	float r, g, b;
+};
+
+
 struct DataView
 {
 	const u8* begin = nullptr;
@@ -214,6 +220,7 @@ struct Material : Object
 
 	Material(const Scene& _scene, const IElement& _element);
 
+	virtual Color getDiffuseColor() const = 0;
 	virtual const Texture* getTexture(Texture::TextureType type) const = 0;
 };
 
@@ -345,6 +352,7 @@ struct IScene
 	virtual const Object* getRoot() const = 0;
 	virtual const TakeInfo* getTakeInfo(const char* name) const = 0;
 	virtual int getMeshCount() const = 0;
+	virtual float getSceneFrameRate() const = 0;
 	virtual const Mesh* getMesh(int index) const = 0;
 	virtual int getAnimationStackCount() const = 0;
 	virtual const AnimationStack* getAnimationStack(int index) const = 0;
