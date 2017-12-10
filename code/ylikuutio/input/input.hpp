2138dd71de82252dc467d916ca64d60e2409f80d
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-12-11 00:30:30 +0200

    Added more input methods to `enum`. These are not yet implemented.
    New input methods:
    `AI`,
    `INPUT_FILE_THEN_KEYBOARD`,
    `INPUT_FILE_THEN_AI`.
    
    	modified:   code/ylikuutio/input/input.hpp

--- code/ylikuutio/input/input.hpp
+++ code/ylikuutio/input/input.hpp
@@ -16,8 +16,11 @@ namespace ylikuutio
     {
         enum input_method
         {
-            KEYBOARD,
-            INPUT_FILE
+            KEYBOARD,                 // regular keyboard input.
+            INPUT_FILE,               // input from file (TODO: implement).
+            AI,                       // AI input (TODO: implement).
+            INPUT_FILE_THEN_KEYBOARD, // input from file, then regular keyboard input (TODO: implement).
+            INPUT_FILE_THEN_AI        // input from file, then AI input (TODO: implement).
         };
 
         void disable_cursor(GLFWwindow* window);
