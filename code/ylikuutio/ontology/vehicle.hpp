9582e298dd2c00363c25042ef24e524220abf6b9
Antti Nuortimo <antti.nuortimo@gmail.com>
2017-12-11 00:46:47 +0200

    Removed unused `#include` lines.
    	modified:   code/ylikuutio/ontology/vehicle.hpp

--- code/ylikuutio/ontology/vehicle.hpp
+++ code/ylikuutio/ontology/vehicle.hpp
@@ -1,14 +1,5 @@
 #include "object.hpp"
 
-// Include GLM
-#ifndef __GLM_GLM_HPP_INCLUDED
-#define __GLM_GLM_HPP_INCLUDED
-#include <glm/glm.hpp> // glm
-#endif
-
-// Include standard headers
-#include <memory> // std::make_shared, std::shared_ptr
-
 namespace ontology
 {
     class Universe;
