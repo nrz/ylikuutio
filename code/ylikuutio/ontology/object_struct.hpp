116505dc698d72490b8a6db69aeeebe3e367a336
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-01-17 20:43:22 +0200

    Split unnecessarily long source code line into several lines.
    	modified:   code/ylikuutio/ontology/object_struct.hpp

--- code/ylikuutio/ontology/object_struct.hpp
+++ code/ylikuutio/ontology/object_struct.hpp
@@ -20,7 +20,16 @@ namespace ontology
 typedef struct ObjectStruct
 {
     ObjectStruct()
-        : species_parent(nullptr), glyph_parent(nullptr), text3D_parent(nullptr), original_scale_vector(glm::vec3(1.0f, 1.0f, 1.0f)), rotate_angle(0.0f), is_character(false), quaternions_in_use(false), cartesian_coordinates(nullptr), rotate_vector(glm::vec3(0.0f, 0.0f, 0.0f)), translate_vector(glm::vec3(0.0f, 0.0f, 0.0f))
+        : species_parent(nullptr),
+        glyph_parent(nullptr),
+        text3D_parent(nullptr),
+        original_scale_vector(glm::vec3(1.0f, 1.0f, 1.0f)),
+        rotate_angle(0.0f),
+        is_character(false),
+        quaternions_in_use(false),
+        cartesian_coordinates(nullptr),
+        rotate_vector(glm::vec3(0.0f, 0.0f, 0.0f)),
+        translate_vector(glm::vec3(0.0f, 0.0f, 0.0f))
     {
         // constructor.
     }
