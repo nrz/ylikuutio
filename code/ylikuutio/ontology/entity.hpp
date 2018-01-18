2c1c639b95408d2013412140513f1a9fbd5a6cbf
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-01-18 20:50:02 +0200

    Cleaning up: removed unnecessary semicolon (`;`).
    	modified:   code/ylikuutio/ontology/entity.hpp

--- code/ylikuutio/ontology/entity.hpp
+++ code/ylikuutio/ontology/entity.hpp
@@ -29,7 +29,7 @@ namespace ontology
             std::string get_type() const;
 
             ontology::Universe* get_universe() const;
-            config::SettingMaster* get_setting_master() const;;
+            config::SettingMaster* get_setting_master() const;
             void set_setting_master(config::SettingMaster* setting_master);
 
             virtual ontology::Entity* get_parent() const = 0;
