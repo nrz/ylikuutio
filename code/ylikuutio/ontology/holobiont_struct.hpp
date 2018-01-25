15bc2425508ef0d1355c8cec86415aec48c5a56b
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-01-26 00:54:42 +0200

    Bugfix: added missing file `holobiont_struct.hpp`.
    	new file:   code/ylikuutio/ontology/holobiont_struct.hpp

---
+++ code/ylikuutio/ontology/holobiont_struct.hpp
@@ -0,0 +1,48 @@
+#ifndef __HOLOBIONT_STRUCT_HPP_INCLUDED
+#define __HOLOBIONT_STRUCT_HPP_INCLUDED
+
+#ifndef PI
+#define PI 3.14159265359f
+#endif
+
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
+// Include standard headers
+#include <memory> // std::make_shared, std::shared_ptr
+
+namespace ontology
+{
+    class Symbiosis;
+}
+
+typedef struct HolobiontStruct
+{
+    HolobiontStruct()
+        : symbiosis_parent(nullptr),
+        original_scale_vector(glm::vec3(1.0f, 1.0f, 1.0f)),
+        rotate_angle(0.0f),
+        initial_rotate_angle(PI),
+        quaternions_in_use(false),
+        cartesian_coordinates(nullptr),
+        rotate_vector(glm::vec3(0.0f, 0.0f, 0.0f)),
+        initial_rotate_vector(glm::vec3(0.0f, 1.0f, 1.0f)),
+        translate_vector(glm::vec3(0.0f, 0.0f, 0.0f))
+    {
+        // constructor.
+    }
+    ontology::Symbiosis* symbiosis_parent; // pointer to the parent `Symbiosis`.
+    glm::vec3 original_scale_vector; // original scale vector.
+    float rotate_angle;              // rotate angle.
+    float initial_rotate_angle;      // initial rotate angle.
+    bool quaternions_in_use;
+    std::shared_ptr<glm::vec3> cartesian_coordinates; // coordinate vector.
+    glm::vec3 rotate_vector;         // rotate vector.
+    glm::vec3 initial_rotate_vector; // initial rotate vector.
+    glm::vec3 translate_vector;      // translate vector.
+} HolobiontStruct;
+
+#endif
