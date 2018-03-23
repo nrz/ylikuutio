aa20fc3401559f84aa5b7ddfecca80a7ffd32d86
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-03-23 20:22:22 +0200

    Removed unnecessary `#include` lines.
    	modified:   code/ajokki/ajokki_helsinki_east_downtown_scene.hpp
    	modified:   code/ajokki/ajokki_joensuu_center_west_scene.hpp

--- code/ajokki/ajokki_joensuu_center_west_scene.hpp
+++ code/ajokki/ajokki_joensuu_center_west_scene.hpp
@@ -5,9 +5,6 @@ namespace ontology
     class EntityFactory;
 }
 
-// Include standard headers
-#include <memory> // std::make_shared, std::shared_ptr
-
 namespace ajokki
 {
     ontology::Entity* create_joensuu_center_west_scene(ontology::EntityFactory* entity_factory, ontology::World* const world);
