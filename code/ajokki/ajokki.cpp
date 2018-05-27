0bfed4a746e8aca3bc0fd569fd0aa333c5d0bae5
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-05-27 22:26:30 +0300

    Bugfix: Ajokki: removed checks for `nullptr` which are after use.
    	modified:   code/ajokki/ajokki.cpp

--- code/ajokki/ajokki.cpp
+++ code/ajokki/ajokki.cpp
@@ -1133,104 +1133,101 @@ int main(int argc, char* argv[])
             printing_struct.font_size = my_universe->get_font_size();
             printing_struct.char_font_texture_file_format = "bmp";
 
-            if (my_universe != nullptr)
+            char angles_and_coordinates_text[256];
+            std::snprintf(
+                    angles_and_coordinates_text,
+                    sizeof(angles_and_coordinates_text),
+                    "%.2f,%.2f rad; %.2f,%.2f deg\\n(%.2f,%.2f,%.2f)",
+                    my_universe->current_camera_horizontal_angle,
+                    my_universe->current_camera_vertical_angle,
+                    RADIANS_TO_DEGREES(my_universe->current_camera_horizontal_angle),
+                    RADIANS_TO_DEGREES(my_universe->current_camera_vertical_angle),
+                    my_universe->current_camera_cartesian_coordinates.x,
+                    my_universe->current_camera_cartesian_coordinates.y,
+                    my_universe->current_camera_cartesian_coordinates.z);
+
+            char time_text[256];
+            std::snprintf(time_text, sizeof(time_text), "%.2f sec", ylikuutio::time::get_time());
+
+            char null_text[] = "";
+            char on_text[] = "on";
+            char off_text[] = "off";
+            char in_use_text[] = " (in use)";
+
+            char help_text_char[1024];
+            std::snprintf(
+                    help_text_char,
+                    sizeof(help_text_char),
+                    "Ajokki 0.0.3\\n"
+                    "\\n"
+                    "arrow keys\\n"
+                    "space jump\\n"
+                    "enter duck\\n"
+                    "F1 help mode\\n"
+                    "`  enter console\\n"
+                    "I  invert mouse (%s)\\n"
+                    "F  flight mode (%s)\\n"
+                    "Ctrl      turbo\\n"
+                    "Ctrl+Ctrl extra turbo\\n"
+                    "for debugging:\\n"
+                    "G  grass texture\\n"
+                    "O  orange fur texture\\n"
+                    "P  pink geometric tiles texture\\n"
+                    "T  terrain species\\n"
+                    "A  suzanne species\\n",
+                    (my_universe->is_invert_mouse_in_use ? on_text : off_text),
+                    (my_universe->is_flight_mode_in_use ? on_text : off_text));
+
+            char spherical_coordinates_text[256];
+
+            if (my_universe->testing_spherical_terrain_in_use)
             {
-                char angles_and_coordinates_text[256];
                 std::snprintf(
-                        angles_and_coordinates_text,
-                        sizeof(angles_and_coordinates_text),
-                        "%.2f,%.2f rad; %.2f,%.2f deg\\n(%.2f,%.2f,%.2f)",
-                        my_universe->current_camera_horizontal_angle,
-                        my_universe->current_camera_vertical_angle,
-                        RADIANS_TO_DEGREES(my_universe->current_camera_horizontal_angle),
-                        RADIANS_TO_DEGREES(my_universe->current_camera_vertical_angle),
-                        my_universe->current_camera_cartesian_coordinates.x,
-                        my_universe->current_camera_cartesian_coordinates.y,
-                        my_universe->current_camera_cartesian_coordinates.z);
+                        spherical_coordinates_text,
+                        sizeof(spherical_coordinates_text),
+                        "rho:%.2f theta:%.2f phi:%.2f",
+                        my_universe->current_camera_spherical_coordinates.rho,
+                        my_universe->current_camera_spherical_coordinates.theta,
+                        my_universe->current_camera_spherical_coordinates.phi);
+            }
 
-                char time_text[256];
-                std::snprintf(time_text, sizeof(time_text), "%.2f sec", ylikuutio::time::get_time());
+            // print cartesian coordinates on bottom left corner.
+            printing_struct.x = 0;
+            printing_struct.y = 0;
+            printing_struct.text_char = angles_and_coordinates_text;
+            printing_struct.horizontal_alignment = "left";
+            printing_struct.vertical_alignment = "bottom";
+            my_font2D->printText2D(printing_struct);
 
-                char null_text[] = "";
-                char on_text[] = "on";
-                char off_text[] = "off";
-                char in_use_text[] = " (in use)";
-
-                char help_text_char[1024];
-                std::snprintf(
-                        help_text_char,
-                        sizeof(help_text_char),
-                        "Ajokki 0.0.3\\n"
-                        "\\n"
-                        "arrow keys\\n"
-                        "space jump\\n"
-                        "enter duck\\n"
-                        "F1 help mode\\n"
-                        "`  enter console\\n"
-                        "I  invert mouse (%s)\\n"
-                        "F  flight mode (%s)\\n"
-                        "Ctrl      turbo\\n"
-                        "Ctrl+Ctrl extra turbo\\n"
-                        "for debugging:\\n"
-                        "G  grass texture\\n"
-                        "O  orange fur texture\\n"
-                        "P  pink geometric tiles texture\\n"
-                        "T  terrain species\\n"
-                        "A  suzanne species\\n",
-                        (my_universe->is_invert_mouse_in_use ? on_text : off_text),
-                        (my_universe->is_flight_mode_in_use ? on_text : off_text));
-
-                char spherical_coordinates_text[256];
-
-                if (my_universe != nullptr && my_universe->testing_spherical_terrain_in_use)
-                {
-                    std::snprintf(
-                            spherical_coordinates_text,
-                            sizeof(spherical_coordinates_text),
-                            "rho:%.2f theta:%.2f phi:%.2f",
-                            my_universe->current_camera_spherical_coordinates.rho,
-                            my_universe->current_camera_spherical_coordinates.theta,
-                            my_universe->current_camera_spherical_coordinates.phi);
-                }
-
-                // print cartesian coordinates on bottom left corner.
+            if (my_universe->in_help_mode && my_universe->can_display_help_screen)
+            {
+                // print help text.
                 printing_struct.x = 0;
-                printing_struct.y = 0;
-                printing_struct.text_char = angles_and_coordinates_text;
-                printing_struct.horizontal_alignment = "left";
-                printing_struct.vertical_alignment = "bottom";
-                my_font2D->printText2D(printing_struct);
-
-                if (my_universe != nullptr && my_universe->in_help_mode && my_universe->can_display_help_screen)
-                {
-                    // print help text.
-                    printing_struct.x = 0;
-                    printing_struct.y = my_universe->get_window_height() - (3 * my_universe->get_text_size());
-                    printing_struct.text_char = help_text_char;
-                    printing_struct.horizontal_alignment = "left";
-                    printing_struct.vertical_alignment = "top";
-                    my_font2D->printText2D(printing_struct);
-                }
-
-                if (my_universe != nullptr && my_universe->testing_spherical_terrain_in_use)
-                {
-                    // print spherical coordinates on bottom left corner.
-                    printing_struct.x = 0;
-                    printing_struct.y += 2 * my_universe->get_text_size();
-                    printing_struct.text_char = spherical_coordinates_text;
-                    printing_struct.horizontal_alignment = "left";
-                    printing_struct.vertical_alignment = "bottom";
-                    my_font2D->printText2D(printing_struct);
-                }
-
-                // print time data on top left corner.
-                printing_struct.x = 0;
-                printing_struct.y = static_cast<GLuint>(my_universe->get_window_height());
-                printing_struct.text_char = time_text;
+                printing_struct.y = my_universe->get_window_height() - (3 * my_universe->get_text_size());
+                printing_struct.text_char = help_text_char;
                 printing_struct.horizontal_alignment = "left";
                 printing_struct.vertical_alignment = "top";
                 my_font2D->printText2D(printing_struct);
             }
+
+            if (my_universe->testing_spherical_terrain_in_use)
+            {
+                // print spherical coordinates on bottom left corner.
+                printing_struct.x = 0;
+                printing_struct.y += 2 * my_universe->get_text_size();
+                printing_struct.text_char = spherical_coordinates_text;
+                printing_struct.horizontal_alignment = "left";
+                printing_struct.vertical_alignment = "bottom";
+                my_font2D->printText2D(printing_struct);
+            }
+
+            // print time data on top left corner.
+            printing_struct.x = 0;
+            printing_struct.y = static_cast<GLuint>(my_universe->get_window_height());
+            printing_struct.text_char = time_text;
+            printing_struct.horizontal_alignment = "left";
+            printing_struct.vertical_alignment = "top";
+            my_font2D->printText2D(printing_struct);
 
             if (ms_frame_text_ready)
             {
