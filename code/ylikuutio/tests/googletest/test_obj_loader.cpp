eb228136d6fc2845848ec89520c5a3d70c742661
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-06-17 14:17:03 +0300

    Test for `load_OBJ`: `TEST(obj_files_must_be_loaded_approriately`.
    	modified:   CMakeLists.txt
    	new file:   code/ylikuutio/tests/googletest/test_obj_loader.cpp

---
+++ code/ylikuutio/tests/googletest/test_obj_loader.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "code/ylikuutio/loaders/obj_loader.cpp"
+
+// Include standard headers
+#include <string>  // std::string
+#include <vector>  // std::vector
+
+TEST(obj_files_must_be_loaded_approriately, suzanne_obj)
+{
+    std::string obj_filename = "suzanne.obj";
+    std::vector<glm::vec3> out_vertices;
+    std::vector<glm::vec2> out_UVs;
+    std::vector<glm::vec3> out_normals;
+
+    bool result = loaders::load_OBJ(obj_filename.c_str(), out_vertices, out_UVs, out_normals);
+    ASSERT_TRUE(result);
+
+    // note: `load_OBJ` only loads the OBJ file. `load_OBJ` does not remove duplicate vertices.
+    const int32_t number_of_faces = 968;
+    const int32_t number_of_vertices_in_triangle = 3;
+    ASSERT_EQ(out_vertices.size(), number_of_faces * number_of_vertices_in_triangle);
+    ASSERT_EQ(out_UVs.size(), number_of_faces * number_of_vertices_in_triangle);
+    ASSERT_EQ(out_normals.size(), number_of_faces * number_of_vertices_in_triangle);
+}
