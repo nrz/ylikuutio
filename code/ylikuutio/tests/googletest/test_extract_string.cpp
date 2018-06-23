9e85099e8b973ffa6b545c2722b8275337be7f3e
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-06-23 22:40:29 +0300

    `test_ylikuutio_string.cpp` split into 4 files.
    Each of the new files contains tests for one (possibly overloaded)
    function.
    
    	modified:   CMakeLists.txt
    	new file:   code/ylikuutio/tests/googletest/test_check_and_report_if_some_string_matches.cpp
    	new file:   code/ylikuutio/tests/googletest/test_extract_string.cpp
    	renamed:    code/ylikuutio/tests/googletest/test_ylikuutio_string.cpp -> code/ylikuutio/tests/googletest/test_extract_string_with_several_endings.cpp
    	new file:   code/ylikuutio/tests/googletest/test_extract_unicode_value_from_string.cpp

---
+++ code/ylikuutio/tests/googletest/test_extract_string.cpp
@@ -0,0 +1,117 @@
+#include "gtest/gtest.h"
+#include "code/ylikuutio/string/ylikuutio_string.hpp"
+
+// Include standard headers
+#include <string>   // std::string
+#include <vector>   // std::vector
+
+TEST(extract_string_from_memory, nothing_must_be_extracted_when_source_string_is_empty_string_and_end_string_is_empty_string)
+{
+    char text[] = "";
+    char* text_pointer = text;
+
+    char end_string[] = "";
+    uint32_t array_size = 128;
+    char* dest_array = new char[array_size];
+    const char* orig_dest_array = dest_array;
+
+    string::extract_string(text, text_pointer, sizeof(text), dest_array, dest_array, array_size, end_string);
+    ASSERT_EQ(dest_array, orig_dest_array);
+    ASSERT_EQ(*dest_array, 0);
+}
+
+TEST(extract_string_from_memory, nothing_must_be_extracted_when_source_string_is_empty_string_and_end_string_is_1_char)
+{
+    char text[] = "";
+    char* text_pointer = text;
+
+    char end_string[] = "a";
+    uint32_t array_size = 128;
+    char* dest_array = new char[array_size];
+    const char* orig_dest_array = dest_array;
+
+    string::extract_string(text, text_pointer, sizeof(text), dest_array, dest_array, array_size, end_string);
+    ASSERT_EQ(dest_array, orig_dest_array);
+    ASSERT_EQ(*dest_array, 0);
+}
+
+TEST(extract_string_from_memory, nothing_must_be_extracted_when_source_string_is_empty_string_and_end_string_is_2_same_chars)
+{
+    char text[] = "";
+    char* text_pointer = text;
+
+    char end_string[] = "aa";
+    uint32_t array_size = 128;
+    char* dest_array = new char[array_size];
+    const char* orig_dest_array = dest_array;
+
+    string::extract_string(text, text_pointer, sizeof(text), dest_array, dest_array, array_size, end_string);
+    ASSERT_EQ(dest_array, orig_dest_array);
+    ASSERT_EQ(*dest_array, 0);
+}
+
+TEST(extract_string_from_memory, nothing_must_be_extracted_when_source_string_is_empty_string_and_end_string_is_2_different_chars)
+{
+    char text[] = "";
+    char* text_pointer = text;
+
+    char end_string[] = "ab";
+    uint32_t array_size = 128;
+    char* dest_array = new char[array_size];
+    const char* orig_dest_array = dest_array;
+
+    string::extract_string(text, text_pointer, sizeof(text), dest_array, dest_array, array_size, end_string);
+    ASSERT_EQ(dest_array, orig_dest_array);
+    ASSERT_EQ(*dest_array, 0);
+}
+
+TEST(extract_string_from_memory, 1_char_must_be_extracted_when_source_string_is_1_char_and_end_string_is_1_char)
+{
+    char text[] = "a";
+    char* text_pointer = text;
+
+    char end_string[] = "a";
+    uint32_t array_size = 128;
+    char* dest_array = new char[array_size];
+    const char* orig_dest_array = dest_array;
+
+    string::extract_string(text, text_pointer, sizeof(text), dest_array, dest_array, array_size, end_string);
+    ASSERT_EQ(dest_array, orig_dest_array);
+    ASSERT_EQ(*dest_array, 0);
+}
+
+TEST(extract_string_from_memory, iloinen_lokki_laulaa_ja_nukkuu)
+{
+    char text[] = "iloinen lokki laulaa ja nukkuu";
+    char* text_pointer = text;
+
+    char end_string[] = "ja";
+    uint32_t array_size = 128;
+    char* dest_array = new char[array_size];
+    const char* orig_dest_array = dest_array;
+
+    string::extract_string(text, text_pointer, sizeof(text), dest_array, dest_array, array_size, end_string);
+    ASSERT_EQ(dest_array, orig_dest_array);
+    ASSERT_EQ(*dest_array++, 'i');
+    ASSERT_EQ(*dest_array++, 'l');
+    ASSERT_EQ(*dest_array++, 'o');
+    ASSERT_EQ(*dest_array++, 'i');
+    ASSERT_EQ(*dest_array++, 'n');
+    ASSERT_EQ(*dest_array++, 'e');
+    ASSERT_EQ(*dest_array++, 'n');
+    ASSERT_EQ(*dest_array++, ' ');
+    ASSERT_EQ(*dest_array++, 'l');
+    ASSERT_EQ(*dest_array++, 'o');
+    ASSERT_EQ(*dest_array++, 'k');
+    ASSERT_EQ(*dest_array++, 'k');
+    ASSERT_EQ(*dest_array++, 'i');
+    ASSERT_EQ(*dest_array++, ' ');
+    ASSERT_EQ(*dest_array++, 'l');
+    ASSERT_EQ(*dest_array++, 'a');
+    ASSERT_EQ(*dest_array++, 'u');
+    ASSERT_EQ(*dest_array++, 'l');
+    ASSERT_EQ(*dest_array++, 'a');
+    ASSERT_EQ(*dest_array++, 'a');
+    ASSERT_EQ(*dest_array++, ' ');
+    ASSERT_EQ(*dest_array++, 0);
+}
