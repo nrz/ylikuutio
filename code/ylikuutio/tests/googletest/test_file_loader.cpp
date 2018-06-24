66f26eb06f15e354b5ff97f08449a266f19eb3cc
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-06-24 14:49:32 +0300

    Renamed a variable in test code: `file_contents` -> `file_content`.
    	modified:   code/ylikuutio/tests/googletest/test_file_loader.cpp

--- code/ylikuutio/tests/googletest/test_file_loader.cpp
+++ code/ylikuutio/tests/googletest/test_file_loader.cpp
@@ -7,13 +7,13 @@
 
 TEST(file_must_be_slurped_appropriately, kongtext_svg)
 {
-    std::string file_contents = file::slurp("kongtext.svg");
+    std::string file_content = file::slurp("kongtext.svg");
     std::string first_line_of_kongtext_svg = "<?xml version=\"1.0\" standalone=\"no\"?>";
     uint32_t length_of_first_line_of_kongtext_svg = first_line_of_kongtext_svg.size();
-    ASSERT_EQ(file_contents.compare(0, length_of_first_line_of_kongtext_svg, first_line_of_kongtext_svg), 0);
+    ASSERT_EQ(file_content.compare(0, length_of_first_line_of_kongtext_svg, first_line_of_kongtext_svg), 0);
 
     uint32_t offset_of_first_character_of_tenth_line = 0x126;
     std::string tenth_line_of_kongtext_svg = "<font id=\"KongtextRegular\" horiz-adv-x=\"1024\" >";
     uint32_t length_of_tenth_line_of_kongtext_svg = tenth_line_of_kongtext_svg.size();
-    ASSERT_EQ(file_contents.compare(offset_of_first_character_of_tenth_line, length_of_tenth_line_of_kongtext_svg, tenth_line_of_kongtext_svg), 0);
+    ASSERT_EQ(file_content.compare(offset_of_first_character_of_tenth_line, length_of_tenth_line_of_kongtext_svg, tenth_line_of_kongtext_svg), 0);
 }
