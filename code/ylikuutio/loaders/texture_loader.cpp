e2a832e2443a928834e44fd77b3b66ff2a710537
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-07-02 19:45:09 +0300

    `loaders::load_texture`: Preliminary support for `GL_MIRRORED_REPEAT`.
    	modified:   code/ylikuutio/loaders/texture_loader.cpp

--- code/ylikuutio/loaders/texture_loader.cpp
+++ code/ylikuutio/loaders/texture_loader.cpp
@@ -66,6 +66,12 @@ namespace loaders
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 #endif
 
+#ifdef MIRRORED_REPEAT_TEXTURES
+        // ... nice trilinear filtering.
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+#endif
+
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glGenerateMipmap(GL_TEXTURE_2D);
