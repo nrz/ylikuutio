73615bda160e6aaa4e9d754fb19f6739f10b9f40
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-07-07 00:02:51 +0300

    Removed obsolete commented out code.
    	modified:   code/ylikuutio/geometry/line_segment.cpp

--- code/ylikuutio/geometry/line_segment.cpp
+++ code/ylikuutio/geometry/line_segment.cpp
@@ -8,14 +8,6 @@ namespace ylikuutio
 {
     namespace geometry
     {
-        /*
-           LineSegment::LineSegment(const std::vector<float> point1, const std::vector<float> point2)
-           {
-        // constructor.
-        // can be used for creating n-dimensional line segments.
-        }
-        */
-
         std::string LineSegment::get_equation()
         {
             std::string line_segment_equation;
