9ba71b45ce9675fb21b605d363d4f81054608458
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-07-28 21:42:42 +0300

    `namespace network` moved inside `namespace yli`.
    	modified:   code/ylikuutio/network/tcp_connection.cpp
    	modified:   code/ylikuutio/network/tcp_connection.hpp
    	modified:   code/ylikuutio/network/tcp_server.cpp
    	modified:   code/ylikuutio/network/tcp_server.hpp

--- code/ylikuutio/network/tcp_connection.cpp
+++ code/ylikuutio/network/tcp_connection.cpp
@@ -7,26 +7,29 @@
 
 // based on http://think-async.com/Asio/asio-1.10.6/doc/asio/tutorial/tutdaytime3.html
 
-namespace network
+namespace yli
 {
-    std::shared_ptr<TcpConnection> TcpConnection::create(asio::io_context& io_context)
+    namespace network
     {
-        return std::make_shared<TcpConnection>(io_context);
-    }
+        std::shared_ptr<TcpConnection> TcpConnection::create(asio::io_context& io_context)
+        {
+            return std::make_shared<TcpConnection>(io_context);
+        }
 
-    asio::ip::tcp::socket& TcpConnection::socket()
-    {
-        return socket_;
-    }
+        asio::ip::tcp::socket& TcpConnection::socket()
+        {
+            return socket_;
+        }
 
-    void TcpConnection::start()
-    {
-        message_ = make_daytime_string();
-        // asio::async_write(socket_, asio::buffer(message_),
-    }
+        void TcpConnection::start()
+        {
+            message_ = make_daytime_string();
+            // asio::async_write(socket_, asio::buffer(message_),
+        }
 
-    std::string TcpConnection::make_daytime_string()
-    {
-        return "test_daytime_string";
+        std::string TcpConnection::make_daytime_string()
+        {
+            return "test_daytime_string";
+        }
     }
 }
