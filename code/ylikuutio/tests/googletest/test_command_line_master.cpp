d66e388fdc3d7704aa528280d54f3171bc88de3a
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-08-03 12:32:18 +0300

    Tests for `yli::command_line::CommandLineMaster::get_value`.
    All tests pass locally with both GCC and Clang.
    
    	modified:   code/ylikuutio/tests/googletest/test_command_line_master.cpp

--- code/ylikuutio/tests/googletest/test_command_line_master.cpp
+++ code/ylikuutio/tests/googletest/test_command_line_master.cpp
@@ -85,3 +85,57 @@ TEST(is_key_must_function_properly, two_keys_with_value)
     ASSERT_FALSE(command_line_master.is_key("quux="));
     ASSERT_FALSE(command_line_master.is_key("=quux"));
 }
+
+TEST(get_value_must_function_properly, no_keys)
+{
+    const int argc = 1; // the executable itself is also in `argv`.
+    char first_arg[] = "foo_executable";
+    const char* argv[] = { first_arg };
+    yli::command_line::CommandLineMaster command_line_master(argc, argv);
+    ASSERT_EQ(command_line_master.get_value("foo_executable"), "");
+    ASSERT_EQ(command_line_master.get_value("foo"), "");
+    ASSERT_EQ(command_line_master.get_value("bar"), "");
+}
+
+TEST(get_value_must_function_properly, one_key_without_value)
+{
+    const int argc = 2; // the executable itself is also in `argv`.
+    char first_arg[] = "foo_executable";
+    char second_arg[] = "bar";
+    const char* argv[] = { first_arg, second_arg };
+    yli::command_line::CommandLineMaster command_line_master(argc, argv);
+    ASSERT_EQ(command_line_master.get_value("foo_executable"), "");
+    ASSERT_EQ(command_line_master.get_value("foo"), "");
+    ASSERT_EQ(command_line_master.get_value("bar"), "");
+    ASSERT_EQ(command_line_master.get_value("baz"), "");
+}
+
+TEST(get_value_must_function_properly, one_key_with_value)
+{
+    const int argc = 2; // the executable itself is also in `argv`.
+    char first_arg[] = "foo_executable";
+    char second_arg[] = "bar=baz";
+    const char* argv[] = { first_arg, second_arg };
+    yli::command_line::CommandLineMaster command_line_master(argc, argv);
+    ASSERT_EQ(command_line_master.get_value("foo_executable"), "");
+    ASSERT_EQ(command_line_master.get_value("foo"), "");
+    ASSERT_EQ(command_line_master.get_value("bar"), "baz");
+    ASSERT_EQ(command_line_master.get_value("baz"), "");
+    ASSERT_EQ(command_line_master.get_value("qux"), "");
+}
+
+TEST(get_value_must_function_properly, two_keys_with_value)
+{
+    const int argc = 3; // the executable itself is also in `argv`.
+    char first_arg[] = "foo_executable";
+    char second_arg[] = "bar=baz";
+    char third_arg[] = "qux=quux";
+    const char* argv[] = { first_arg, second_arg, third_arg };
+    yli::command_line::CommandLineMaster command_line_master(argc, argv);
+    ASSERT_EQ(command_line_master.get_value("foo_executable"), "");
+    ASSERT_EQ(command_line_master.get_value("foo"), "");
+    ASSERT_EQ(command_line_master.get_value("bar"), "baz");
+    ASSERT_EQ(command_line_master.get_value("baz"), "");
+    ASSERT_EQ(command_line_master.get_value("qux"), "quux");
+    ASSERT_EQ(command_line_master.get_value("quux"), "");
+}
