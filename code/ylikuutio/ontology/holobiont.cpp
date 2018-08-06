1cb0c028f2b342ea10e65730b06f68960209a0a2
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-08-06 15:22:49 +0300

    `Holobiont::create_bionts`: added `const` modifier to a variable.
    	modified:   code/ylikuutio/ontology/holobiont.cpp

--- code/ylikuutio/ontology/holobiont.cpp
+++ code/ylikuutio/ontology/holobiont.cpp
@@ -93,7 +93,7 @@ namespace yli
             std::cout << "Creating bionts for Holobiont located at 0x" << std::hex << (uint64_t) this << std::dec << " ...\n";
             // Create `Biont` entities so that
             // they bind this `Holobiont`.
-            std::size_t correct_number_of_bionts = this->symbiosis_parent->get_number_of_symbionts();
+            const std::size_t correct_number_of_bionts = this->symbiosis_parent->get_number_of_symbionts();
             std::cout << "Number of bionts to be created: " << correct_number_of_bionts << "\n";
 
             for (std::size_t biontID = 0; biontID < correct_number_of_bionts; biontID++)
