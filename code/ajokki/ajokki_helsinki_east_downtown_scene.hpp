e777df1540f8c202fb86f4ebda8ceec29e76afa2
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-09-16 00:18:24 +0300

    Bugfix: added missing `#include` guards.
    	modified:   code/ajokki/ajokki_helsinki_east_downtown_scene.hpp
    	modified:   code/ajokki/ajokki_joensuu_center_west_scene.hpp

--- code/ajokki/ajokki_helsinki_east_downtown_scene.hpp
+++ code/ajokki/ajokki_helsinki_east_downtown_scene.hpp
@@ -1,3 +1,6 @@
+#ifndef __AJOKKI_HELSINKI_EAST_DOWNTOWN_SCENE_HPP_INCLUDED
+#define __AJOKKI_HELSINKI_EAST_DOWNTOWN_SCENE_HPP_INCLUDED
+
 namespace yli
 {
     namespace ontology
@@ -12,3 +15,5 @@ namespace ajokki
 {
     yli::ontology::Entity* create_helsinki_east_downtown_scene(yli::ontology::EntityFactory* entity_factory, yli::ontology::World* const world);
 }
+
+#endif
