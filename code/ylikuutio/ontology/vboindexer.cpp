924de15e76f316db164633cb0461ef98495c99db
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-09-15 23:34:12 +0300

    Edited whitespace.
    	modified:   code/ylikuutio/ontology/vboindexer.cpp
    	modified:   code/ylikuutio/ontology/vboindexer.hpp

--- code/ylikuutio/ontology/vboindexer.cpp
+++ code/ylikuutio/ontology/vboindexer.cpp
@@ -94,7 +94,6 @@ namespace yli
                 std::vector<glm::vec3>& in_vertices,
                 std::vector<glm::vec2>& in_UVs,
                 std::vector<glm::vec3>& in_normals,
-
                 std::vector<GLuint>& out_indices,
                 std::vector<glm::vec3>& out_vertices,
                 std::vector<glm::vec2>& out_UVs,
