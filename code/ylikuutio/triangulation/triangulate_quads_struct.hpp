b8fa7683a4a67c8a3eae0218959be9eb44925ac4
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-09-17 00:53:08 +0300

    Split unnecessarily long line into multiple lines.
    	modified:   code/ylikuutio/triangulation/triangulate_quads_struct.hpp

--- code/ylikuutio/triangulation/triangulate_quads_struct.hpp
+++ code/ylikuutio/triangulation/triangulate_quads_struct.hpp
@@ -15,7 +15,12 @@ namespace yli
         typedef struct TriangulateQuadsStruct
         {
             TriangulateQuadsStruct()
-                : image_width(0), image_height(0), x_step(1), z_step(1), should_ylikuutio_use_real_texture_coordinates(true), sphere_radius(NAN)
+                : image_width(0),
+                image_height(0),
+                x_step(1),
+                z_step(1),
+                should_ylikuutio_use_real_texture_coordinates(true),
+                sphere_radius(NAN)
             {
                 // constructor.
             }
