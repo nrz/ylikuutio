02d36fb527d9ac80562b9d5dbc922a07c2558802
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-09-29 19:29:20 +0300

    `yli_convert`: more code. Exporting to FBX still fails.
    	modified:   code/tools/yli_convert.cpp

--- code/tools/yli_convert.cpp
+++ code/tools/yli_convert.cpp
@@ -1,12 +1,25 @@
 #include "code/ylikuutio/ontology/symbiosis.hpp"
+#include "code/ylikuutio/ontology/symbiont_species.hpp"
 #include "code/ylikuutio/ontology/symbiosis_struct.hpp"
 #include "code/ylikuutio/command_line/command_line_master.hpp"
 #include "code/ylikuutio/string/ylikuutio_string.hpp"
 
+#include <assimp/scene.h>
+#include <assimp/Exporter.hpp>
+#include <assimp/types.h>
+
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
 // Include standard headers
 #include <algorithm>     // std::transform
+#include <cstddef>       // std::size_t
 #include <cstdlib>       // EXIT_FAILURE, EXIT_SUCCESS
 #include <iostream>      // std::cout, std::cin, std::cerr
+#include <limits>        // std::numeric_limits
 #include <string>        // std::string
 #include <vector>        // std::vector
 
@@ -20,6 +33,7 @@
 // optional:
 // `--informat=baz` assume file format `baz` (if `--informat` is left out, file format is deduced from the filename extension).
 // `--outformat=qux` export 3D data into file `qux` (if `--outformat` is left out, file format is deduced from the filename extension).
+// `--intexture=quux` use texture data into file `quux` as the material for all meshes (eg. `.obj` input file).
 
 namespace yli
 {
@@ -36,13 +50,13 @@ int main(const int argc, const char* argv[])
 
     if (!command_line_master.is_key("--in"))
     {
-        std::cerr << "--in parameter is required!\n\nexample:\n\nyli_convert --in=foo --out=bar\n";
+        std::cerr << "ERROR: --in parameter is required!\n\nexample:\n\nyli_convert --in=foo --out=bar\n";
         exit(EXIT_FAILURE);
     }
 
     if (!command_line_master.is_key("--out"))
     {
-        std::cerr << "--out parameter is required!\n\nexample:\n\nyli_convert --in=foo --out=bar\n";
+        std::cerr << "ERROR: --out parameter is required!\n\nexample:\n\nyli_convert --in=foo --out=bar\n";
         exit(EXIT_FAILURE);
     }
 
@@ -63,7 +77,7 @@ int main(const int argc, const char* argv[])
 
     if (in_file_format.empty())
     {
-        std::cerr << "--informat is empty!\n";
+        std::cerr << "ERROR: --informat is empty!\n";
         exit(EXIT_FAILURE);
     }
 
@@ -83,7 +97,7 @@ int main(const int argc, const char* argv[])
 
     if (out_file_format.empty())
     {
-        std::cerr << "--outformat is empty!\n";
+        std::cerr << "ERROR: --outformat is empty!\n";
         exit(EXIT_FAILURE);
     }
 
@@ -97,7 +111,7 @@ int main(const int argc, const char* argv[])
 
     if (in_file_format != "obj" && in_file_format != "fbx")
     {
-        std::cerr << "unsupported in file format: " << in_file_format << "\n";
+        std::cerr << "ERROR: unsupported in file format: " << in_file_format << "\n";
         exit(EXIT_FAILURE);
     }
 
@@ -109,18 +123,119 @@ int main(const int argc, const char* argv[])
     symbiosis_struct.parent = nullptr;           // no parent.
     symbiosis_struct.vram_buffer_in_use = false; // do not use VRAM buffer.
     yli::ontology::Universe* const universe = nullptr;
-    yli::ontology::Symbiosis* const symbiosis = new yli::ontology::Symbiosis(universe, symbiosis_struct);
+    yli::ontology::Symbiosis const symbiosis = yli::ontology::Symbiosis(universe, symbiosis_struct);
 
-    // TODO: Export 3D model file.
-    if (out_file_format == "obj")
+    // Assimp usage example:
+    // https://github.com/assimp/assimp/issues/203
+
+    aiScene ai_scene;
+    ai_scene.mRootNode = new aiNode();
+    ai_scene.mNumMaterials = symbiosis.get_number_of_symbiont_materials();
+    ai_scene.mMaterials = new aiMaterial*[ai_scene.mNumMaterials];
+    ai_scene.mNumMeshes = symbiosis.get_number_of_symbiont_species();
+    ai_scene.mMeshes = new aiMesh*[ai_scene.mNumMeshes];
+
+    std::cout << "Number of `SymbiontMaterial`s: " << symbiosis.get_number_of_symbiont_materials() << "\n";
+
+    for (std::size_t symbiont_material_i = 0; symbiont_material_i < symbiosis.get_number_of_symbiont_materials(); symbiont_material_i++)
     {
+        std::cout << "Processing `SymbiontMaterial` " << symbiont_material_i << "\n";
+        yli::ontology::SymbiontMaterial* symbiont_material = symbiosis.get_symbiont_material(symbiont_material_i);
+
+        ai_scene.mMaterials[symbiont_material_i] = new aiMaterial();
+
+        if (symbiont_material == nullptr)
+        {
+            std::cerr << "ERROR: `yli_convert`: `symbiont_material` is `nullptr`!\n";
+            continue;
+        }
+
+        if (symbiont_material->get_childID() == std::numeric_limits<std::size_t>::max())
+        {
+            // childID is the default (invalid) value, not a real childID!
+            std::cerr << "ERROR: `yli_convert`: `childID` is `std::numeric_limits<std::size_t>::max()`!\n";
+            continue;
+        }
     }
-    else if (out_file_format == "fbx")
+
+    for (std::size_t symbiont_species_i = 0; symbiont_species_i < symbiosis.get_number_of_symbiont_species(); symbiont_species_i++)
     {
+        const yli::ontology::SymbiontSpecies* const symbiont_species = symbiosis.get_symbiont_species(symbiont_species_i);
+
+        if (symbiont_species == nullptr)
+        {
+            std::cerr << "ERROR: `yli_convert`: `symbiont_species` is `nullptr`!\n";
+            continue;
+        }
+
+        ai_scene.mMeshes[symbiont_species_i] = new aiMesh();
+        const yli::ontology::SymbiontMaterial* const symbiont_material = static_cast<yli::ontology::SymbiontMaterial*>(symbiont_species->get_parent());
+
+        if (symbiont_material == nullptr)
+        {
+            continue;
+        }
+
+        ai_scene.mMeshes[symbiont_species_i]->mMaterialIndex = symbiont_material->get_childID();
+
+        std::vector<glm::vec3> vertices = symbiont_species->get_vertices();
+        std::vector<glm::vec3> normals = symbiont_species->get_normals();
+        std::vector<glm::vec2> uvs = symbiont_species->get_uvs();
+
+        const std::size_t number_of_vertices = vertices.size();
+
+        ai_scene.mMeshes[symbiont_species_i]->mNumVertices = number_of_vertices;
+        ai_scene.mMeshes[symbiont_species_i]->mVertices = new aiVector3D[number_of_vertices];
+        ai_scene.mMeshes[symbiont_species_i]->mNormals = new aiVector3D[number_of_vertices];
+        ai_scene.mMeshes[symbiont_species_i]->mTextureCoords[0] = new aiVector3D[number_of_vertices];
+        ai_scene.mMeshes[symbiont_species_i]->mNumUVComponents[0] = number_of_vertices;
+
+        for (std::size_t vertex_i = 0; vertex_i < number_of_vertices; vertex_i++)
+        {
+            ai_scene.mMeshes[symbiont_species_i]->mVertices[vertex_i] = aiVector3D(vertices[vertex_i].x, vertices[vertex_i].y, vertices[vertex_i].z);
+            ai_scene.mMeshes[symbiont_species_i]->mVertices[vertex_i] = aiVector3D(normals[vertex_i].x, normals[vertex_i].y, normals[vertex_i].z);
+            ai_scene.mMeshes[symbiont_species_i]->mTextureCoords[0][vertex_i] = aiVector3D(uvs[vertex_i].x, uvs[vertex_i].y, 0);
+        }
+
+        const std::size_t number_of_faces = number_of_vertices / 3;
+        std::cout << "Number of faces: " << number_of_faces << "\n";
+
+        ai_scene.mMeshes[symbiont_species_i]->mFaces = new aiFace[number_of_faces];
+        ai_scene.mMeshes[symbiont_species_i]->mNumFaces = number_of_faces;
+
+        for (std::size_t face_i = 0; face_i < number_of_faces; face_i++)
+        {
+            ai_scene.mMeshes[symbiont_species_i]->mFaces[face_i].mIndices = new unsigned int[3];
+            ai_scene.mMeshes[symbiont_species_i]->mFaces[face_i].mNumIndices = 3;
+            ai_scene.mMeshes[symbiont_species_i]->mFaces[face_i].mIndices[0] = 3 * face_i;
+            ai_scene.mMeshes[symbiont_species_i]->mFaces[face_i].mIndices[1] = 3 * face_i + 1;
+            ai_scene.mMeshes[symbiont_species_i]->mFaces[face_i].mIndices[2] = 3 * face_i + 2;
+        }
+    }
+
+    std::cout << "Creating Assimp::Exporter ...\n";
+    Assimp::Exporter assimp_exporter = Assimp::Exporter();
+    std::cout << "Exporting to " << out_file << " ...\n";
+    const aiReturn export_ai_return = assimp_exporter.Export(&ai_scene, out_file_format, out_file);
+
+    if (export_ai_return == aiReturn_SUCCESS)
+    {
+        std::cout << "Export to " << out_file << " successful.\n";
+    }
+    else if (export_ai_return == aiReturn_FAILURE)
+    {
+        std::cerr << "ERROR: Export to " << out_file << " failed.\n";
+    }
+    else if (export_ai_return == aiReturn_OUTOFMEMORY)
+    {
+        std::cerr << "ERROR: Export to " << out_file << " failed: out of memory.\n";
+    }
+    else if (export_ai_return == _AI_ENFORCE_ENUM_SIZE)
+    {
+        std::cerr << "ERROR: Export to " << out_file << " failed: _AI_ENFORCE_ENUM_SIZE.\n";
     }
     else
     {
-        std::cerr << "unsupported out file format: " << out_file_format << "\n";
-        exit(EXIT_FAILURE);
+        std::cerr << "ERROR: Export to " << out_file << " failed: unknown error.\n";
     }
 }
