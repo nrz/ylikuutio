bba9ee93edb06cdd3a941ab9bcf1796c5d53354e
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-10-13 20:09:58 +0300

    Removed obsolete code.
    	modified:   CMakeLists.txt
    	deleted:    code/ylikuutio/console/command_and_callback_struct.hpp
    	modified:   code/ylikuutio/console/console.cpp
    	modified:   code/ylikuutio/console/console.hpp

--- code/ylikuutio/console/console.hpp
+++ code/ylikuutio/console/console.hpp
@@ -2,7 +2,6 @@
 #define __CONSOLE_HPP_INCLUDED
 
 #include "console_struct.hpp"
-#include "command_and_callback_struct.hpp"
 #include "code/ylikuutio/callback_system/key_and_callback_struct.hpp"
 #include "code/ylikuutio/callback_system/callback_parameter.hpp"
 #include "code/ylikuutio/callback_system/callback_object.hpp"
