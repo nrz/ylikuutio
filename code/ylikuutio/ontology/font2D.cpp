e218a3fc61e1ffbd676ff1cf930aabdcf2aeda38
Antti Nuortimo <antti.nuortimo@gmail.com>
2018-10-28 22:42:00 +0200

    Bugfix: added missing `#include` line.
    	modified:   code/ylikuutio/ontology/font2D.cpp

--- code/ylikuutio/ontology/font2D.cpp
+++ code/ylikuutio/ontology/font2D.cpp
@@ -5,6 +5,12 @@
 #include "family_templates.hpp"
 #include "code/ylikuutio/hierarchy/hierarchy_templates.hpp"
 
+// Include GLEW
+#ifndef __GL_GLEW_H_INCLUDED
+#define __GL_GLEW_H_INCLUDED
+#include <GL/glew.h> // GLfloat, GLuint etc.
+#endif
+
 // Include GLM
 #ifndef __GLM_GLM_HPP_INCLUDED
 #define __GLM_GLM_HPP_INCLUDED
