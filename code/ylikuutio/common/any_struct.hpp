8ab95978f74baa1928fc10a76acf9923a94122c5
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-01-06 00:46:19 +0100

    `yli::datatypes::AnyStruct` works.
    	modified:   code/ylikuutio/common/any_struct.cpp
    	modified:   code/ylikuutio/common/any_struct.hpp

--- code/ylikuutio/common/any_struct.hpp
+++ code/ylikuutio/common/any_struct.hpp
@@ -2,6 +2,7 @@
 #define __ANY_STRUCT_HPP_INCLUDED
 
 // Include standard headers
+#include <cstddef>       // std::size_t
 #include <memory>        // std::make_shared, std::shared_ptr
 #include <string>        // std::string
 #include <unordered_map> // std::unordered_map
@@ -33,12 +34,34 @@ namespace yli
                 // destructor.
                 ~AnyStruct();
 
-                void enter_data(const std::string& target, std::shared_ptr<yli::datatypes::AnyValue> any_value);
-                std::shared_ptr<yli::datatypes::AnyValue> read_data(const std::string& target);
-                void erase_data(const std::string& target);
+                bool enter_data(const std::string& target, std::shared_ptr<yli::datatypes::AnyValue> any_value);
+                bool erase_data(const std::string& target);
+                bool check_if_exist(const std::string& target) const;
+                std::shared_ptr<yli::datatypes::AnyValue> read_data(const std::string& target) const;
                 std::vector<std::string> get_fieldnames() const;
 
             private:
+                bool enter_data(
+                        const std::string& target,
+                        std::size_t& data_index,
+                        std::shared_ptr<yli::datatypes::AnyValue> any_value,
+                        std::string& first_part);
+
+                bool erase_data(
+                        const std::string& target,
+                        std::size_t& data_index,
+                        std::string& first_part);
+
+                bool check_if_exist(
+                        const std::string& target,
+                        std::size_t& data_index,
+                        std::string& first_part) const;
+
+                std::shared_ptr<yli::datatypes::AnyValue> read_data(
+                        const std::string& target,
+                        std::size_t& data_index,
+                        std::string& first_part) const;
+
                 std::unordered_map<std::string, std::shared_ptr<yli::datatypes::AnyValue>> values;
         };
     }
