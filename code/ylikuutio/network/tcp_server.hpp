ad7eff2449830fbd06dea1539512646e2424c55e
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-01-08 19:51:59 +0200

    Added appropriate license information to modified Asio code.
    	new file:   code/asio/LICENSE_1_0.txt
    	modified:   code/asio/asio_chat_client.cpp
    	modified:   code/asio/asio_chat_server.cpp
    	modified:   code/ylikuutio/network/tcp_connection.cpp
    	modified:   code/ylikuutio/network/tcp_connection.hpp
    	modified:   code/ylikuutio/network/tcp_server.cpp
    	modified:   code/ylikuutio/network/tcp_server.hpp

--- code/ylikuutio/network/tcp_server.hpp
+++ code/ylikuutio/network/tcp_server.hpp
@@ -3,6 +3,16 @@
 #include <asio.hpp>
 
 // based on http://think-async.com/Asio/asio-1.10.6/doc/asio/tutorial/tutdaytime3.html
+//
+// server.cpp
+// ~~~~~~~~~~
+//
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// This file has been modified by Antti Nuortimo.
 
 namespace yli
 {
