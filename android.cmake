8ba10ecd4909adaba6a3890f0ed5e019734fea69
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-01-13 20:57:03 +0200

    `android.cmake`: adjust the behavior of FIND_XXX() commands.
    Set values of the following parameters:
    `CMAKE_FIND_ROOT_PATH_MODE_PROGRAM`,
    `CMAKE_FIND_ROOT_PATH_MODE_LIBRARY`,
    `CMAKE_FIND_ROOT_PATH_MODE_INCLUDE`.
    
    	modified:   android.cmake

--- android.cmake
+++ android.cmake
@@ -7,3 +7,10 @@ set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 set(CMAKE_ANDROID_NDK /usr/lib/android-ndk)
 # set(CMAKE_ANDROID_ARM_MODE ON)
 # set(CMAKE_ANDROID_ARM_NEON ON)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
