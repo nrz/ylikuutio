2d528e049fc7e9acf8b9e979252760541bc74f48
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-01-13 17:46:40 +0200

    Set `CMAKE_ANDROID_ARCH_ABI` to `arm64-v8a`.
    	modified:   android.cmake

--- android.cmake
+++ android.cmake
@@ -3,6 +3,7 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_SYSTEM_VERSION 23)
+set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
 set(CMAKE_ANDROID_NDK /usr/lib/android-ndk)
 # set(CMAKE_ANDROID_ARM_MODE ON)
 # set(CMAKE_ANDROID_ARM_NEON ON)
