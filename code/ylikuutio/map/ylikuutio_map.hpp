390b22aeda7f8baa2d6a788f1852b38c6b097d2e
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-01-26 18:17:48 +0200

    Removed unused function templates.
    	modified:   code/ylikuutio/map/ylikuutio_map.hpp

--- code/ylikuutio/map/ylikuutio_map.hpp
+++ code/ylikuutio/map/ylikuutio_map.hpp
@@ -17,23 +17,6 @@ namespace yli
     namespace map
     {
         template <class T1>
-            std::vector<std::string> get_keys(const std::unordered_map<std::string, T1>* const unordered_map_pointer)
-            {
-                std::vector<std::string> key_vector;
-                key_vector.reserve(unordered_map_pointer->size());
-
-                for (auto key_and_value : *unordered_map_pointer)
-                {
-                    key_vector.push_back(key_and_value.first); // key.
-                }
-
-                // sort key vector alphabetically.
-                std::sort(key_vector.begin(), key_vector.end());
-
-                return key_vector;
-            }
-
-        template <class T1>
             std::vector<std::string> get_keys(const std::unordered_map<std::string, T1>& unordered_map)
             {
                 std::vector<std::string> key_vector;
@@ -48,43 +31,6 @@ namespace yli
                 std::sort(key_vector.begin(), key_vector.end());
 
                 return key_vector;
-            }
-
-        template <class T1>
-            void print_keys_to_console(const std::unordered_map<std::string, T1>* const unordered_map_pointer, yli::console::Console* const console)
-            {
-                std::vector<std::string> key_vector = yli::map::get_keys(unordered_map_pointer);
-
-                std::size_t characters_for_line = console->get_universe()->get_window_width() / console->get_universe()->get_text_size();
-
-                std::string keys_text;
-
-                for (std::string key : key_vector)
-                {
-                    if (keys_text.size() > 0 &&
-                            keys_text.size() + key.size() >= characters_for_line)
-                    {
-                        // Not enough space for this key on this line.
-                        // Print this line.
-                        console->print_text(keys_text);
-                        keys_text = key;
-                    }
-                    else if (keys_text.size() > 0)
-                    {
-                        // There is space, and this is not the first key on this line.
-                        keys_text += " " + key;
-                    }
-                    else
-                    {
-                        // This is the first key on this line.
-                        keys_text += key;
-                    }
-                }
-                if (keys_text.size() > 0)
-                {
-                    // Print the last line.
-                    console->print_text(keys_text);
-                }
             }
 
         template <class T1>
@@ -121,30 +67,6 @@ namespace yli
                 {
                     // Print the last line.
                     console->print_text(keys_text);
-                }
-            }
-
-        template <class T1>
-            void print_keys_and_values(const std::unordered_map<std::string, T1>* const unordered_map_pointer)
-            {
-                if (unordered_map_pointer->size() == 0)
-                {
-                    std::cout << "no keys.\n";
-                    return;
-                }
-
-                std::vector<std::string> key_vector = yli::map::get_keys(unordered_map_pointer);
-
-                for (std::string key : key_vector)
-                {
-                    if (unordered_map_pointer->at(key).empty())
-                    {
-                        std::cout << key << " (no value)\n";
-                    }
-                    else
-                    {
-                        std::cout << key << " = " << unordered_map_pointer->at(key) << "\n";
-                    }
                 }
             }
 
