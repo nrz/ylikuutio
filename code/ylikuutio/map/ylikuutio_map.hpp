ef0949193b116cbbe613b3c31496f8ed440477b9
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-01-26 17:36:24 +0200

    Overloaded `yli::map::print_keys_to_console`.
    	modified:   code/ylikuutio/map/ylikuutio_map.hpp

--- code/ylikuutio/map/ylikuutio_map.hpp
+++ code/ylikuutio/map/ylikuutio_map.hpp
@@ -88,6 +88,43 @@ namespace yli
             }
 
         template <class T1>
+            void print_keys_to_console(const std::unordered_map<std::string, T1>& unordered_map, yli::console::Console* const console)
+            {
+                std::vector<std::string> key_vector = yli::map::get_keys(unordered_map);
+
+                std::size_t characters_for_line = console->get_universe()->get_window_width() / console->get_universe()->get_text_size();
+
+                std::string keys_text;
+
+                for (std::string key : key_vector)
+                {
+                    if (keys_text.size() > 0 &&
+                            keys_text.size() + key.size() >= characters_for_line)
+                    {
+                        // Not enough space for this key on this line.
+                        // Print this line.
+                        console->print_text(keys_text);
+                        keys_text = key;
+                    }
+                    else if (keys_text.size() > 0)
+                    {
+                        // There is space, and this is not the first key on this line.
+                        keys_text += " " + key;
+                    }
+                    else
+                    {
+                        // This is the first key on this line.
+                        keys_text += key;
+                    }
+                }
+                if (keys_text.size() > 0)
+                {
+                    // Print the last line.
+                    console->print_text(keys_text);
+                }
+            }
+
+        template <class T1>
             void print_keys_and_values(const std::unordered_map<std::string, T1>* const unordered_map_pointer)
             {
                 if (unordered_map_pointer->size() == 0)
