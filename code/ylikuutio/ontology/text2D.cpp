nrz/ylikuutio

commit 1dfbda16ad9831dbf5a58ca5a4b94e3475cccbeb

`Text2D::render`: `"\n"` is interpreted as newline, just like `"\\n"`.

code/ylikuutio/ontology/text2D.cpp
@@ -224,6 +224,15 @@ namespace yli
                         continue;
                     }
                 }
+                else if (character == (char) '\n')
+                {
+                    // jump to the beginning of the next line.
+                    // `"left"` horizontal alignment and `"top"` vertical alignment are assumed.
+                    // TODO: implement newline for other horizontal and vertical alignments too!
+                    current_left_x = this->x;
+                    current_top_y -= this->text_size;
+                    continue;
+                }
 
                 vertex_up_left_x = vertex_down_left_x = current_left_x;
                 vertex_up_right_x = vertex_down_right_x = current_left_x + this->text_size;

commit 3d8574f23777eababb4369b981887cdc537df3d8

Bugfix: `Text2D::render`: compute `number_of_lines` correctly.
This fixes the bug introduced in commit
1dfbda16ad9831dbf5a58ca5a4b94e3475cccbeb, though the bug is visible in
Hirvi only from 21c419f827a5edbc6337dc51f7bdeaacbeadcb8e onwards.

code/ylikuutio/ontology/text2D.cpp
@@ -140,6 +140,10 @@ namespace yli
                         number_of_lines++;
                     }
                 }
+                else if (character == (char) '\n')
+                {
+                    number_of_lines++;
+                }
             }
 
             std::size_t current_left_x;