24efc12c0c85a4ad296dbeec85cff70ced3f812b
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-10-04 19:02:14 +0300

    Bugfix: `Universe`: `Console`s are counted as children & descendants.

--- code/ylikuutio/ontology/universe.hpp
+++ code/ylikuutio/ontology/universe.hpp
@@ -463,6 +463,7 @@ namespace yli
                     // `yli::ontology::Entity` member variables begin here.
                     this->child_vector_pointers_vector.push_back(&this->world_pointer_vector);
                     this->child_vector_pointers_vector.push_back(&this->font2D_pointer_vector);
+                    this->child_vector_pointers_vector.push_back(&this->console_pointer_vector);
                     this->child_vector_pointers_vector.push_back(&this->any_value_entity_pointer_vector);
                     this->child_vector_pointers_vector.push_back(&this->any_struct_entity_pointer_vector);
                     this->type_string = "yli::ontology::Universe*";
