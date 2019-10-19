0662b1790a9de4678c01f9087fdef3e68a1c6888
Antti Nuortimo <antti.nuortimo@gmail.com>
2019-10-19 17:38:45 +0300

    `Console::exit_console`: check `console` for `nullptr`.

--- code/ylikuutio/ontology/console_callbacks.cpp
+++ code/ylikuutio/ontology/console_callbacks.cpp
@@ -310,6 +310,12 @@ namespace yli
                 std::vector<yli::callback_system::CallbackParameter*>&,
                 yli::ontology::Console* console)
         {
+            if (console == nullptr)
+            {
+                // We did not exit the console.
+                return nullptr;
+            }
+
             if (console->in_console && console->exit_console())
             {
                 // Signal to caller that we have exited the console.
