03db2cddf73f003852863a9528591250156b3668
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-04-04 22:34:43 +0300

    `export_blend_into_fbx`.
    Bash script and Python script that export `.blend` file using FBX file
    format, with `.fbx` extension.
    
    `export_blend_into_fbx.py` is GPL 2.0 or later.

---
+++ code/python/export_blend_into_fbx.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+# Data preprocessing script for converting .blend into .fbx.
+#
+# Copyright (C) 2015-2020 Antti Nuortimo.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# This script does the following:
+# 1. Unpack all data into local files.
+# 2. Export data into an FBX file that is named after its parent directory.
+
+import os
+import bpy
+
+# get the name of the directory where this 3D model file (.blend or something else) is located.
+directory = os.path.basename(os.path.dirname(bpy.data.filepath))
+new_filename = directory + ".fbx"
+new_abs_filename = os.path.join(os.path.dirname(bpy.data.filepath), new_filename)
+print("new_abs_filename: " + new_abs_filename)
+
+# 1. Unpack all data into local files.
+bpy.ops.file.unpack_all(method='WRITE_LOCAL')
+
+# 2. Export data into an FBX file that is named after its parent directory.
+bpy.ops.export_scene.fbx(
+        filepath=new_abs_filename,
+        check_existing=False,
+        axis_forward='Z',
+        axis_up='Y',
+        use_selection=False)
