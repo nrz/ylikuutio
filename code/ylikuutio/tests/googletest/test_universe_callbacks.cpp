04229fc63a9f401032263d8c0fa4b22b61e4501f
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-04-23 17:23:05 +0300

    Tests for `Universe::activate`.

--- code/ylikuutio/tests/googletest/test_universe_callbacks.cpp
+++ code/ylikuutio/tests/googletest/test_universe_callbacks.cpp
@@ -22,9 +22,23 @@
 #include "code/ylikuutio/ontology/universe.hpp"
 #include "code/ylikuutio/ontology/world.hpp"
 #include "code/ylikuutio/ontology/scene.hpp"
+#include "code/ylikuutio/ontology/shader.hpp"
+#include "code/ylikuutio/ontology/material.hpp"
+#include "code/ylikuutio/ontology/species.hpp"
+#include "code/ylikuutio/ontology/object.hpp"
+#include "code/ylikuutio/ontology/camera.hpp"
+#include "code/ylikuutio/ontology/font2D.hpp"
+#include "code/ylikuutio/ontology/console.hpp"
 #include "code/ylikuutio/ontology/any_value_entity.hpp"
 #include "code/ylikuutio/ontology/any_struct_entity.hpp"
 #include "code/ylikuutio/ontology/universe_struct.hpp"
+#include "code/ylikuutio/ontology/scene_struct.hpp"
+#include "code/ylikuutio/ontology/shader_struct.hpp"
+#include "code/ylikuutio/ontology/material_struct.hpp"
+#include "code/ylikuutio/ontology/species_struct.hpp"
+#include "code/ylikuutio/ontology/object_struct.hpp"
+#include "code/ylikuutio/ontology/camera_struct.hpp"
+#include "code/ylikuutio/ontology/font2D.hpp"
 
 // Include standard headers
 #include <stdint.h> // uint32_t etc.
@@ -603,4 +617,158 @@ TEST(scene_must_be_activated_appropriately, universe_callback)
     std::vector<std::string> command_parameters = { scene_name };
     universe->activate(console, universe, command_parameters);
     ASSERT_EQ(universe->get_active_scene(), scene);
+}
+
+TEST(console_must_be_activated_appropriately, universe_callback_without_font2D)
+{
+    yli::ontology::UniverseStruct universe_struct;
+    universe_struct.is_headless = true;
+    yli::ontology::Universe* const universe = new yli::ontology::Universe(universe_struct);
+
+    const std::string console_name = "foo";
+
+    yli::ontology::Console* const console = new yli::ontology::Console(universe, &universe->parent_of_consoles);
+    console->set_name(console_name);
+
+    ASSERT_EQ(universe->get_active_console(), nullptr);
+
+    std::vector<std::string> command_parameters = { console_name };
+    universe->activate(console, universe, command_parameters);
+    ASSERT_EQ(universe->get_active_console(), console);
+}
+
+TEST(console_must_be_activated_appropriately, universe_callback_with_font2D)
+{
+    yli::ontology::UniverseStruct universe_struct;
+    universe_struct.is_headless = true;
+    yli::ontology::Universe* const universe = new yli::ontology::Universe(universe_struct);
+
+    yli::ontology::Font2D* font2D = new yli::ontology::Font2D(universe, yli::ontology::FontStruct(), &universe->parent_of_font2Ds);
+
+    const std::string console_name = "foo";
+
+    yli::ontology::Console* const console = new yli::ontology::Console(universe, &universe->parent_of_consoles);
+    console->set_name(console_name);
+
+    ASSERT_EQ(universe->get_active_console(), nullptr);
+
+    std::vector<std::string> command_parameters = { console_name };
+    universe->activate(console, universe, command_parameters);
+    ASSERT_EQ(universe->get_active_console(), console);
+}
+
+TEST(scene_and_camera_must_be_activated_appropriately, universe_callback)
+{
+    yli::ontology::Console* const console = nullptr;
+
+    yli::ontology::UniverseStruct universe_struct;
+    universe_struct.is_headless = true;
+    yli::ontology::Universe* const universe = new yli::ontology::Universe(universe_struct);
+
+    yli::ontology::World* const world = new yli::ontology::World(universe, &universe->parent_of_worlds);
+
+    const std::string scene_name = "foo";
+
+    yli::ontology::SceneStruct scene_struct;
+    scene_struct.world = world;
+    yli::ontology::Scene* const scene = new yli::ontology::Scene(universe, scene_struct, &world->parent_of_scenes);
+    scene->set_name(scene_name);
+
+    const std::string camera_name = "bar";
+    yli::ontology::Camera* const camera = new yli::ontology::Camera(universe, yli::ontology::CameraStruct(), &scene->parent_of_cameras);
+    camera->set_name(camera_name);
+
+    ASSERT_EQ(universe->get_active_scene(), nullptr);
+    ASSERT_EQ(scene->get_active_camera(), nullptr);
+
+    std::vector<std::string> activate_scene_command_parameters = { scene_name };
+    universe->activate(console, universe, activate_scene_command_parameters);
+    ASSERT_EQ(universe->get_active_scene(), scene);
+    ASSERT_EQ(scene->get_active_camera(), nullptr);
+
+    std::vector<std::string> activate_camera_command_parameters = { camera_name };
+    universe->activate(console, universe, activate_camera_command_parameters);
+    ASSERT_EQ(universe->get_active_scene(), scene);
+    ASSERT_EQ(scene->get_active_camera(), camera);
+}
+
+TEST(scene_must_be_deleted_appropriately, universe_callback)
+{
+    yli::ontology::Console* const console = nullptr;
+
+    yli::ontology::UniverseStruct universe_struct;
+    universe_struct.is_headless = true;
+    yli::ontology::Universe* const universe = new yli::ontology::Universe(universe_struct);
+
+    yli::ontology::World* const world = new yli::ontology::World(universe, &universe->parent_of_worlds);
+
+    const std::string scene_name = "foo";
+
+    yli::ontology::SceneStruct scene_struct;
+    scene_struct.world = world;
+    yli::ontology::Scene* const scene = new yli::ontology::Scene(universe, scene_struct, &world->parent_of_scenes);
+    scene->set_name(scene_name);
+
+    std::vector<std::string> command_parameters = { scene_name };
+    universe->activate(console, universe, command_parameters);
+
+    universe->delete_entity(console, universe, command_parameters);
+    ASSERT_EQ(universe->get_active_scene(), nullptr);
+}
+
+TEST(object_must_be_bound_to_species_appropriately, universe_callback)
+{
+    yli::ontology::Console* const console = nullptr;
+
+    yli::ontology::UniverseStruct universe_struct;
+    universe_struct.is_headless = true;
+    yli::ontology::Universe* const universe = new yli::ontology::Universe(universe_struct);
+
+    yli::ontology::World* const world = new yli::ontology::World(universe, &universe->parent_of_worlds);
+
+    yli::ontology::SceneStruct scene_struct;
+    scene_struct.world = world;
+    yli::ontology::Scene* const scene = new yli::ontology::Scene(universe, scene_struct, &world->parent_of_scenes);
+
+    yli::ontology::ShaderStruct shader_struct;
+    shader_struct.parent = scene;
+    yli::ontology::Shader* const shader = new yli::ontology::Shader(universe, shader_struct);
+
+    yli::ontology::Material* const material = new yli::ontology::Material(universe, yli::ontology::MaterialStruct(), &shader->parent_of_materials);
+
+    const std::string species1_name = "foo";
+
+    yli::ontology::SpeciesStruct species1_struct;
+    species1_struct.scene = scene;
+    species1_struct.shader = shader;
+    species1_struct.material = material;
+    yli::ontology::Species* const species1 = new yli::ontology::Species(universe, species1_struct, &material->parent_of_species);
+    species1->set_name(species1_name);
+
+    const std::string object_name = "bar";
+
+    yli::ontology::ObjectStruct object_struct;
+    object_struct.species_parent = species1;
+    yli::ontology::Object* const object = new yli::ontology::Object(universe, object_struct, &species1->parent_of_objects);
+    object->set_name(object_name);
+
+    const std::string species2_name = "baz";
+
+    yli::ontology::SpeciesStruct species2_struct;
+    species2_struct.scene = scene;
+    species2_struct.shader = shader;
+    species2_struct.material = material;
+    yli::ontology::Species* const species2 = new yli::ontology::Species(universe, species2_struct, &material->parent_of_species);
+    species2->set_name(species2_name);
+
+    std::vector<std::string> bind_object_to_species2_command_parameters = { object_name, species2_name };
+
+    ASSERT_EQ(object->get_parent(), species1);
+    ASSERT_EQ(species1->get_number_of_children(), 1);
+    ASSERT_EQ(species2->get_number_of_children(), 0);
+
+    universe->bind(console, universe, bind_object_to_species2_command_parameters);
+    ASSERT_EQ(object->get_parent(), species2);
+    ASSERT_EQ(species1->get_number_of_children(), 0);
+    ASSERT_EQ(species2->get_number_of_children(), 1);
 }
