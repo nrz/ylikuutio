861ada2a4a74b8e002f0a57d957d5b46fc19ae72
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-05-05 01:30:10 +0300

    Updated `yli::common::Datatype`.

--- code/ylikuutio/common/datatype.hpp
+++ code/ylikuutio/common/datatype.hpp
@@ -53,35 +53,58 @@ namespace yli::common
         CONST_MOVABLE_POINTER,
         // `yli::ontology` datatypes, 'simple' path.
         UNIVERSE_POINTER,
+        CONST_UNIVERSE_POINTER,
         WORLD_POINTER,
+        CONST_WORLD_POINTER,
         SCENE_POINTER,
+        CONST_SCENE_POINTER,
         SHADER_POINTER,
+        CONST_SHADER_POINTER,
         MATERIAL_POINTER,
+        CONST_MATERIAL_POINTER,
         SPECIES_POINTER,
+        CONST_SPECIES_POINTER,
         OBJECT_POINTER,
+        CONST_OBJECT_POINTER,
         // `yli::ontology` datatypes, 'symbiont' path.
         SYMBIOSIS_POINTER,
+        CONST_SYMBIOSIS_POINTER,
         SYMBIONTMATERIAL_POINTER,
+        CONST_SYMBIONTMATERIAL_POINTER,
         SYMBIONTSPECIES_POINTER,
+        CONST_SYMBIONTSPECIES_POINTER,
         // `yli::ontology` datatypes, 'holobiont' path.
         HOLOBIONT_POINTER,
+        CONST_HOLOBIONT_POINTER,
         BIONT_POINTER,
+        CONST_BIONT_POINTER,
         // `yli::ontology` datatypes, 'text 2D' path.
         FONT2D_POINTER,
+        CONST_FONT2D_POINTER,
         TEXT2D_POINTER,
+        CONST_TEXT2D_POINTER,
         // `yli::ontology` datatypes, 'text 3D' path.
         VECTORFONT_POINTER,
+        CONST_VECTORFONT_POINTER,
         GLYPH_POINTER,
+        CONST_GLYPH_POINTER,
         TEXT3D_POINTER,
+        CONST_TEXT3D_POINTER,
         // `yli::ontology` datatypes, 'console' path.
         CONSOLE_POINTER,
+        CONST_CONSOLE_POINTER,
         // `yli::ontology` datatypes, 'compute' path.
         COMPUTETASK_POINTER,
+        CONST_COMPUTETASK_POINTER,
+        // `yli::config` datatypes.
+        SETTING_POINTER,
+        CONST_SETTING_POINTER,
         // `std::shared_ptr` objects.
         ANY_VALUE_SHARED_PTR,
         ANY_STRUCT_SHARED_PTR,
         // Structs.
         SPHERICAL_COORDINATES_STRUCT_POINTER,
+        CONST_SPHERICAL_COORDINATES_STRUCT_POINTER,
         // Raw pointers to standard library datatypes.
         STD_STRING_POINTER,
         CONST_STD_STRING_POINTER,
@@ -93,6 +116,7 @@ namespace yli::common
         STD_VECTOR_INT32_T_SHARED_PTR,
         STD_VECTOR_UINT32_T_SHARED_PTR,
         STD_VECTOR_FLOAT_SHARED_PTR,
+        STD_STRING_SHARED_PTR,
         // Shared pointers to GLM datatypes.
         GLM_VEC3_SHARED_PTR,
         GLM_VEC4_SHARED_PTR
