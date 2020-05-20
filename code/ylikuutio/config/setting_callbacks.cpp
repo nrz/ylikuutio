53907dc27c0d210b5db747396efd03104282758b
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-05-20 18:38:49 +0300

    `Setting::activate_background_color`: cleaning up code.

--- code/ylikuutio/config/setting_callbacks.cpp
+++ code/ylikuutio/config/setting_callbacks.cpp
@@ -167,16 +167,16 @@ namespace yli::config
             return nullptr;
         }
 
-        if (setting_master->setting_pointer_map.count("red") != 1 ||
-                setting_master->setting_pointer_map.count("green") != 1 ||
-                setting_master->setting_pointer_map.count("blue") != 1 ||
-                setting_master->setting_pointer_map.count("alpha") != 1)
+        if (!setting_master->is_setting("red") ||
+                !setting_master->is_setting("green") ||
+                !setting_master->is_setting("blue") ||
+                !setting_master->is_setting("alpha"))
         {
             return nullptr;
         }
 
         // red.
-        std::shared_ptr<yli::data::AnyValue> red_any_value = setting_master->setting_pointer_map["red"]->get();
+        std::shared_ptr<yli::data::AnyValue> red_any_value = setting_master->get("red")->get();
 
         if (red_any_value == nullptr || !std::holds_alternative<float>(red_any_value->data))
         {
@@ -186,7 +186,7 @@ namespace yli::config
         float red = std::get<float>(red_any_value->data);
 
         // green.
-        std::shared_ptr<yli::data::AnyValue> green_any_value = setting_master->setting_pointer_map["green"]->get();
+        std::shared_ptr<yli::data::AnyValue> green_any_value = setting_master->get("green")->get();
 
         if (green_any_value == nullptr || !std::holds_alternative<float>(green_any_value->data))
         {
@@ -196,7 +196,7 @@ namespace yli::config
         float green = std::get<float>(green_any_value->data);
 
         // blue.
-        std::shared_ptr<yli::data::AnyValue> blue_any_value = setting_master->setting_pointer_map["blue"]->get();
+        std::shared_ptr<yli::data::AnyValue> blue_any_value = setting_master->get("blue")->get();
 
         if (blue_any_value == nullptr || !std::holds_alternative<float>(blue_any_value->data))
         {
@@ -206,7 +206,7 @@ namespace yli::config
         float blue = std::get<float>(blue_any_value->data);
 
         // alpha.
-        std::shared_ptr<yli::data::AnyValue> alpha_any_value = setting_master->setting_pointer_map["alpha"]->get();
+        std::shared_ptr<yli::data::AnyValue> alpha_any_value = setting_master->get("alpha")->get();
 
         if (alpha_any_value == nullptr || !std::holds_alternative<float>(alpha_any_value->data))
         {
