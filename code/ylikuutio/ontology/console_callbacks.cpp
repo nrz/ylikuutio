7925d308e2f0f527cd91390a7f09555012177d00
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-06-25 23:35:05 +0300

    Edit whitespace.

--- code/ylikuutio/ontology/console_callbacks.cpp
+++ code/ylikuutio/ontology/console_callbacks.cpp
@@ -25,12 +25,12 @@
 
 // Include standard headers
 #include <algorithm> // std::copy etc.
-#include <iterator> // std::back_inserter
-#include <list>     // std::list
-#include <memory>   // std::make_shared, std::shared_ptr
-#include <stdint.h> // uint32_t etc.
-#include <string>   // std::string, std::getline
-#include <vector>   // std::vector
+#include <iterator>  // std::back_inserter
+#include <list>      // std::list
+#include <memory>    // std::make_shared, std::shared_ptr
+#include <stdint.h>  // uint32_t etc.
+#include <string>    // std::string, std::getline
+#include <vector>    // std::vector
 
 namespace yli::callback
 {
