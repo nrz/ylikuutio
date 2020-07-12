516df72613f1eb567dd5ca00599bd0e24449c12b
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-07-13 01:12:18 +0300

    Bugfix: add missing forward declaration.

--- code/ylikuutio/time/time.hpp
+++ code/ylikuutio/time/time.hpp
@@ -15,7 +15,12 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#ifndef __TIME_HPP_INCLUDED
+#define __TIME_HPP_INCLUDED
+
 namespace yli::time
 {
     double get_time();
 }
+
+#endif
