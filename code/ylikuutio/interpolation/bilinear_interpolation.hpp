c12d99651e57df680e35bb4cd68ef2ace812f118
Antti Nuortimo <antti.nuortimo@gmail.com>
2020-09-01 16:45:37 +0300

    Implement proper bilinear interpolation.
    Currently only shrinking is supported. Enlarging is not supported.

--- code/ylikuutio/interpolation/bilinear_interpolation.hpp
+++ code/ylikuutio/interpolation/bilinear_interpolation.hpp
@@ -18,74 +18,125 @@
 #ifndef __YLIKUUTIO_INTERPOLATION_BILINEAR_INTERPOLATION_HPP_INCLUDED
 #define __YLIKUUTIO_INTERPOLATION_BILINEAR_INTERPOLATION_HPP_INCLUDED
 
+#include "bilinear_interpolation_struct.hpp"
+
 // Include standard headers
-#include <stdint.h> // uint32_t etc.
-#include <math.h>   // ceil, floor etc.
+#include <cmath>    // NAN, std::ceil, std::floor, std::isnan
+#include <cstddef>  // std::ptrdiff_t, std::size_t
+#include <iostream> // std::cout, std::cin, std::cerr
+#include <limits>   // std::numeric_limits
+#include <memory>   // std::make_shared, std::shared_ptr
+#include <vector>   // std::vector
 
 namespace yli::interpolation
 {
     template<class T1>
-        bool interpolate_bilinearly(
-                T1* input_vertex_pointer,
-                float* output_vertex_pointer,
-                uint32_t image_width,
-                uint32_t image_height,
-                float start_x,
-                float start_z,
-                float end_x,
-                float end_z,
-                float delta_x,
-                float delta_z)
+        float sample2D(
+                const T1* source_image,
+                const float image_width,
+                const float image_height,
+                const float x,
+                const float z)
         {
-            if (start_x < 0.0f || start_x > static_cast<float>(image_width) ||
-                    end_x < 0.0f || end_x > static_cast<float>(image_width) ||
-                    start_z < 0.0f || start_z > static_cast<float>(image_height) ||
-                    end_z < 0.0f || end_z > static_cast<float>(image_height))
+            // (x,z) coordinates.
+            // First texel extends from (0, 0) to (1, 1).
+            // Sampling is always done from the center of the texel.
+
+            const float floor_x = std::floor(x);
+            const float floor_z = std::floor(z);
+
+            const float weight_x0 = (x - floor_x >= 0.5f ? 1.0f - (x - floor_x - 0.5f) : 0.5f - (x - floor_x)); // 0 <= weight_x0 <= 1
+            const float weight_x1 = 1.0f - weight_x0;                                                           // 0 <= weight_x1 <= 1
+
+            const float weight_z0 = (z - floor_z >= 0.5f ? 1.0f - (z - floor_z - 0.5f) : 0.5f - (z - floor_z)); // 0 <= weight_z0 <= 1
+            const float weight_z1 = 1.0f - weight_z0;                                                           // 0 <= weight_z1 <= 1
+
+            const std::size_t x0 = (x - floor_x >= 0.5f ? static_cast<std::size_t>(floor_x) : static_cast<std::size_t>(floor_x - 1));
+            const std::size_t x1 = (x - floor_x <= 0.5f ? static_cast<std::size_t>(floor_x) : static_cast<std::size_t>(floor_x + 1));
+
+            const std::size_t z0 = (z - floor_z >= 0.5f ? static_cast<std::size_t>(floor_z) : static_cast<std::size_t>(floor_z - 1));
+            const std::size_t z1 = (z - floor_z <= 0.5f ? static_cast<std::size_t>(floor_z) : static_cast<std::size_t>(floor_z + 1));
+
+            const std::ptrdiff_t data_sw_ptrdiff_t = static_cast<ptrdiff_t>(image_width * z0 + x0); // southwest.
+            const std::ptrdiff_t data_se_ptrdiff_t = static_cast<ptrdiff_t>(image_width * z0 + x1); // southeast.
+            const std::ptrdiff_t data_nw_ptrdiff_t = static_cast<ptrdiff_t>(image_width * z1 + x0); // northwest.
+            const std::ptrdiff_t data_ne_ptrdiff_t = static_cast<ptrdiff_t>(image_width * z1 + x1); // northeast.
+            const float data_sw = static_cast<float>(*(source_image + data_sw_ptrdiff_t));
+            const float data_se = static_cast<float>(*(source_image + data_se_ptrdiff_t));
+            const float data_nw = static_cast<float>(*(source_image + data_nw_ptrdiff_t));
+            const float data_ne = static_cast<float>(*(source_image + data_ne_ptrdiff_t));
+
+            // Interpolate.
+            return weight_x0 * weight_z0 * data_sw + weight_x1 * weight_z0 * data_se + weight_x0 * weight_z1 * data_nw + weight_x1 * weight_z1 * data_ne;
+        }
+
+    template<class T1>
+        std::shared_ptr<std::vector<float>> interpolate_bilinearly(
+                const T1* source_image,
+                const yli::interpolation::BilinearInterpolationStruct& bilinear_interpolation_struct)
+        {
+            const std::size_t& input_image_width = bilinear_interpolation_struct.input_image_width;
+
+            if (input_image_width == 0)
             {
-                // invalid value in some input variable.
-                return false;
+                std::cerr << "ERROR: `yli::interpolation::interpolate_bilinearly2D`: the value of `input_image_width` is 0.\n";
+                return nullptr;
             }
 
-            // If `start_x` > `end_x` or if `start_z` > `end_z`, then no output will be produced.
-            if (start_x > end_x || start_z > end_z)
+            const std::size_t& input_image_height = bilinear_interpolation_struct.input_image_height;
+
+            if (input_image_height == 0)
             {
-                return false;
+                std::cerr << "ERROR: `yli::interpolation::interpolate_bilinearly2D`: the value of `input_image_height` is 0.\n";
+                return nullptr;
             }
 
-            // `start_x`, `end_x`, `start_z`, and `end_z` are all inclusive.
-            // With inclusive coordinates it's easier to eg. keep the original data.
+            const std::size_t& output_image_width = bilinear_interpolation_struct.output_image_width;
 
-            for (float z = start_z; z <= end_z; z += delta_z)
+            if (output_image_width == 0)
             {
-                const float floor_z = floor(z);
-                const float ceil_z = ceil(z);
-                const float rel_z = z - floor_z; // 0 <= rel_z <= 1
-                const float inv_rel_z = 1.0f - rel_z;
-                const uint32_t z0 = static_cast<uint32_t>(floor_z);
-                const uint32_t z1 = static_cast<uint32_t>(ceil_z);
+                std::cerr << "ERROR: `yli::interpolation::interpolate_bilinearly2D`: the value of `output_image_width` is 0.\n";
+                return nullptr;
+            }
 
-                // Interpolate along z axis.
-                for (float x = start_x; x <= end_x; x += delta_x)
+            const std::size_t& output_image_height = bilinear_interpolation_struct.output_image_height;
+
+            if (output_image_height == 0)
+            {
+                std::cerr << "ERROR: `yli::interpolation::interpolate_bilinearly2D`: the value of `output_image_height` is 0.\n";
+                return nullptr;
+            }
+
+            if (output_image_width > input_image_width || output_image_height > input_image_height)
+            {
+                std::cerr << "ERROR: `yli::interpolation::interpolate_bilinearly2D`: enlarging is not currently supported!\n";
+                return nullptr;
+            }
+
+            const std::size_t n_output_vertices = output_image_width * output_image_height;
+            const std::ptrdiff_t n_output_vertices_ptrdiff_t = static_cast<std::ptrdiff_t>(n_output_vertices);
+
+            std::shared_ptr<std::vector<float>> output_vector = std::make_shared<std::vector<float>>();
+            output_vector->reserve(n_output_vertices);
+
+            const float delta_x = static_cast<float>(input_image_width) / static_cast<float>(output_image_width);
+            const float delta_z = static_cast<float>(input_image_height) / static_cast<float>(output_image_height);
+
+            float z = delta_z / 2.0f;
+
+            for (std::size_t z_index = 0; z_index < output_image_height; z_index++, z += delta_z)
+            {
+                float x = delta_x / 2.0f;
+
+                for (std::size_t x_index = 0; x_index < output_image_width; x_index++, x += delta_x)
                 {
-                    // https://en.wikipedia.org/wiki/Bilinear_interpolation#Unit_Square
-                    const float floor_x = floor(x);
-                    const float ceil_x = ceil(x);
-                    const float rel_x = x - floor_x; // 0 <= rel_x <= 1
-                    const float inv_rel_x = 1.0f - rel_x;
-                    const uint32_t x0 = static_cast<uint32_t>(floor_x);
-                    const uint32_t x1 = static_cast<uint32_t>(ceil_x);
+                    const float value = yli::interpolation::sample2D(source_image, input_image_width, input_image_height, x, z);
 
-                    const float data_0_0 = static_cast<float>(*(input_vertex_pointer + image_width * z0 + x0));
-                    const float data_0_1 = static_cast<float>(*(input_vertex_pointer + image_width * z1 + x0));
-                    const float data_1_0 = static_cast<float>(*(input_vertex_pointer + image_width * z0 + x1));
-                    const float data_1_1 = static_cast<float>(*(input_vertex_pointer + image_width * z1 + x1));
-
-                    const float value = inv_rel_x * inv_rel_z * data_0_0 + rel_x * inv_rel_z * data_1_0 + inv_rel_x * rel_z * data_0_1 + rel_x * rel_z * data_1_1;
-                    *output_vertex_pointer++ = value;
+                    output_vector->emplace_back(value);
                 }
             }
 
-            return true;
+            return output_vector;
         }
 }
 
