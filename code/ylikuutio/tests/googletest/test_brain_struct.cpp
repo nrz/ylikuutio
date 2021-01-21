c16e124ae7d510259f32489f92c0beb4b0534c17
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-01-21 22:09:47 +0200

    Test for `yli::ontology::BrainStruct`.

---
+++ code/ylikuutio/tests/googletest/test_brain_struct.cpp
@@ -0,0 +1,26 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2021 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "gtest/gtest.h"
+#include "code/ylikuutio/ontology/brain_struct.hpp"
+
+TEST(brain_struct_must_be_initialized_appropriately, brain_struct)
+{
+    const yli::ontology::BrainStruct test_brain_struct;
+    ASSERT_EQ(test_brain_struct.parent, nullptr);
+    ASSERT_EQ(test_brain_struct.callback_engine, nullptr);
+}
