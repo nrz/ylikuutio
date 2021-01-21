6d0f470d9bb3077fc9fe3de242cbc879a4370a33
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-01-21 23:30:19 +0200

    Cleaning up `SpeciesStruct` code.
    Edited comments.

--- code/ylikuutio/ontology/species_struct.hpp
+++ code/ylikuutio/ontology/species_struct.hpp
@@ -44,26 +44,6 @@ namespace yli::ontology
     struct SpeciesStruct: public yli::ontology::ModelStruct
     {
         SpeciesStruct()
-            : triangulation_type("bilinear_interpolation"),
-            light_position(glm::vec3(0.0f, 0.0f, 0.0f)),
-            scene(nullptr),
-            shader(nullptr),
-            material(nullptr),
-            symbiont_material(nullptr),
-            shapeshifter_transformation(nullptr),
-            vertex_count(std::numeric_limits<std::size_t>::max()), // `std::numeric_limits<std::size_t>::max()` means that `vertex_count` is not defined.
-            mesh_i(0),
-            x_step(1),
-            z_step(1),
-            latitude(0.0f),
-            longitude(0.0f),
-            planet_radius(NAN),
-            divisor(1.0f),
-            water_level(-1.0f * std::numeric_limits<float>::infinity()),
-            is_terrain(false),
-            is_symbiont_species(false),
-            opengl_in_use(true),
-            use_real_texture_coordinates(true)
         {
             // constructor.
         }
@@ -76,29 +56,29 @@ namespace yli::ontology
                                                             // `"srtm"`/`"SRTM"` - SRTM heightmap.
                                                             // `"asc"`/`"ascii_grid"`/`"ASCII_grid"` - ASCII grid.
         std::string color_channel;                          // color channel to use for altitude data, for BMP model files.
-        std::string triangulation_type;                     // `"bilinear_interpolation"`, `"southwest_northeast_edges"`, `"southeast_northwest_edges"`.
+        std::string triangulation_type { "bilinear_interpolation" }; // `"bilinear_interpolation"`, `"southwest_northeast_edges"`, `"southeast_northwest_edges"`.
         std::vector<glm::vec3> vertices;                    // for `SymbiontSpecies`.
         std::vector<glm::vec2> uvs;                         // for `SymbiontSpecies`.
         std::vector<glm::vec3> normals;                     // for `SymbiontSpecies`.
-        glm::vec3 light_position;                           // light position.
-        yli::ontology::Scene* scene;                        // pointer to `Scene` object.
-        yli::ontology::Shader* shader;                      // pointer to `Shader` object.
-        yli::ontology::Material* material;                  // pointer to `Material` object.
-        yli::ontology::SymbiontMaterial* symbiont_material; // pointer to `SymbiontMaterial` object.
-        yli::ontology::ShapeshifterTransformation* shapeshifter_transformation; // pointer to `ShapeshifterTransformation` object.
-        std::size_t vertex_count;                           // for `SymbiontSpecies`.
-        std::size_t mesh_i;                                 // for FBX.
-        std::size_t x_step;                                 // step in x-dimension for input data (set to 1 to load all data points/measurements).
-        std::size_t z_step;                                 // step in z-dimension for input data (set to 1 to load all data points/measurements).
-        float latitude;                                     // in degrees, for SRTM model files.
-        float longitude;                                    // in degrees, for SRTM model files.
-        float planet_radius;                                // radius of sea level in kilometers. used only for terrains (planets and moons). `6371.0f` for Earth.
-        float divisor;                                      // value by which SRTM values are divided to convert them to kilometers.
-        float water_level;                                  // water level in meters. used only for terrains (planets and moons).
-        bool is_terrain;                                    // Terrains (planets and moons) currently neither rotate nor translate.
-        bool is_symbiont_species;
-        bool opengl_in_use;                                 // If `opengl_in_use` is `false, then no OpenGL-specific code shall be executed.
-        bool use_real_texture_coordinates;
+        glm::vec3 light_position { glm::vec3(0.0f, 0.0f, 0.0f) }; // light position.
+        yli::ontology::Scene* scene { nullptr };                                            // pointer to `Scene` object.
+        yli::ontology::Shader* shader { nullptr };                                          // pointer to `Shader` object.
+        yli::ontology::Material* material { nullptr };                                      // pointer to `Material` object.
+        yli::ontology::SymbiontMaterial* symbiont_material { nullptr };                     // pointer to `SymbiontMaterial` object.
+        yli::ontology::ShapeshifterTransformation* shapeshifter_transformation { nullptr }; // pointer to `ShapeshifterTransformation` object.
+        std::size_t vertex_count { std::numeric_limits<std::size_t>::max() };               // for `SymbiontSpecies`.
+        std::size_t mesh_i { 0 };    // for FBX.
+        std::size_t x_step { 1 };    // step in x-dimension for input data (set to 1 to load all data points/measurements).
+        std::size_t z_step { 1 };    // step in z-dimension for input data (set to 1 to load all data points/measurements).
+        float latitude  { 0.0f };    // in degrees, for SRTM model files.
+        float longitude { 0.0f };    // in degrees, for SRTM model files.
+        float planet_radius { NAN }; // radius of sea level in kilometers. used only for terrains (planets and moons). `6371.0f` for Earth.
+        float divisor { 1.0f };;     // value by which SRTM values are divided to convert them to kilometers.
+        float water_level { -1.0f * std::numeric_limits<float>::infinity() }; // water level in meters. used only for terrains (planets and moons).
+        bool is_terrain { false };                                            // Terrains (planets and moons) currently neither rotate nor translate.
+        bool is_symbiont_species { false };                                   // By default `Species` are not `SymbiontSpecies`.
+        bool opengl_in_use { true };                                          // If `opengl_in_use` is `false`, then no OpenGL-specific code shall be executed.
+        bool use_real_texture_coordinates { true };
     };
 }
 
