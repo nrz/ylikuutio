e2c895673900f6b531ba39362fc8a721ab16df82
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-03-21 21:00:45 +0200

    Add tests for PNG loading.
    Current tests for PNG loading are simply copied from tests for BMP
    loading.
    
    Add also test files for PNG loading, created with ImageMagick `convert`.

---
+++ code/ylikuutio/load/png_heightmap_loader.hpp
@@ -0,0 +1,46 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2021 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef __YLIKUUTIO_LOAD_PNG_HEIGHTMAP_LOADER_HPP_INCLUDED
+#define __YLIKUUTIO_LOAD_PNG_HEIGHTMAP_LOADER_HPP_INCLUDED
+
+#include "heightmap_loader_struct.hpp"
+
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
+// Include standard headers
+#include <cstddef>  // std::size_t
+#include <string>   // std::string
+#include <vector>   // std::vector
+
+namespace yli::load
+{
+    bool load_png_terrain(
+            const yli::load::HeightmapLoaderStruct& heightmap_loader_struct,
+            std::vector<glm::vec3>& out_vertices,
+            std::vector<glm::vec2>& out_uvs,
+            std::vector<glm::vec3>& out_normals,
+            std::size_t& image_width,
+            std::size_t& image_height,
+            const std::string& color_channel);
+}
+
+#endif
