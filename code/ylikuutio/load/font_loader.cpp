36596fee41599eb7dd5ca832000f44bf068476a9
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-07-20 00:27:37 +0300

    `SVG_data` -> `svg_data`.

--- code/ylikuutio/load/font_loader.cpp
+++ code/ylikuutio/load/font_loader.cpp
@@ -455,15 +455,15 @@ namespace yli::load
     {
         const std::shared_ptr<std::string> file_content = yli::file::slurp(filename);
         const std::size_t file_size = file_content->size();
-        std::shared_ptr<std::vector<char>> SVG_data = std::make_shared<std::vector<char>>();
-        SVG_data->reserve(file_size);
+        std::shared_ptr<std::vector<char>> svg_data = std::make_shared<std::vector<char>>();
+        svg_data->reserve(file_size);
 
-        std::strncpy(&(*SVG_data)[0], file_content->c_str(), file_size);
+        std::strncpy(&(*svg_data)[0], file_content->c_str(), file_size);
 
         bool is_first_glyph_found;
 
-        const char* const svg_base_pointer { &(*SVG_data)[0] };
-        const char* svg_data_pointer       { &(*SVG_data)[0] };
+        const char* const svg_base_pointer { &(*svg_data)[0] };
+        const char* svg_data_pointer       { &(*svg_data)[0] };
 
         // SVG files are XML files, so we just need to read until we find the data we need.
         is_first_glyph_found = yli::load::find_first_glyph_in_svg(svg_base_pointer, svg_data_pointer, file_size);
