2eef58c3e70451a1ce606513b9df81e571d288ed
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-07-20 19:27:13 +0300

    Bugfix: `ObjectStruct`: remove field with the same name as in parent.

--- code/ylikuutio/ontology/object_struct.hpp
+++ code/ylikuutio/ontology/object_struct.hpp
@@ -25,7 +25,6 @@
 
 namespace yli::ontology
 {
-    class Brain;
     class Species;
     class ShapeshifterSequence;
     class Glyph;
@@ -53,7 +52,6 @@ namespace yli::ontology
 
         std::variant<yli::ontology::Species*, yli::ontology::ShapeshifterSequence*, yli::ontology::Text3D*> parent;
         yli::ontology::Glyph* glyph { nullptr }; // pointer to the `Glyph` (not a parent!).
-        yli::ontology::Brain* brain { nullptr }; // pointer to the `Brain` master.
     };
 }
 
