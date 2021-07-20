fe6709cc893ff42228df916f6c8736d83fa07c83
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-07-20 11:12:34 +0300

    Bugfix: use `const` references for passing parameters.

--- code/ylikuutio/geometry/line_3d.hpp
+++ code/ylikuutio/geometry/line_3d.hpp
@@ -35,19 +35,19 @@ namespace yli::geometry
     {
         public:
             // constructor.
-            Line3D(const std::vector<float> point1, const std::vector<float> point2)
+            Line3D(const std::vector<float>& point1, const std::vector<float>& point2)
                 : Line(point1, point2)
             {
             }
 
             // constructor.
-            Line3D(const std::vector<float> general_form_coefficients, const float general_form_constant)
+            Line3D(const std::vector<float>& general_form_coefficients, const float general_form_constant)
                 : Line(general_form_coefficients, general_form_constant)
             {
             }
 
-            bool do_lines_3d_intersect(const yli::geometry::Line3D* const line1, const yli::geometry::Line3D* const line2) const;
-            bool do_lines_3d_intersect(const yli::geometry::Line3D* const line) const;
+            bool do_lines_3d_intersect(const yli::geometry::Line3D& line1, const yli::geometry::Line3D& line2) const;
+            bool do_lines_3d_intersect(const yli::geometry::Line3D& line) const;
     };
 }
 
