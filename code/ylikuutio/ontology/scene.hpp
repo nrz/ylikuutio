7bd4757c199e1eae8a61526cb0db74467a15702d
Antti Nuortimo <antti.nuortimo@gmail.com>
2021-09-27 22:56:50 +0300

    `Scene` constructor: add `std::cerr` error output.

--- code/ylikuutio/ontology/scene.hpp
+++ code/ylikuutio/ontology/scene.hpp
@@ -43,6 +43,7 @@
 #include <queue>   // std::priority_queue, std::queue
 #include <string>  // std::string
 #include <vector>  // std::vector
+#include <iostream> // std::cout, std::cin, std::cerr
 
 // How `yli::ontology::Scene` class works:
 //
@@ -119,6 +120,12 @@ namespace yli::ontology
                     this->dynamics_world->setGravity(btVector3(0.0f, -this->gravity, 0.0f));
 
                     // Bullet is now initialized for this `Scene`.
+                }
+                else if (this->universe == nullptr)
+                {
+                    // Dynamics world could not be created.
+
+                    std::cerr << "ERROR: `Scene::Scene`: `this->universe` is `nullptr`!\n";
                 }
 
                 // `yli::ontology::Entity` member variables begin here.
