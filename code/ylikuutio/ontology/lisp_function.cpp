nrz/ylikuutio

commit 6466586257c16c768dd86111aab30e2f44005d05

Use `std::optional` for the return value of `LispFunction::execute`.
`LispFunctionOverload::execute`:
* Return `std::nullopt` in case of failed preconditions for a successful
  argument binding.

code/ylikuutio/ontology/lisp_function.cpp
@@ -23,6 +23,7 @@
 // Include standard headers
 #include <cstddef>    // std::size_t
 #include <memory>     // std::make_shared, std::shared_ptr
+#include <optional>   // std::optional
 #include <string>     // std::string
 #include <vector>     // std::vector
 
@@ -57,7 +58,7 @@ namespace yli::ontology
         return yli::ontology::get_number_of_descendants(this->parent_of_generic_lisp_function_overloads.child_pointer_vector);
     }
 
-    std::shared_ptr<yli::data::AnyValue> LispFunction::execute(const std::vector<std::string>& parameter_vector)
+    std::optional<std::shared_ptr<yli::data::AnyValue>> LispFunction::execute(const std::vector<std::string>& parameter_vector)
     {
         // The execution of a `LispFunction` proceeds as follows:
         // The execution of `GenericLispFunctionOverload` children of
@@ -81,15 +82,9 @@ namespace yli::ontology
                 it != this->parent_of_generic_lisp_function_overloads.child_pointer_vector.end();
                 ++it)
         {
-            auto [success, return_value] =
-                static_cast<yli::ontology::GenericLispFunctionOverload*>(*it)->execute(parameter_vector);
-
-            if (success)
-            {
-                return return_value; // Return the return value of the callback.
-            }
+            return static_cast<yli::ontology::GenericLispFunctionOverload*>(*it)->execute(parameter_vector);
         }
 
-        return nullptr;
+        return std::nullopt;
     }
 }

code/ylikuutio/lisp/executor.cpp
@@ -23,6 +23,7 @@
 
 // Include standard headers
 #include <memory> // std::make_shared, std::shared_ptr
+#include <optional> // std::optional
 #include <string> // std::string, std::getline
 #include <vector> // std::vector
 
@@ -52,7 +53,12 @@ namespace yli::lisp
 
                 if (lisp_function != nullptr)
                 {
-                    any_value = lisp_function->execute(parameter_vector);
+                    std::optional<std::shared_ptr<yli::data::AnyValue>> return_value = lisp_function->execute(parameter_vector);
+
+                    if (*return_value)
+                    {
+                        any_value = *return_value;
+                    }
                 }
             }
         }

code/ylikuutio/ontology/generic_lisp_function_overload.hpp
@@ -26,8 +26,8 @@
 #include <cstddef>    // std::size_t
 #include <functional> // std::function
 #include <memory>     // std::make_shared, std::shared_ptr
+#include <optional>   // std::optional
 #include <string>     // std::string
-#include <utility>    // std::pair
 #include <vector>     // std::vector
 
 namespace yli::data
@@ -65,7 +65,7 @@ namespace yli::ontology
 
             yli::ontology::Entity* get_parent() const override;
 
-            virtual std::pair<bool, std::shared_ptr<yli::data::AnyValue>> execute(const std::vector<std::string>& parameter_vector) = 0;
+            virtual std::optional<std::shared_ptr<yli::data::AnyValue>> execute(const std::vector<std::string>& parameter_vector) = 0;
 
             yli::ontology::ChildModule child_of_lisp_function;
 

code/ylikuutio/ontology/lisp_function.hpp
@@ -26,6 +26,7 @@
 // Include standard headers
 #include <cstddef>    // std::size_t
 #include <memory>     // std::make_shared, std::shared_ptr
+#include <optional>   // std::optional
 #include <string>     // std::string
 #include <vector>     // std::vector
 
@@ -65,7 +66,7 @@ namespace yli::ontology
 
             yli::ontology::Entity* get_parent() const override;
 
-            std::shared_ptr<yli::data::AnyValue> execute(const std::vector<std::string>& parameter_vector);
+            std::optional<std::shared_ptr<yli::data::AnyValue>> execute(const std::vector<std::string>& parameter_vector);
 
             yli::ontology::ChildModule child_of_console;
             yli::ontology::ParentModule parent_of_generic_lisp_function_overloads;

code/ylikuutio/ontology/lisp_function_overload.hpp
@@ -29,9 +29,9 @@
 #include <functional> // std::function, std::invoke
 #include <iostream>   // std::cout, std::cin, std::cerr
 #include <memory>     // std::make_shared, std::shared_ptr
+#include <optional>   // std::optional
 #include <string>     // std::string
 #include <tuple>      // std::apply, std::tuple, std::tuple_cat
-#include <utility>    // std::pair
 #include <vector>     // std::vector
 
 // This is a bit complicated, as the callback may receive different kinds of arguments.
@@ -107,14 +107,14 @@ namespace yli::ontology
                 // destructor.
             }
 
-            std::pair<bool, std::shared_ptr<yli::data::AnyValue>> execute(const std::vector<std::string>& parameter_vector) override
+            std::optional<std::shared_ptr<yli::data::AnyValue>> execute(const std::vector<std::string>& parameter_vector) override
             {
                 yli::ontology::Universe* const universe = this->get_universe();
 
                 if (universe == nullptr)
                 {
                     std::cerr << "ERROR: `LispFunctionOverload::execute`: `universe` is `nullptr`!\n";
-                    return std::pair(false, nullptr);
+                    return std::nullopt;
                 }
 
                 yli::ontology::Entity* const lisp_function_entity = this->child_of_lisp_function.get_parent();
@@ -124,7 +124,7 @@ namespace yli::ontology
                 if (lisp_function == nullptr)
                 {
                     std::cerr << "ERROR: `LispFunctionOverload::execute`: `lisp_function` is `nullptr`!\n";
-                    return std::pair(false, nullptr);
+                    return std::nullopt;
                 }
 
                 yli::ontology::Entity* const console_entity = lisp_function->get_parent();
@@ -132,7 +132,7 @@ namespace yli::ontology
                 if (console_entity == nullptr)
                 {
                     std::cerr << "ERROR: `LispFunctionOverload::execute`: `console_entity` is `nullptr`!\n";
-                    return std::pair(false, nullptr);
+                    return std::nullopt;
                 }
 
                 yli::ontology::Console* const console = dynamic_cast<yli::ontology::Console*>(console_entity);
@@ -140,7 +140,7 @@ namespace yli::ontology
                 if (console == nullptr)
                 {
                     std::cerr << "ERROR: `LispFunctionOverload::execute`: `console` is `nullptr`!\n";
-                    return std::pair(false, nullptr);
+                    return std::nullopt;
                 }
 
                 // OK, all preconditions for a successful argument binding are met.
@@ -151,10 +151,10 @@ namespace yli::ontology
                 {
 
                     // Call the callback function if binding was successful.
-                    return std::pair(true, std::apply(this->callback, arg_tuple));
+                    return std::apply(this->callback, arg_tuple);
                 }
 
-                return std::pair(false, nullptr);
+                return std::nullopt;
             }
 
         private:

commit 6b0aa70c127680bd7fc13f4a795f8bdecc1c097f

Bugfix: try all overloads and not only the first one.
This bug was introduced in commit
6466586257c16c768dd86111aab30e2f44005d05.

code/ylikuutio/ontology/lisp_function.cpp
@@ -81,7 +81,12 @@ namespace yli::ontology
                 it != this->parent_of_generic_lisp_function_overloads.child_pointer_vector.end();
                 ++it)
         {
-            return static_cast<yli::ontology::GenericLispFunctionOverload*>(*it)->execute(parameter_vector);
+            std::optional<yli::data::AnyValue> result = static_cast<yli::ontology::GenericLispFunctionOverload*>(*it)->execute(parameter_vector);
+
+            if (result)
+            {
+                return result;
+            }
         }
 
         return std::nullopt;