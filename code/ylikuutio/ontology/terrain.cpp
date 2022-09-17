64e131d5ce1877b0f2de9805d758aab582f6c8ea
Antti Nuortimo <antti.nuortimo@gmail.com>
2022-09-18 01:32:27 +0300

    New class: `yli::ontology::Terrain`.
    Currently only a derivation of `yli::ontology::Object`.
    Work in progress.

---
+++ code/ylikuutio/ontology/terrain.cpp
@@ -0,0 +1,41 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "terrain.hpp"
+#include "object.hpp"
+
+namespace yli::ontology
+{
+    class Universe;
+    class GenericParentModule;
+    class GenericMasterModule;
+    struct HolobiontStruct;
+
+    Terrain::Terrain(
+            yli::ontology::Universe& universe,
+            const yli::ontology::ObjectStruct& terrain_struct,
+            yli::ontology::GenericParentModule* const scene_parent,
+            yli::ontology::GenericMasterModule* const mesh_master,
+            yli::ontology::GenericMasterModule* const /* brain_master */)
+        : yli::ontology::Object(universe, terrain_struct, scene_parent, mesh_master, nullptr)
+    {
+        // constructor.
+
+        // `yli::ontology::Entity` member variables begin here.
+        this->type_string = "yli::ontology::Terrain*";
+    }
+}
