f2d4f9df7929307fee09ee2443d23351ab0a116f
Antti Nuortimo <antti.nuortimo@gmail.com>
2022-09-18 01:08:40 +0300

    New classes: `LocomotionModule` and `LocomotionEnvironment`.
    These locomotion-related classes lay the groundwork for the upcoming
    data-oriented locomotion. Locomotion is a work in progress.

---
+++ code/ylikuutio/ontology/locomotion_module.hpp
@@ -0,0 +1,38 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef __YLIKUUTIO_ONTOLOGY_LOCOMOTION_MODULE_HPP_INCLUDED
+#define __YLIKUUTIO_ONTOLOGY_LOCOMOTION_MODULE_HPP_INCLUDED
+
+#include "locomotion_environment.hpp"
+
+namespace yli::ontology
+{
+    enum class LocomotionEnvironment;
+
+    class LocomotionModule
+    {
+        public:
+            explicit LocomotionModule(yli::ontology::LocomotionEnvironment environment);
+            yli::ontology::LocomotionEnvironment get_environment() const;
+
+        private:
+            yli::ontology::LocomotionEnvironment environment;
+    };
+}
+
+#endif
---
+++ code/ylikuutio/ontology/locomotion_module_struct.hpp
@@ -0,0 +1,41 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef __YLIKUUTIO_ONTOLOGY_LOCOMOTION_STRUCT_HPP_INCLUDED
+#define __YLIKUUTIO_ONTOLOGY_LOCOMOTION_STRUCT_HPP_INCLUDED
+
+namespace yli::ontology
+{
+    struct LocomotionModuleStruct
+    {
+        LocomotionModuleStruct() = default;
+
+        LocomotionModuleStruct(const float max_speed)
+            : max_speed(max_speed)
+        {
+        }
+
+        float max_speed            { 0.0f }; // m/s.
+        float acceleration         { 0.0f }; // m/s^2
+        float deceleration         { 0.0f }; // m/s^2
+        float max_angular_speed    { 0.0f }; // 1/s
+        float angular_acceleration { 0.0f }; // 1/s^2
+        float angular_deceleration { 0.0f }; // 1/s^2
+    };
+}
+
+#endif
