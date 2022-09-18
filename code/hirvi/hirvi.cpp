nrz/ylikuutio

commit 1821a2e1cd67b268b73e7708f92616937433764a

Symbioses can be shared between `Scene`s by using `Ecosystem`s.
`Symbiosis` Entities contain complete 3D models which may include
several meshes and several textures.

Also enable creating `Species` and `Symbiosis` Entities that are
immediately children of an `Ecosystem`.

Ajokki sandbox app: create Earth `Ecosystem` in a separate file.

CMakeLists.txt
@@ -780,6 +780,8 @@ add_executable(ajokki
     code/ajokki/ajokki_altiplano_scene.hpp
     code/ajokki/ajokki_console_callbacks.cpp
     code/ajokki/ajokki_console_callbacks.hpp
+    code/ajokki/ajokki_earth_ecosystem.cpp
+    code/ajokki/ajokki_earth_ecosystem.hpp
     code/ajokki/ajokki_helsinki_east_downtown_scene.cpp
     code/ajokki/ajokki_helsinki_east_downtown_scene.hpp
     code/ajokki/ajokki_joensuu_center_west_scene.cpp

code/ajokki/ajokki.cpp
@@ -29,6 +29,7 @@
 #endif
 
 #include "ajokki_console_callbacks.hpp"
+#include "ajokki_earth_ecosystem.hpp"
 #include "ajokki_helsinki_east_downtown_scene.hpp"
 #include "ajokki_joensuu_center_west_scene.hpp"
 #include "code/ylikuutio/audio/audio_system.hpp"
@@ -68,6 +69,7 @@
 #include "code/ylikuutio/ontology/variable_struct.hpp"
 #include "code/ylikuutio/ontology/universe_struct.hpp"
 #include "code/ylikuutio/ontology/ecosystem_struct.hpp"
+#include "code/ylikuutio/ontology/model_struct.hpp"
 #include "code/ylikuutio/ontology/console_struct.hpp"
 #include "code/ylikuutio/ontology/font_struct.hpp"
 #include "code/ylikuutio/ontology/text_struct.hpp"
@@ -308,18 +310,7 @@ namespace ajokki
 
                 // Create an Earth `Ecosystem`.
 
-                std::cout << "Creating yli::ontology::Entity* earth_ecosystem and its contents ...\n";
-                yli::ontology::EcosystemStruct earth_ecosystem_struct;
-                earth_ecosystem_struct.global_name = "earth_ecosystem";
-                yli::ontology::Entity* const earth_ecosystem_entity = entity_factory->create_ecosystem(earth_ecosystem_struct);
-
-                std::cout << "Creating yli::ontology::Ecosystem* earth_ecosystem ...\n";
-                yli::ontology::Ecosystem* const earth_ecosystem = dynamic_cast<yli::ontology::Ecosystem*>(earth_ecosystem_entity);
-
-                if (earth_ecosystem == nullptr)
-                {
-                    return false;
-                }
+                yli::ontology::Ecosystem* const earth_ecosystem = ajokki::create_earth_ecosystem(entity_factory);
 
                 // Create a Mars `Ecosystem`.
 
@@ -1024,6 +1015,7 @@ namespace ajokki
                 yli::ontology::create_lisp_function_overload("bind", my_console, std::function(&yli::ontology::Object::bind_to_new_text_3d_master));
 
                 // `Entity` binding callbacks, `Symbiosis`.
+                yli::ontology::create_lisp_function_overload("bind", my_console, std::function(&yli::ontology::Symbiosis::bind_to_new_ecosystem_parent));
                 yli::ontology::create_lisp_function_overload("bind", my_console, std::function(&yli::ontology::Symbiosis::bind_to_new_scene_parent));
                 yli::ontology::create_lisp_function_overload("bind", my_console, std::function(&yli::ontology::Symbiosis::bind_to_new_shader));
 

code/ajokki/ajokki_altiplano_scene.cpp
@@ -113,7 +113,7 @@ namespace ajokki
 
         // Create the species, store it in `terrain_species`.
         yli::ontology::ModelStruct altiplano_terrain_model_struct;
-        altiplano_terrain_model_struct.scene = altiplano_scene;
+        altiplano_terrain_model_struct.parent = altiplano_scene;
         altiplano_terrain_model_struct.shader = altiplano_shader;
         altiplano_terrain_model_struct.material = altiplano_grass_material;
         altiplano_terrain_model_struct.model_file_format = "SRTM";

code/ajokki/ajokki_earth_ecosystem.cpp
@@ -1,0 +1,94 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "code/ylikuutio/ontology/ecosystem.hpp"
+#include "code/ylikuutio/ontology/shader.hpp"
+#include "code/ylikuutio/ontology/symbiosis.hpp"
+#include "code/ylikuutio/ontology/ecosystem_struct.hpp"
+#include "code/ylikuutio/ontology/shader_struct.hpp"
+#include "code/ylikuutio/ontology/model_struct.hpp"
+#include "code/ylikuutio/ontology/entity_factory.hpp"
+
+// Include standard headers
+#include <iostream> // std::cout, std::cin, std::cerr
+
+namespace yli::ontology
+{
+    class Entity;
+}
+
+namespace ajokki
+{
+    yli::ontology::Ecosystem* create_earth_ecosystem(yli::ontology::EntityFactory* const entity_factory)
+    {
+        // Earth `Ecosystem` begins here.
+
+        std::cout << "Creating yli::ontology::Entity* earth_ecosystem and its contents ...\n";
+        yli::ontology::EcosystemStruct earth_ecosystem_struct;
+        earth_ecosystem_struct.global_name = "earth_ecosystem";
+        yli::ontology::Entity* const earth_ecosystem_entity = entity_factory->create_ecosystem(earth_ecosystem_struct);
+
+        std::cout << "Creating yli::ontology::Ecosystem* earth_ecosystem ...\n";
+        yli::ontology::Ecosystem* const earth_ecosystem = dynamic_cast<yli::ontology::Ecosystem*>(earth_ecosystem_entity);
+
+        if (earth_ecosystem == nullptr)
+        {
+            return nullptr;
+        }
+
+        // Create the shader, store it in `earth_shader`.
+        yli::ontology::ShaderStruct earth_shader_struct;
+        earth_shader_struct.parent = earth_ecosystem;
+        earth_shader_struct.global_name = "earth_shader";
+        earth_shader_struct.local_name = "helsinki_regular_shader";
+        earth_shader_struct.vertex_shader = "standard_shading.vert";
+        earth_shader_struct.fragment_shader = "standard_shading.frag";
+
+        std::cout << "Creating yli::ontology::Entity* earth_shader_entity ...\n";
+        yli::ontology::Entity* const earth_shader_entity = entity_factory->create_shader(earth_shader_struct);
+        std::cout << "Creating yli::ontology::Shader* earth_shader ...\n";
+        yli::ontology::Shader* const earth_shader = dynamic_cast<yli::ontology::Shader*>(earth_shader_entity);
+
+        if (earth_shader == nullptr)
+        {
+            std::cerr << "Failed to create `Shader`.\n";
+            return nullptr;
+        }
+
+        yli::ontology::ModelStruct turbo_polizei_png_model_struct;
+        turbo_polizei_png_model_struct.parent = earth_ecosystem;
+        turbo_polizei_png_model_struct.shader = earth_shader;
+        turbo_polizei_png_model_struct.model_file_format = "fbx";
+        turbo_polizei_png_model_struct.model_filename = "turbo_polizei_png_textures.fbx";
+
+        std::cout << "Creating yli::ontology::Entity* turbo_polizei_png_symbiosis_entity ...\n";
+        yli::ontology::Entity* const turbo_polizei_png_symbiosis_entity = entity_factory->create_symbiosis(turbo_polizei_png_model_struct);
+
+        std::cout << "Creating yli::ontology::Symbiosis* turbo_polizei_png_symbiosis ...\n";
+        yli::ontology::Symbiosis* const turbo_polizei_png_symbiosis = dynamic_cast<yli::ontology::Symbiosis*>(turbo_polizei_png_symbiosis_entity);
+
+        if (turbo_polizei_png_symbiosis == nullptr)
+        {
+            std::cerr << "Failed to create turbo_polizei_png Symbiosis.\n";
+            return nullptr;
+        }
+
+        turbo_polizei_png_symbiosis->set_global_name("turbo_polizei_png_symbiosis");
+
+        return earth_ecosystem;
+    }
+}

code/ajokki/ajokki_earth_ecosystem.hpp
@@ -1,0 +1,27 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace yli::ontology
+{
+    class Ecosystem;
+    class EntityFactory;
+}
+
+namespace ajokki
+{
+    yli::ontology::Ecosystem* create_earth_ecosystem(yli::ontology::EntityFactory* const entity_factory);
+}

code/ajokki/ajokki_helsinki_east_downtown_scene.cpp
@@ -490,7 +490,7 @@ namespace ajokki
         orange_fur_material->set_global_name("helsinki_east_downtown_orange_fur_material");
 
         yli::ontology::ModelStruct helsinki_east_downtown_terrain_model_struct;
-        helsinki_east_downtown_terrain_model_struct.scene = helsinki_east_downtown_scene;
+        helsinki_east_downtown_terrain_model_struct.parent = helsinki_east_downtown_scene;
         helsinki_east_downtown_terrain_model_struct.shader = helsinki_east_downtown_shader;
         helsinki_east_downtown_terrain_model_struct.material = helsinki_east_downtown_grass_material;
         helsinki_east_downtown_terrain_model_struct.model_file_format = "ASCII_grid";
@@ -517,7 +517,7 @@ namespace ajokki
         entity_factory->create_object(helsinki_east_downtown_struct);
 
         yli::ontology::ModelStruct suzanne_model_struct;
-        suzanne_model_struct.scene = helsinki_east_downtown_scene;
+        suzanne_model_struct.parent = helsinki_east_downtown_scene;
         suzanne_model_struct.shader = helsinki_east_downtown_shader;
         suzanne_model_struct.material = orange_fur_material;
         suzanne_model_struct.model_file_format = "obj";
@@ -621,7 +621,7 @@ namespace ajokki
         suzanne5->set_global_name("suzanne5");
 
         yli::ontology::ModelStruct cat_model_struct;
-        cat_model_struct.scene = helsinki_east_downtown_scene;
+        cat_model_struct.parent = helsinki_east_downtown_scene;
         cat_model_struct.global_name = "cat_species";
         cat_model_struct.local_name = "cat";
         cat_model_struct.shader = helsinki_east_downtown_shader;
@@ -679,7 +679,7 @@ namespace ajokki
         }
 
         yli::ontology::ModelStruct freight_train_model_struct;
-        freight_train_model_struct.scene = helsinki_east_downtown_scene;
+        freight_train_model_struct.parent = helsinki_east_downtown_scene;
         freight_train_model_struct.shader = helsinki_east_downtown_shader;
         freight_train_model_struct.model_file_format = "fbx";
         freight_train_model_struct.model_filename = "freight_train.fbx";
@@ -720,73 +720,60 @@ namespace ajokki
 
         freight_train1->set_global_name("freight_train1");
 
-        yli::ontology::ModelStruct turbo_polizei_png_model_struct;
-        turbo_polizei_png_model_struct.scene = helsinki_east_downtown_scene;
-        turbo_polizei_png_model_struct.shader = helsinki_east_downtown_shader;
-        turbo_polizei_png_model_struct.model_file_format = "fbx";
-        turbo_polizei_png_model_struct.model_filename = "turbo_polizei_png_textures.fbx";
+        yli::ontology::Entity* const turbo_polizei_png_symbiosis_entity = universe.get_entity("turbo_polizei_png_symbiosis");
 
-        std::cout << "Creating yli::ontology::Entity* turbo_polizei_png_symbiosis_entity ...\n";
-        yli::ontology::Entity* const turbo_polizei_png_symbiosis_entity = entity_factory->create_symbiosis(turbo_polizei_png_model_struct);
-
-        std::cout << "Creating yli::ontology::Symbiosis* turbo_polizei_png_symbiosis ...\n";
         yli::ontology::Symbiosis* const turbo_polizei_png_symbiosis = dynamic_cast<yli::ontology::Symbiosis*>(turbo_polizei_png_symbiosis_entity);
 
-        if (turbo_polizei_png_symbiosis == nullptr)
+        if (turbo_polizei_png_symbiosis != nullptr)
         {
-            std::cerr << "Failed to create turbo_polizei_png Symbiosis.\n";
-            return nullptr;
+            yli::ontology::HolobiontStruct turbo_polizei_png_holobiont_struct1(*helsinki_east_downtown_scene, *turbo_polizei_png_symbiosis);
+            turbo_polizei_png_holobiont_struct1.brain = rest_brain;
+            turbo_polizei_png_holobiont_struct1.initial_rotate_vectors = { glm::vec3(0.0f, 1.0f, 1.0f) };
+            turbo_polizei_png_holobiont_struct1.initial_rotate_angles = { PI };
+            turbo_polizei_png_holobiont_struct1.original_scale_vector = glm::vec3(1.0f, 1.0f, 1.0f);
+            turbo_polizei_png_holobiont_struct1.cartesian_coordinates = glm::vec3(85.00f, 30.00f, 160.00f);
+
+            std::cout << "Creating yli::ontology::Entity* turbo_polizei_png1_entity ...\n";
+
+            yli::ontology::Entity* const turbo_polizei_png1_entity = entity_factory->create_holobiont(turbo_polizei_png_holobiont_struct1);
+
+            std::cout << "Creating yli::ontology::Holobiont* turbo_polizei_png1 ...\n";
+
+            yli::ontology::Holobiont* const turbo_polizei_png1 = dynamic_cast<yli::ontology::Holobiont*>(turbo_polizei_png1_entity);
+
+            if (turbo_polizei_png1 == nullptr)
+            {
+                std::cerr << "Failed to create turbo_polizei_png1 Object.\n";
+                return nullptr;
+            }
+
+            turbo_polizei_png1->set_global_name("turbo_polizei_png1");
+            turbo_polizei_png1->set_local_name("sinivuokko1");
+
+            yli::ontology::HolobiontStruct turbo_polizei_png_holobiont_struct2(*helsinki_east_downtown_scene, *turbo_polizei_png_symbiosis);
+            turbo_polizei_png_holobiont_struct2.brain = rest_brain;
+            turbo_polizei_png_holobiont_struct2.initial_rotate_vectors = { glm::vec3(0.0f, 1.0f, 1.0f) };
+            turbo_polizei_png_holobiont_struct2.initial_rotate_angles = { PI };
+            turbo_polizei_png_holobiont_struct2.original_scale_vector = glm::vec3(1.0f, 1.0f, 1.0f);
+            turbo_polizei_png_holobiont_struct2.cartesian_coordinates = glm::vec3(85.00f, 30.00f, 175.00f);
+
+            std::cout << "Creating yli::ontology::Entity* turbo_polizei_png2_entity ...\n";
+
+            yli::ontology::Entity* const turbo_polizei_png2_entity = entity_factory->create_holobiont(turbo_polizei_png_holobiont_struct2);
+
+            std::cout << "Creating yli::ontology::Holobiont* turbo_polizei_png2 ...\n";
+
+            yli::ontology::Holobiont* const turbo_polizei_png2 = dynamic_cast<yli::ontology::Holobiont*>(turbo_polizei_png2_entity);
+
+            if (turbo_polizei_png2 == nullptr)
+            {
+                std::cerr << "Failed to create turbo_polizei_png2 Object.\n";
+                return nullptr;
+            }
+
+            turbo_polizei_png2->set_global_name("turbo_polizei_png2");
+            turbo_polizei_png2->set_local_name("sinivuokko2");
         }
-
-        turbo_polizei_png_symbiosis->set_global_name("turbo_polizei_png_symbiosis");
-
-        yli::ontology::HolobiontStruct turbo_polizei_png_holobiont_struct1(*helsinki_east_downtown_scene, *turbo_polizei_png_symbiosis);
-        turbo_polizei_png_holobiont_struct1.brain = rest_brain;
-        turbo_polizei_png_holobiont_struct1.initial_rotate_vectors = { glm::vec3(0.0f, 1.0f, 1.0f) };
-        turbo_polizei_png_holobiont_struct1.initial_rotate_angles = { PI };
-        turbo_polizei_png_holobiont_struct1.original_scale_vector = glm::vec3(1.0f, 1.0f, 1.0f);
-        turbo_polizei_png_holobiont_struct1.cartesian_coordinates = glm::vec3(85.00f, 30.00f, 160.00f);
-
-        std::cout << "Creating yli::ontology::Entity* turbo_polizei_png1_entity ...\n";
-
-        yli::ontology::Entity* const turbo_polizei_png1_entity = entity_factory->create_holobiont(turbo_polizei_png_holobiont_struct1);
-
-        std::cout << "Creating yli::ontology::Holobiont* turbo_polizei_png1 ...\n";
-
-        yli::ontology::Holobiont* const turbo_polizei_png1 = dynamic_cast<yli::ontology::Holobiont*>(turbo_polizei_png1_entity);
-
-        if (turbo_polizei_png1 == nullptr)
-        {
-            std::cerr << "Failed to create turbo_polizei_png1 Object.\n";
-            return nullptr;
-        }
-
-        turbo_polizei_png1->set_global_name("turbo_polizei_png1");
-        turbo_polizei_png1->set_local_name("sinivuokko1");
-
-        yli::ontology::HolobiontStruct turbo_polizei_png_holobiont_struct2(*helsinki_east_downtown_scene, *turbo_polizei_png_symbiosis);
-        turbo_polizei_png_holobiont_struct2.brain = rest_brain;
-        turbo_polizei_png_holobiont_struct2.initial_rotate_vectors = { glm::vec3(0.0f, 1.0f, 1.0f) };
-        turbo_polizei_png_holobiont_struct2.initial_rotate_angles = { PI };
-        turbo_polizei_png_holobiont_struct2.original_scale_vector = glm::vec3(1.0f, 1.0f, 1.0f);
-        turbo_polizei_png_holobiont_struct2.cartesian_coordinates = glm::vec3(85.00f, 30.00f, 175.00f);
-
-        std::cout << "Creating yli::ontology::Entity* turbo_polizei_png2_entity ...\n";
-
-        yli::ontology::Entity* const turbo_polizei_png2_entity = entity_factory->create_holobiont(turbo_polizei_png_holobiont_struct2);
-
-        std::cout << "Creating yli::ontology::Holobiont* turbo_polizei_png2 ...\n";
-
-        yli::ontology::Holobiont* const turbo_polizei_png2 = dynamic_cast<yli::ontology::Holobiont*>(turbo_polizei_png2_entity);
-
-        if (turbo_polizei_png2 == nullptr)
-        {
-            std::cerr << "Failed to create turbo_polizei_png2 Object.\n";
-            return nullptr;
-        }
-
-        turbo_polizei_png2->set_global_name("turbo_polizei_png2");
-        turbo_polizei_png2->set_local_name("sinivuokko2");
 
         yli::ontology::CameraStruct cat_camera_struct;
         cat_camera_struct.scene = helsinki_east_downtown_scene;

code/ajokki/ajokki_joensuu_center_west_scene.cpp
@@ -118,7 +118,7 @@ namespace ajokki
         joensuu_center_west_grass_material->set_global_name("joensuu_center_west_grass_material");
 
         yli::ontology::ModelStruct joensuu_center_west_terrain_model_struct;
-        joensuu_center_west_terrain_model_struct.scene = joensuu_center_west_scene;
+        joensuu_center_west_terrain_model_struct.parent = joensuu_center_west_scene;
         joensuu_center_west_terrain_model_struct.shader = joensuu_center_west_shader;
         joensuu_center_west_terrain_model_struct.material = joensuu_center_west_grass_material;
         joensuu_center_west_terrain_model_struct.model_file_format = "ASCII_grid";
@@ -165,7 +165,7 @@ namespace ajokki
         orange_fur_material_joensuu->set_global_name("joensuu_center_west_orange_fur_material");
 
         yli::ontology::ModelStruct horse_model_struct;
-        horse_model_struct.scene = joensuu_center_west_scene;
+        horse_model_struct.parent = joensuu_center_west_scene;
         horse_model_struct.shader = joensuu_center_west_shader;
         horse_model_struct.material = orange_fur_material_joensuu;
         horse_model_struct.model_file_format = "fbx";

code/ajokki/ajokki_tallinn_scene.cpp
@@ -113,7 +113,7 @@ namespace ajokki
 
         // Create the species, store it in `terrain_species`.
         yli::ontology::ModelStruct tallinn_terrain_model_struct;
-        tallinn_terrain_model_struct.scene = tallinn_scene;
+        tallinn_terrain_model_struct.parent = tallinn_scene;
         tallinn_terrain_model_struct.shader = tallinn_shader;
         tallinn_terrain_model_struct.material = tallinn_grass_material;
         tallinn_terrain_model_struct.model_file_format = "SRTM";

commit e7da7d41615c270783488263f7ad866201d38804

Bugfix: Hirvi: create `earth_ecosystem`.
This bug was introduced in 1821a2e1cd67b268b73e7708f92616937433764a when
Ajokki code was modified to match the change of creating the
`earth_ecosystem` and `earth_shader` and `turbo_polizei_png_symbiosis`
in it, but Hirvi code was left unmodified.

CMakeLists.txt
@@ -812,6 +812,8 @@ add_executable(hirvi
     code/hirvi/hirvi.cpp
     code/hirvi/hirvi_console_callbacks.cpp
     code/hirvi/hirvi_console_callbacks.hpp
+    code/hirvi/hirvi_earth_ecosystem.cpp
+    code/hirvi/hirvi_earth_ecosystem.hpp
 
     # ajokki, in alphabetical order.
     code/ajokki/ajokki_helsinki_east_downtown_scene.cpp

code/hirvi/hirvi.cpp
@@ -20,6 +20,7 @@
 #endif
 
 #include "hirvi_console_callbacks.hpp"
+#include "hirvi_earth_ecosystem.hpp"
 #include "code/ajokki/ajokki_helsinki_east_downtown_scene.hpp"
 #include "code/ajokki/ajokki_joensuu_center_west_scene.hpp"
 #include "code/ylikuutio/audio/audio_system.hpp"
@@ -41,6 +42,7 @@
 #include "code/ylikuutio/ontology/entity.hpp"
 #include "code/ylikuutio/ontology/variable.hpp"
 #include "code/ylikuutio/ontology/universe.hpp"
+#include "code/ylikuutio/ontology/ecosystem.hpp"
 #include "code/ylikuutio/ontology/scene.hpp"
 #include "code/ylikuutio/ontology/object.hpp"
 #include "code/ylikuutio/ontology/holobiont.hpp"
@@ -254,6 +256,10 @@ namespace hirvi
                 }
 
                 my_universe->set_active_console(my_console);
+
+                // Create an Earth `Ecosystem`.
+
+                yli::ontology::Ecosystem* const earth_ecosystem = hirvi::create_earth_ecosystem(entity_factory);
 
                 std::cout << "Setting up console ...\n";
                 yli::snippets::set_console(my_universe, 15, 0, 0, 39);

code/hirvi/hirvi_earth_ecosystem.cpp
@@ -1,0 +1,94 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "code/ylikuutio/ontology/ecosystem.hpp"
+#include "code/ylikuutio/ontology/shader.hpp"
+#include "code/ylikuutio/ontology/symbiosis.hpp"
+#include "code/ylikuutio/ontology/ecosystem_struct.hpp"
+#include "code/ylikuutio/ontology/shader_struct.hpp"
+#include "code/ylikuutio/ontology/model_struct.hpp"
+#include "code/ylikuutio/ontology/entity_factory.hpp"
+
+// Include standard headers
+#include <iostream> // std::cout, std::cin, std::cerr
+
+namespace yli::ontology
+{
+    class Entity;
+}
+
+namespace hirvi
+{
+    yli::ontology::Ecosystem* create_earth_ecosystem(yli::ontology::EntityFactory* const entity_factory)
+    {
+        // Earth `Ecosystem` begins here.
+
+        std::cout << "Creating yli::ontology::Entity* earth_ecosystem and its contents ...\n";
+        yli::ontology::EcosystemStruct earth_ecosystem_struct;
+        earth_ecosystem_struct.global_name = "earth_ecosystem";
+        yli::ontology::Entity* const earth_ecosystem_entity = entity_factory->create_ecosystem(earth_ecosystem_struct);
+
+        std::cout << "Creating yli::ontology::Ecosystem* earth_ecosystem ...\n";
+        yli::ontology::Ecosystem* const earth_ecosystem = dynamic_cast<yli::ontology::Ecosystem*>(earth_ecosystem_entity);
+
+        if (earth_ecosystem == nullptr)
+        {
+            return nullptr;
+        }
+
+        // Create the shader, store it in `earth_shader`.
+        yli::ontology::ShaderStruct earth_shader_struct;
+        earth_shader_struct.parent = earth_ecosystem;
+        earth_shader_struct.global_name = "earth_shader";
+        earth_shader_struct.local_name = "helsinki_regular_shader";
+        earth_shader_struct.vertex_shader = "standard_shading.vert";
+        earth_shader_struct.fragment_shader = "standard_shading.frag";
+
+        std::cout << "Creating yli::ontology::Entity* earth_shader_entity ...\n";
+        yli::ontology::Entity* const earth_shader_entity = entity_factory->create_shader(earth_shader_struct);
+        std::cout << "Creating yli::ontology::Shader* earth_shader ...\n";
+        yli::ontology::Shader* const earth_shader = dynamic_cast<yli::ontology::Shader*>(earth_shader_entity);
+
+        if (earth_shader == nullptr)
+        {
+            std::cerr << "Failed to create `Shader`.\n";
+            return nullptr;
+        }
+
+        yli::ontology::ModelStruct turbo_polizei_png_model_struct;
+        turbo_polizei_png_model_struct.parent = earth_ecosystem;
+        turbo_polizei_png_model_struct.shader = earth_shader;
+        turbo_polizei_png_model_struct.model_file_format = "fbx";
+        turbo_polizei_png_model_struct.model_filename = "turbo_polizei_png_textures.fbx";
+
+        std::cout << "Creating yli::ontology::Entity* turbo_polizei_png_symbiosis_entity ...\n";
+        yli::ontology::Entity* const turbo_polizei_png_symbiosis_entity = entity_factory->create_symbiosis(turbo_polizei_png_model_struct);
+
+        std::cout << "Creating yli::ontology::Symbiosis* turbo_polizei_png_symbiosis ...\n";
+        yli::ontology::Symbiosis* const turbo_polizei_png_symbiosis = dynamic_cast<yli::ontology::Symbiosis*>(turbo_polizei_png_symbiosis_entity);
+
+        if (turbo_polizei_png_symbiosis == nullptr)
+        {
+            std::cerr << "Failed to create turbo_polizei_png Symbiosis.\n";
+            return nullptr;
+        }
+
+        turbo_polizei_png_symbiosis->set_global_name("turbo_polizei_png_symbiosis");
+
+        return earth_ecosystem;
+    }
+}

code/hirvi/hirvi_earth_ecosystem.hpp
@@ -1,0 +1,32 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef __HIRVI_HIRVI_EARTH_ECOSYSTEM_HPP_INCLUDED
+#define __HIRVI_HIRVI_EARTH_ECOSYSTEM_HPP_INCLUDED
+
+namespace yli::ontology
+{
+    class Ecosystem;
+    class EntityFactory;
+}
+
+namespace hirvi
+{
+    yli::ontology::Ecosystem* create_earth_ecosystem(yli::ontology::EntityFactory* const entity_factory);
+}
+
+#endif