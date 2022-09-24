c674fe1f72f164bc538641959e7f75af0f0bbf69
Antti Nuortimo <antti.nuortimo@gmail.com>
2022-09-25 00:35:55 +0300

    Hirvi: custom datatype `Train`.

---
+++ code/hirvi/train.hpp
@@ -0,0 +1,56 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2022 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef __HIRVI_TRAIN_HPP_INCLUDED
+#define __HIRVI_TRAIN_HPP_INCLUDED
+
+#include "code/ylikuutio/ontology/holobiont.hpp"
+#include "code/ylikuutio/snippets/locomotion/rail_vehicle_module.hpp"
+
+namespace yli::ontology
+{
+    class Universe;
+    class GenericParentModule;
+    class GenericMasterModule;
+    struct HolobiontStruct;
+    struct LocomotionModuleStruct;
+}
+
+namespace hirvi
+{
+    class Train : public yli::ontology::Holobiont
+    {
+        public:
+            explicit Train(
+                    yli::ontology::Universe& universe,
+                    const yli::ontology::HolobiontStruct& train_struct,
+                    const yli::ontology::LocomotionModuleStruct& rail_vehicle_struct,
+                    yli::ontology::GenericParentModule* const scene_parent,
+                    yli::ontology::GenericMasterModule* const symbiosis_master,
+                    yli::ontology::GenericMasterModule* const brain_master);
+
+            Train(const Train&) = delete;            // Delete copy constructor.
+            Train &operator=(const Train&) = delete; // Delete copy assignment.
+
+            ~Train() = default;
+
+        private:
+            yli::snippets::locomotion::RailVehicleModule rail_vehicle;
+    };
+}
+
+#endif
