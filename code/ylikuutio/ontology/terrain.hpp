4e506c5a86d566eebc7cbfb696160a54722a21ad
Antti Nuortimo <antti.nuortimo@gmail.com>
2023-05-21 13:48:08 +0300

    Reorder forward declarations into canonical order.

--- code/ylikuutio/ontology/terrain.hpp
+++ code/ylikuutio/ontology/terrain.hpp
@@ -27,9 +27,9 @@ namespace yli::core
 
 namespace yli::ontology
 {
-    class Universe;
     class GenericParentModule;
     class GenericMasterModule;
+    class Universe;
     struct ObjectStruct;
 
     class Terrain final : public yli::ontology::Object
