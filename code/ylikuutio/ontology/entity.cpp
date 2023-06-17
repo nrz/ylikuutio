nrz/ylikuutio

commit 05338affd7cb06d9d2f22d3ae37be7a53d96486e

Bugfix: do not call `Universe` member functions in `Entity` destructor.

code/ylikuutio/ontology/entity.cpp
@@ -86,7 +86,8 @@ namespace yli::ontology
                 &this->registry,
                 application.get_memory_allocator(yli::data::Datatype::VARIABLE),
                 ""), // Do not index `parent_of_variables`, index only the variables.
-        universe { universe }
+        universe { universe },
+        is_universe { entity_struct.is_universe }
     {
         // constructor.
 
@@ -110,13 +111,15 @@ namespace yli::ontology
     {
         // destructor.
 
-        // FIXME `Entity` destructor must not access derived class `Universe`!
-        this->universe.unbind_entity(this->entityID);
-
-        if (!this->global_name.empty())
+        if (!this-is_universe)
         {
-            // OK, this `Entity` had a global name, so it's global name shall be erased.
-            this->universe.erase_entity(this->global_name);
+            this->universe.unbind_entity(this->entityID);
+
+            if (!this->global_name.empty())
+            {
+                // OK, this `Entity` had a global name, so it's global name shall be erased.
+                this->universe.erase_entity(this->global_name);
+            }
         }
 
         // Local names must be erased in the destructors

code/ylikuutio/ontology/entity.hpp
@@ -179,6 +179,7 @@ namespace yli::ontology
             std::string local_name;  // local name of this `Entity`.
 
             bool can_be_erased { false };
+            const bool is_universe;
 
         private:
             virtual std::size_t get_number_of_children() const = 0;

code/ylikuutio/ontology/universe.cpp
@@ -236,6 +236,14 @@ namespace yli::ontology
         // destructor.
 
         SDL_Quit();
+
+        this->unbind_entity(this->entityID);
+
+        if (!this->global_name.empty())
+        {
+            // OK, this `Universe` had a global name, so it's global name shall be erased.
+            this->universe.erase_entity(this->global_name);
+        }
     }
 
     void Universe::do_physics()

commit 21e6ac2dc4b98d28b2db3cd1d48bb5f205d0497d

Bugfix: condition in `Entity` destructor.
This bug was introduced in 05338affd7cb06d9d2f22d3ae37be7a53d96486e

code/ylikuutio/ontology/entity.cpp
@@ -95,7 +95,7 @@ namespace yli::ontology
     {
         // destructor.
 
-        if (!this-is_universe)
+        if (!this->is_universe)
         {
             this->universe.unbind_entity(this->entityID);
 