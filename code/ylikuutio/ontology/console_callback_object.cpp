80ec59d324f8021a82a1b4f9985ec38e95fabc48
Antti Nuortimo <antti.nuortimo@gmail.com>
2023-06-22 13:24:02 +0300

    Remove unnecessary `#include` line.

--- code/ylikuutio/ontology/console_callback_object.cpp
+++ code/ylikuutio/ontology/console_callback_object.cpp
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "callback_parameter.hpp"
 #include "console_callback_engine.hpp"
 #include "console_callback_object.hpp"
 #include "console_callback_object_struct.hpp"
