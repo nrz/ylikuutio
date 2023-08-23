3f1b759beed5f56d8c44a3f7f409509b9731f097
Antti Nuortimo <antti.nuortimo@gmail.com>
2023-08-24 00:25:48 +0300

    Comment out an unused variable from a function signature.

--- code/ylikuutio/triangulation/vertices.cpp
+++ code/ylikuutio/triangulation/vertices.cpp
@@ -57,7 +57,7 @@ namespace yli::triangulation
     }
 
     bool define_vertices_uvs_and_normals(
-            const yli::triangulation::TriangulateQuadsStruct& triangulate_quads_struct,
+            const yli::triangulation::TriangulateQuadsStruct& /* triangulate_quads_struct */,
             const std::vector<glm::vec3>& temp_vertices,
             const std::vector<glm::vec2>& temp_uvs,
             const std::vector<glm::vec3>& temp_normals,
