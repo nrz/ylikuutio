e7020e80a13a4d3df46263d890525044105ad38f
Antti Nuortimo <antti.nuortimo@gmail.com>
2023-12-28 21:41:54 +0200

    Use `std::string_view` in unit tests.

--- src/tests/test_string.cpp
+++ src/tests/test_string.cpp
@@ -3,151 +3,134 @@
 
 extern "C"
 {
-    int FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(char* string, int sz);
+    int FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(const char* string, int sz);
 }
 
 #include <cstddef>  // std::size_t
+#include <string_view> // std::string_view
 
 TEST(get_has_line_code_must_work_properly, empty)
 {
-    char empty[] { "" };
-    std::size_t sz = strlen(empty);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(empty, sz);
+    std::string_view empty { "" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(empty.data(), empty.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, space)
 {
-    char space[] { " " };
-    std::size_t sz = strlen(space);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space, sz);
+    std::string_view space { " " };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space.data(), space.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, a)
 {
-    char a[] { "a" };
-    std::size_t sz = strlen(a);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a, sz);
+    std::string_view a { "a" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a.data(), a.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, hash)
 {
-    char hash[] { "#" };
-    std::size_t sz = strlen(hash);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash, sz);
+    std::string_view hash { "#" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash.data(), hash.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, space_space)
 {
-    char space_space[] { "  " };
-    std::size_t sz = strlen(space_space);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_space, sz);
+    std::string_view space_space { "  " };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_space.data(), space_space.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, space_a)
 {
-    char space_a[] { " a" };
-    std::size_t sz = strlen(space_a);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_a, sz);
+    std::string_view space_a { " a" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_a.data(), space_a.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, space_hash)
 {
-    char space_hash[] { " #" };
-    std::size_t sz = strlen(space_hash);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_hash, sz);
+    std::string_view space_hash { " #" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_hash.data(), space_hash.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, a_space)
 {
-    char a_space[] { "a " };
-    std::size_t sz = strlen(a_space);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_space, sz);
+    std::string_view a_space { "a " };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_space.data(), a_space.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, a_a)
 {
-    char a_a[] { "aa" };
-    std::size_t sz = strlen(a_a);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_a, sz);
+    std::string_view a_a { "aa" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_a.data(), a_a.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, a_hash)
 {
-    char a_hash[] { "a#" };
-    std::size_t sz = strlen(a_hash);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_hash, sz);
+    std::string_view a_hash { "a#" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_hash.data(), a_hash.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, hash_space)
 {
-    char hash_space[] { "# " };
-    std::size_t sz = strlen(hash_space);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash_space, sz);
+    std::string_view hash_space { "# " };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash_space.data(), hash_space.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, hash_a)
 {
-    char hash_a[] { "#a" };
-    std::size_t sz = strlen(hash_a);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash_a, sz);
+    std::string_view hash_a { "#a" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash_a.data(), hash_a.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, hash_hash)
 {
-    char hash_hash[] { "##" };
-    std::size_t sz = strlen(hash_hash);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash_hash, sz);
+    std::string_view hash_hash { "##" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(hash_hash.data(), hash_hash.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, space_a_space)
 {
-    char space_a_space[] { " a " };
-    std::size_t sz = strlen(space_a_space);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_a_space, sz);
+    std::string_view space_a_space { " a " };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_a_space.data(), space_a_space.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, a_space_a)
 {
-    char a_space_a[] { "a a" };
-    std::size_t sz = strlen(a_space_a);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_space_a, sz);
+    std::string_view a_space_a { "a a" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_space_a.data(), a_space_a.size());
     ASSERT_TRUE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, space_hash_space)
 {
-    char space_hash_space[] { " # " };
-    std::size_t sz = strlen(space_hash_space);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_hash_space, sz);
+    std::string_view space_hash_space { " # " };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(space_hash_space.data(), space_hash_space.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, a_space_hash)
 {
-    char a_space_hash[] { "# #" };
-    std::size_t sz = strlen(a_space_hash);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_space_hash, sz);
+    std::string_view a_space_hash { "# #" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(a_space_hash.data(), a_space_hash.size());
     ASSERT_FALSE(has_line_code);
 }
 
 TEST(get_has_line_code_must_work_properly, abc)
 {
-    char abc[] { "abc" };
-    std::size_t sz = strlen(abc);
-    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(abc, sz);
+    std::string_view abc { "abc" };
+    int has_line_code = FortranCInterface_MODULE_(string_mod, get_has_line_code, string_mod, get_has_line_code)(abc.data(), abc.size());
     ASSERT_TRUE(has_line_code);
 }
