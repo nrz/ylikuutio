c474a91f08b4c8a108455f2b98e3a91a5b13d87b
Antti Nuortimo <antti.nuortimo@gmail.com>
2023-12-29 20:30:24 +0200

    Use `std::string_view` in unit tests.

--- src/tests/test_get_first_token.cpp
+++ src/tests/test_get_first_token.cpp
@@ -13,7 +13,7 @@ TEST(get_first_token_must_work_properly, empty)
     std::string_view empty { "" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(empty.data(), empty.size(), &next_i);
-    std::string expected_token { "" };
+    std::string_view expected_token { "" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, -1); // No token found!
     free(token);
@@ -24,7 +24,7 @@ TEST(get_first_token_must_work_properly, space)
     std::string_view space { " " };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(space.data(), space.size(), &next_i);
-    std::string expected_token { "" };
+    std::string_view expected_token { "" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, -1); // No token found!
     free(token);
@@ -35,7 +35,7 @@ TEST(get_first_token_must_work_properly, a)
     std::string_view a { "a" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(a.data(), a.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 2); // Searching for next token can begin from Fortran index 2.
     free(token);
@@ -46,7 +46,7 @@ TEST(get_first_token_must_work_properly, hash)
     std::string_view hash { "#" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(hash.data(), hash.size(), &next_i);
-    std::string expected_token { "" };
+    std::string_view expected_token { "" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, -1); // No token found!
     free(token);
@@ -57,7 +57,7 @@ TEST(get_first_token_must_work_properly, newline)
     std::string_view newline { "\n" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(newline.data(), newline.size(), &next_i);
-    std::string expected_token { "" };
+    std::string_view expected_token { "" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, -1); // No token found!
     free(token);
@@ -68,7 +68,7 @@ TEST(get_first_token_must_work_properly, space_a)
     std::string_view space_a { " a" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(space_a.data(), space_a.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 3); // Searching for next token can begin from Fortran index 3.
     free(token);
@@ -79,7 +79,7 @@ TEST(get_first_token_must_work_properly, space_hash)
     std::string_view space_hash { " #" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(space_hash.data(), space_hash.size(), &next_i);
-    std::string expected_token { "" };
+    std::string_view expected_token { "" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, -1); // No token found!
     free(token);
@@ -90,7 +90,7 @@ TEST(get_first_token_must_work_properly, a_space)
     std::string_view a_space { "a " };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(a_space.data(), a_space.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 2); // Searching for next token can begin from Fortran index 2.
     free(token);
@@ -101,7 +101,7 @@ TEST(get_first_token_must_work_properly, a_hash)
     std::string_view a_hash { "a#" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(a_hash.data(), a_hash.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 2); // Searching for next token can begin from Fortran index 2.
     free(token);
@@ -112,7 +112,7 @@ TEST(get_first_token_must_work_properly, a_newline)
     std::string_view a_newline { "a\n" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(a_newline.data(), a_newline.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 2); // Searching for next token can begin from Fortran index 2.
     free(token);
@@ -123,7 +123,7 @@ TEST(get_first_token_must_work_properly, newline_a)
     std::string_view newline_a { "\na" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(newline_a.data(), newline_a.size(), &next_i);
-    std::string expected_token { "" };
+    std::string_view expected_token { "" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, -1); // No token found!
     free(token);
@@ -134,7 +134,7 @@ TEST(get_first_token_must_work_properly, space_a_space)
     std::string_view space_a_space { " a " };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(space_a_space.data(), space_a_space.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 3); // Searching for next token can begin from Fortran index 3.
     free(token);
@@ -145,7 +145,7 @@ TEST(get_first_token_must_work_properly, space_a_hash)
     std::string_view space_a_hash { " a#" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(space_a_hash.data(), space_a_hash.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 3); // Searching for next token can begin from Fortran index 3.
     free(token);
@@ -156,7 +156,7 @@ TEST(get_first_token_must_work_properly, a_space_a)
     std::string_view a_space_a { "a a" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(a_space_a.data(), a_space_a.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 2); // Searching for next token can begin from Fortran index 2.
     free(token);
@@ -167,7 +167,7 @@ TEST(get_first_token_must_work_properly, a_space_hash)
     std::string_view a_space_hash { "a #" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(a_space_hash.data(), a_space_hash.size(), &next_i);
-    std::string expected_token { "a" };
+    std::string_view expected_token { "a" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 2); // Searching for next token can begin from Fortran index 2.
     free(token);
@@ -178,7 +178,7 @@ TEST(get_first_token_must_work_properly, abc)
     std::string_view abc { "abc" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(abc.data(), abc.size(), &next_i);
-    std::string expected_token { "abc" };
+    std::string_view expected_token { "abc" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 4); // Searching for next token can begin from Fortran index 4.
     free(token);
@@ -189,7 +189,7 @@ TEST(get_first_token_must_work_properly, abc_space)
     std::string_view abc_space { "abc " };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(abc_space.data(), abc_space.size(), &next_i);
-    std::string expected_token { "abc" };
+    std::string_view expected_token { "abc" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 4); // Searching for next token can begin from Fortran index 4.
     free(token);
@@ -200,7 +200,7 @@ TEST(get_first_token_must_work_properly, abc_hash)
     std::string_view abc_hash { "abc#" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(abc_hash.data(), abc_hash.size(), &next_i);
-    std::string expected_token { "abc" };
+    std::string_view expected_token { "abc" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 4); // Searching for next token can begin from Fortran index 4.
     free(token);
@@ -211,7 +211,7 @@ TEST(get_first_token_must_work_properly, abc_newline)
     std::string_view abc_newline { "abc\n" };
     int next_i = 0;
     char* token = FortranCInterface_MODULE_(string_mod, get_first_token, string_mod, get_first_token)(abc_newline.data(), abc_newline.size(), &next_i);
-    std::string expected_token { "abc" };
+    std::string_view expected_token { "abc" };
     ASSERT_EQ(expected_token.compare(token), 0);
     ASSERT_EQ(next_i, 4); // Searching for next token can begin from Fortran index 4.
     free(token);
