3298008b5a768b751d3abd07e8ac88820215d6a5
Antti Nuortimo <antti.nuortimo@gmail.com>
2023-12-31 01:33:20 +0200

    Planetary motion: Check & print begin & end lines of input file blocks.

--- src/planetary_motion.f90
+++ src/planetary_motion.f90
@@ -8,7 +8,7 @@ program planetary_motion
     use file_mod
 
     ! These are needed for C++/Fortran interface used by unit tests implemented in C++.
-    use, intrinsic :: iso_c_binding, only: c_ptr
+    use, intrinsic :: iso_c_binding, only: c_char, c_f_pointer, c_ptr
 
     implicit none
 
@@ -17,6 +17,9 @@ program planetary_motion
     character(len = :), allocatable :: global_parameters_string, objects_string
     type(c_ptr) :: file_content
     integer :: file_sz
+    character(kind = c_char), pointer, dimension(:) :: fortran_file_content
+    integer :: begin_global_parameters_line_i, end_global_parameters_line_i
+    integer :: begin_objects_line_i, end_objects_line_i
 
     ! Objects are stored in the a vector of objects.
     type(planetary_system) :: my_planetary_system
@@ -39,6 +42,58 @@ program planetary_motion
 
     global_parameters_string = "global_parameters"
     objects_string = "objects"
+
+    ! `file_content` is `c_ptr`. It needs to be converted into a Fortran pointer.
+    call c_f_pointer(file_content, fortran_file_content, [ file_sz ])
+
+    begin_global_parameters_line_i = get_line_i_with_2_token_statement(fortran_file_content, file_sz, &
+        begin_string, len(begin_string), &
+        global_parameters_string, len(global_parameters_string))
+
+    if (begin_global_parameters_line_i .ge. 1) then
+        write(stdout, "(A40)", advance = "no") "`begin global_parameters` found on line "
+        write(stdout, "(g0)") begin_global_parameters_line_i
+    else
+        write(stdout, "(A86)") &
+            "`begin global_parameters` not found! Please check provided `input.dat` for an example!"
+        return
+    end if
+
+    end_global_parameters_line_i = get_line_i_with_2_token_statement(fortran_file_content, file_sz, &
+        end_string, len(end_string), &
+        global_parameters_string, len(global_parameters_string))
+    if (end_global_parameters_line_i .ge. 1) then
+        write(stdout, "(A38)", advance = "no") "`end global_parameters` found on line "
+        write(stdout, "(g0)") end_global_parameters_line_i
+    else
+        write(stdout, "(A84)") &
+            "`end global_parameters` not found! Please check provided `input.dat` for an example!"
+        return
+    end if
+
+    begin_objects_line_i = get_line_i_with_2_token_statement(fortran_file_content, file_sz, &
+        begin_string, len(begin_string), &
+        objects_string, len(objects_string))
+    if (begin_objects_line_i .ge. 1) then
+        write(stdout, "(A30)", advance = "no") "`begin objects` found on line "
+        write(stdout, "(g0)") begin_objects_line_i
+    else
+        write(stdout, "(A76)") &
+            "`begin objects` not found! Please check provided `input.dat` for an example!"
+        return
+    end if
+
+    end_objects_line_i = get_line_i_with_2_token_statement(fortran_file_content, file_sz, &
+        end_string, len(end_string), &
+        objects_string, len(objects_string))
+    if (end_objects_line_i .ge. 1) then
+        write(stdout, "(A28)", advance = "no") "`end objects` found on line "
+        write(stdout, "(g0)") end_objects_line_i
+    else
+        write(stdout, "(A74)") &
+            "`end objects` not found! Please check provided `input.dat` for an example!"
+        return
+    end if
 
     call simulate(my_planetary_system)
 
