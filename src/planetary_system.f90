d6fbbca113fc8e81d41f61040ccb6923865bed03
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-01-04 21:07:37 +0200

    `print_objects`: improve printing.

--- src/planetary_system.f90
+++ src/planetary_system.f90
@@ -21,29 +21,33 @@ contains
         implicit none
 
         type(planetary_system), intent(in) :: my_planetary_system
-        type(object) :: object
+        type(object) :: my_object
         integer :: object_i
+        character(len = 100) :: name_length_string
 
         object_loop: do object_i = 1, my_planetary_system % n_objects
 
-            object = my_planetary_system % objects(object_i)
+            my_object = my_planetary_system % objects(object_i)
+
+            ! Write the length of the name into `name_length_string`.
+            write(name_length_string, "(g0)") len(my_object % name)
 
             write(stdout, "(A7)", advance = "no") "name = "
-            print *, object % name
-            write(stdout, "(G10.2)", advance = "no") "mass = "
-            print *, object % mass
-            write(stdout, "(G10.2)", advance = "no") "x    = "
-            print *, object % position(1)
-            write(stdout, "(G10.2)", advance = "no") "y    = "
-            print *, object % position(2)
-            write(stdout, "(G10.2)", advance = "no") "z    = "
-            print *, object % position(3)
-            write(stdout, "(G10.2)", advance = "no") "vx   = "
-            print *, object % velocity(1)
-            write(stdout, "(G10.2)", advance = "no") "vy   = "
-            print *, object % velocity(2)
-            write(stdout, "(G10.2)", advance = "no") "vz   = "
-            print *, object % velocity(3)
+            write(stdout, "(A" // name_length_string // ")") my_object % name
+            write(stdout, "(A7)", advance = "no") "mass = "
+            write(stdout, "(g0)") my_object % mass
+            write(stdout, "(A7)", advance = "no") "x    = "
+            write(stdout, "(g0)") my_object % position(1)
+            write(stdout, "(A7)", advance = "no") "y    = "
+            write(stdout, "(g0)") my_object % position(2)
+            write(stdout, "(A7)", advance = "no") "z    = "
+            write(stdout, "(g0)") my_object % position(3)
+            write(stdout, "(A7)", advance = "no") "vx   = "
+            write(stdout, "(g0)") my_object % velocity(1)
+            write(stdout, "(A7)", advance = "no") "vy   = "
+            write(stdout, "(g0)") my_object % velocity(2)
+            write(stdout, "(A7)", advance = "no") "vz   = "
+            write(stdout, "(g0)") my_object % velocity(3)
         end do object_loop
 
     end subroutine print_objects
