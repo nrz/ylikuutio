25c4ac469a39c5cdbb0183c0cac103b848c75b92
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-03-10 17:54:31 +0200

    Edit whitespace.

--- code/ylikuutio/ontology/variable.cpp
+++ code/ylikuutio/ontology/variable.cpp
@@ -22,11 +22,11 @@
 #include "code/ylikuutio/data/any_value.hpp"
 
 // Include standard headers
-#include <cstddef>  // std::size_t
-#include <iostream> // std::cout, std::cerr
-#include <optional> // std::optional
+#include <cstddef>   // std::size_t
+#include <iostream>  // std::cout, std::cerr
+#include <optional>  // std::optional
 #include <stdexcept> // std::runtime_error
-#include <string>   // std::string
+#include <string>    // std::string
 
 namespace yli::core
 {
