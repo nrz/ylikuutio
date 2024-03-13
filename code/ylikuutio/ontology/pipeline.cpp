9b3e5dfe2499c022a69388a8491afc5b1c7a0edc
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-03-13 19:44:23 +0200

    `Pipeline::bind_to_new_ecosystem_parent`: rename a local variable.

--- code/ylikuutio/ontology/pipeline.cpp
+++ code/ylikuutio/ontology/pipeline.cpp
@@ -47,14 +47,14 @@ namespace yli::ontology
         // Set pointer to `Pipeline` to `nullptr`, set parent according to the input,
         // and request a new childID from `new_parent`.
 
-        const yli::ontology::Entity* const old_parent = pipeline.get_parent();
+        const yli::ontology::Entity* const old_scene_or_ecosystem_parent = pipeline.get_parent();
 
-        if (old_parent == nullptr)
+        if (old_scene_or_ecosystem_parent == nullptr)
         {
-            throw std::runtime_error("ERROR: `Pipeline::bind_to_new_ecosystem_parent`: `old_parent` is `nullptr`!");
+            throw std::runtime_error("ERROR: `Pipeline::bind_to_new_ecosystem_parent`: `old_scene_or_ecosystem_parent` is `nullptr`!");
         }
 
-        if (&new_parent == old_parent)
+        if (&new_parent == old_scene_or_ecosystem_parent)
         {
             // Setting current parent as the new parent. Nothing to do.
             return std::nullopt;
