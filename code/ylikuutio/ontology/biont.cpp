dbae3ce6598e484f850143ebd291c5ec1a0c6e2e
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-03-14 00:33:32 +0200

    `Biont::render_this_biont`: throw is render system is `nullptr`.

--- code/ylikuutio/ontology/biont.cpp
+++ code/ylikuutio/ontology/biont.cpp
@@ -142,8 +142,7 @@ namespace yli::ontology
 
         if (this->universe.get_render_system() == nullptr)
         {
-            std::cerr << "ERROR: `Biont::render_this_biont`: `this->universe.get_render_system()` is `nullptr`!\n";
-            return;
+            throw std::runtime_error("ERROR: `Biont::render_this_biont`: `this->universe.get_render_system()` is `nullptr`!");
         }
 
         if (pipeline == nullptr)
