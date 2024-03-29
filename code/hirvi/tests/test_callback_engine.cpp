5379ac1905cc8daf4792751086bb80dc394a9aa6
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-03-29 13:06:43 +0200

    Test for `CallbackEngine` created in `HirviApplication`.

---
+++ code/hirvi/tests/test_callback_engine.cpp
@@ -0,0 +1,51 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2024 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "gtest/gtest.h"
+#include "code/hirvi/hirvi.hpp"
+#include "code/hirvi/datatype.hpp"
+#include "code/ylikuutio/snippets/brain_snippets.hpp"
+#include "code/ylikuutio/memory/constructible_module.hpp"
+#include "code/ylikuutio/ontology/universe.hpp"
+#include "code/ylikuutio/ontology/callback_engine.hpp"
+#include "code/ylikuutio/ontology/brain.hpp"
+
+// Include GLM
+#ifndef __GLM_GLM_HPP_INCLUDED
+#define __GLM_GLM_HPP_INCLUDED
+#include <glm/glm.hpp> // glm
+#endif
+
+// Include standard headers
+#include <numbers> // std::numbers::pi
+
+TEST(callback_engine_must_be_initialized_appropriately, hirvi_callback_engine)
+{
+    const int argc { 0 };
+    const char** const argv { nullptr };
+    hirvi::HirviApplication hirvi_application(argc, argv);
+
+    yli::ontology::CallbackEngineStruct rest_callback_engine_struct;
+    auto rest_callback_engine = hirvi_application.entity_factory.create_callback_engine(rest_callback_engine_struct);
+    rest_callback_engine->create_callback_object(&yli::snippets::rest);
+    ASSERT_NE(rest_callback_engine, nullptr);
+    yli::memory::ConstructibleModule callback_engine_constructible_module = rest_callback_engine->get_constructible_module();
+    ASSERT_EQ(callback_engine_constructible_module.datatype, hirvi::Datatype::CALLBACK_ENGINE);
+    ASSERT_EQ(callback_engine_constructible_module.storage_i, 0);
+    ASSERT_EQ(callback_engine_constructible_module.slot_i, 0);
+    ASSERT_EQ(callback_engine_constructible_module.alive, true);
+}
