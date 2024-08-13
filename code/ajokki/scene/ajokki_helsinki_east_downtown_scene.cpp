6611fe95779fdbbd8cec0291c2c934492d9d6854
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-08-13 23:04:39 +0300

    Ajokki: move Helsinki and Joensuu scene files into `scene` directory.

--- code/ajokki/scene/ajokki_helsinki_east_downtown_scene.cpp
+++ code/ajokki/scene/ajokki_helsinki_east_downtown_scene.cpp
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "ajokki.hpp"
+#include "code/ajokki/ajokki.hpp"
 #include "code/ylikuutio/snippets/brain_snippets.hpp"
 #include "code/ylikuutio/snippets/keyboard_callback_snippets.hpp"
 #include "code/ylikuutio/ontology/universe.hpp"
