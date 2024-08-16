94c57c52ca1091e29860a36d7386d64c02146323
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-08-16 23:55:25 +0300

    Bugfix: yaw and pitch work now properly for `Holobiont` instances.

--- code/ylikuutio/ontology/biont.cpp
+++ code/ylikuutio/ontology/biont.cpp
@@ -167,7 +167,7 @@ namespace yli::ontology
         }
 
         this->model_matrix = glm::scale(this->model_matrix, holobiont_parent->get_scale() * this->original_scale_vector);
-        glm::vec3 euler_angles { holobiont_parent->orientation.roll, holobiont_parent->orientation.yaw, holobiont_parent->orientation.pitch };
+        glm::vec3 euler_angles { holobiont_parent->orientation.roll, -holobiont_parent->orientation.pitch, holobiont_parent->orientation.yaw };
         glm::quat my_quaternion = glm::quat(euler_angles);
         glm::mat4 rotation_matrix = glm::mat4_cast(my_quaternion);
         this->model_matrix = rotation_matrix * this->model_matrix;
