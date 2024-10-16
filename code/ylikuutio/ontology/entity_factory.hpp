nrz/ylikuutio

commit 6ae7902c9af2c7ddfbe109067fb3f51837f445d1

`Object`: reorder member variables and constructor arguments.

code/ylikuutio/ontology/entity_factory.hpp
@@ -377,6 +377,8 @@ namespace yli::ontology
                             object_struct.scene,
                             object_struct,
                             {},
+                            // `Brain` master.
+                            (object_struct.brain_master != nullptr ? object_struct.brain_master->get_generic_master_module<Movable>() : nullptr),
                             // mesh master.
                             ((std::holds_alternative<Species*>(object_struct.mesh_master) && std::get<Species*>(object_struct.mesh_master) != nullptr) ?
                              &(std::get<Species*>(object_struct.mesh_master)->master_of_objects) :
@@ -384,9 +386,7 @@ namespace yli::ontology
                              &(std::get<ShapeshifterSequence*>(object_struct.mesh_master)->master_of_objects) :
                              (std::holds_alternative<Text3d*>(object_struct.mesh_master) && std::get<Text3d*>(object_struct.mesh_master) != nullptr) ?
                              &(std::get<Text3d*>(object_struct.mesh_master)->master_of_objects) :
-                             nullptr),
-                            // `Brain` master.
-                            (object_struct.brain_master != nullptr ? object_struct.brain_master->get_generic_master_module<Movable>() : nullptr));
+                             nullptr));
             }
 
             // TODO: implement `create_heightmap` here!

code/ylikuutio/ontology/object.cpp
@@ -176,8 +176,8 @@ namespace yli::ontology
             yli::ontology::Universe& universe,
             const yli::ontology::ObjectStruct& object_struct,
             yli::ontology::GenericParentModule* const scene_parent_module,
-            yli::ontology::GenericMasterModule* const mesh_master_module,
-            yli::ontology::GenericMasterModule* const brain_master_module)
+            yli::ontology::GenericMasterModule* const brain_master_module,
+            yli::ontology::GenericMasterModule* const mesh_master_module)
         : Movable(
                 application,
                 universe,

code/ylikuutio/ontology/object.hpp
@@ -86,8 +86,8 @@ namespace yli::ontology
                     Universe& universe,
                     const ObjectStruct& object_struct,
                     GenericParentModule* const scene_parent_module,
-                    GenericMasterModule* const mesh_master_module,
-                    GenericMasterModule* const brain_master_module);
+                    GenericMasterModule* const brain_master_module,
+                    GenericMasterModule* const mesh_master_module);
 
             virtual ~Object() = default;
 

commit eadd2376dbcf8664faaaca4b57d28a11d0c875d0

Bugfix: order of master modules in `Object` derivative factory function.
This was caused by 6ae7902c9af2c7ddfbe109067fb3f51837f445d1 where
the appropriate change was done in `EntityFactory::create_object` but by
mistake the corresponding change to
`EntityFactory::create_object_derivative` was left out.

code/ylikuutio/ontology/entity_factory.hpp
@@ -713,6 +713,8 @@ namespace yli::ontology
                                 object_struct.scene,
                                 object_struct,
                                 {},
+                                // `Brain` master.
+                                (object_struct.brain_master != nullptr ? object_struct.brain_master->get_generic_master_module<Movable>() : nullptr),
                                 // mesh master.
                                 ((std::holds_alternative<Species*>(object_struct.mesh_master) && std::get<Species*>(object_struct.mesh_master) != nullptr) ?
                                  &(std::get<Species*>(object_struct.mesh_master)->master_of_objects) :
@@ -721,8 +723,6 @@ namespace yli::ontology
                                  (std::holds_alternative<Text3d*>(object_struct.mesh_master) && std::get<Text3d*>(object_struct.mesh_master) != nullptr) ?
                                  &(std::get<Text3d*>(object_struct.mesh_master)->master_of_objects) :
                                  nullptr),
-                                // `Brain` master.
-                                (object_struct.brain_master != nullptr ? object_struct.brain_master->get_generic_master_module<Movable>() : nullptr),
                                 // Skill modules.
                                 std::forward<ModuleArgs>(module_args)...);
                 }