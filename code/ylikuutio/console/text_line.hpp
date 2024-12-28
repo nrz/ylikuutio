d2f391889f800f5677132fd002530ff6ff43830a
Antti Nuortimo <antti.nuortimo@gmail.com>
2024-12-28 07:28:24 +0200

    New class: `yli::console::TextLine`.
    `TextLine` is to be used in the scrollback buffer of the console.

---
+++ code/ylikuutio/console/text_line.hpp
@@ -0,0 +1,43 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2024 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef YLIKUUTIO_CONSOLE_TEXT_LINE_HPP_INCLUDED
+#define YLIKUUTIO_CONSOLE_TEXT_LINE_HPP_INCLUDED
+
+#include "code/ylikuutio/data/codepoint.hpp"
+
+// Include standard headers
+#include <vector> // std::vector
+
+namespace yli::console
+{
+    class TextLine
+    {
+        public:
+            TextLine(const std::vector<yli::data::Codepoint>& text_line)
+                : line { text_line }
+            {
+            }
+
+            const std::vector<yli::data::Codepoint>& data() const;
+
+        private:
+            std::vector<yli::data::Codepoint> line;
+    };
+}
+
+#endif
---
+++ code/ylikuutio/tests/test_text_line.cpp
@@ -0,0 +1,30 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2024 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "gtest/gtest.h"
+#include "code/ylikuutio/console/text_line.hpp"
+#include "code/ylikuutio/data/codepoint.hpp"
+
+// Include standard headers
+#include <vector> // std::vector
+
+TEST(text_line_must_be_initialized_appropriately, text_line)
+{
+    const std::vector<yli::data::Codepoint> char_container { 'a', 'b', 'c' };
+    yli::console::TextLine text_line(char_container);
+    ASSERT_EQ(text_line.data(), char_container);
+}
