14afeaac5cdc3e238f9cc86f7fe26d865cc7f4f2
Antti Nuortimo <antti.nuortimo@gmail.com>
2025-10-05 18:27:58 +0300

    New struct: `PrintConsoleStruct`.
    `PrintConsoleStruct` will be used for the upcoming
    `Font2d::print_console` member function.

---
+++ code/ylikuutio/ontology/print_console_struct.hpp
@@ -0,0 +1,51 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2025 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef YLIKUUTIO_ONTOLOGY_PRINT_CONSOLE_STRUCT_HPP_INCLUDED
+#define YLIKUUTIO_ONTOLOGY_PRINT_CONSOLE_STRUCT_HPP_INCLUDED
+
+#include "position_struct.hpp"
+#include "code/ylikuutio/console/text_line.hpp"
+#include "code/ylikuutio/console/text_input.hpp"
+
+// Include standard headers
+#include <cstddef>  // std::size_t
+#include <optional> // std::optional
+#include <span>     // std::span
+#include <string>   // std::string
+
+namespace yli::ontology
+{
+    struct PrintConsoleStruct
+    {
+        PrintConsoleStruct(
+                const std::optional<std::span<const yli::console::TextLine>> buffer_text,
+                const yli::console::TextInput* const text_input)
+            : buffer_text { buffer_text },
+            text_input { text_input }
+        {
+        }
+
+        PositionStruct position;
+        std::size_t font_size { 0 };
+        const std::optional<std::span<const yli::console::TextLine>> buffer_text;
+        const yli::console::TextInput* const text_input;
+        std::string prompt;
+    };
+}
+
+#endif
