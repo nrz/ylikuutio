28632cb214356ba239ae7339c525a8f6a9a25ccf
Antti Nuortimo <antti.nuortimo@gmail.com>
2026-02-28 23:39:02 +0200

    Bugfix: add missing file.

---
+++ code/ylikuutio/ontology/console_lisp_function_struct.hpp
@@ -0,0 +1,42 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2026 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef YLIKUUTIO_ONTOLOGY_CONSOLE_LISP_FUNCTION_STRUCT_HPP_INCLUDED
+#define YLIKUUTIO_ONTOLOGY_CONSOLE_LISP_FUNCTION_STRUCT_HPP_INCLUDED
+
+#include "lisp_function_struct.hpp"
+#include "request.hpp"
+
+// Include standard headers
+#include <utility> // std::move
+
+namespace yli::ontology
+{
+    class Console;
+
+    struct ConsoleLispFunctionStruct : public LispFunctionStruct
+    {
+        explicit ConsoleLispFunctionStruct(Request<Console>&& console_parent)
+            : console_parent { std::move(console_parent) }
+        {
+        }
+
+        Request<Console> console_parent {};
+    };
+}
+
+#endif
