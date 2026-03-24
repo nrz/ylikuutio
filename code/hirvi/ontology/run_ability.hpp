6674ffed55c8ad1002f8f8557776011b16e79f45
Antti Nuortimo <antti.nuortimo@gmail.com>
2026-03-24 23:35:25 +0200

    `hirvi::RunAbility` class, work in progress

---
+++ code/hirvi/ontology/run_ability.hpp
@@ -0,0 +1,56 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2026 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef HIRVI_RUN_ABILITY_HPP_INCLUDED
+#define HIRVI_RUN_ABILITY_HPP_INCLUDED
+
+#include "code/ylikuutio/ontology/ability.hpp"
+
+// Include standard headers
+#include <cstddef> // std::size_t
+
+namespace yli::core
+{
+    class Application;
+}
+
+namespace yli::ontology
+{
+    class GenericParentModule;
+    class Universe;
+    struct AbilityStruct;
+}
+
+namespace hirvi
+{
+    class RunAbility final : public yli::ontology::Ability
+    {
+        public:
+            explicit RunAbility(
+                    yli::core::Application& application,
+                    yli::ontology::Universe& universe,
+                    const yli::ontology::AbilityStruct& ability_struct,
+                    yli::ontology::GenericParentModule* const parent_module);
+
+            RunAbility(const RunAbility&) = delete;            // Delete copy constructor.
+            RunAbility &operator=(const RunAbility&) = delete; // Delete copy assignment.
+
+            virtual ~RunAbility() = default;
+    };
+}
+
+#endif
