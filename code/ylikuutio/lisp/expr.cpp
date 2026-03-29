985fcb73a6a40d7b351b097eadef49b61dde6d8d
Antti Nuortimo <antti.nuortimo@gmail.com>
2026-03-29 16:55:41 +0300

    Bugfix: add missing `#include` line

--- code/ylikuutio/lisp/expr.cpp
+++ code/ylikuutio/lisp/expr.cpp
@@ -16,6 +16,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "expr.hpp"
+#include "expr_type.hpp"
 
 // Include standard headers
 #include <stdexcept> // std::runtime_error
