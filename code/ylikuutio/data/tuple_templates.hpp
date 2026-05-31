43db35fc13c2577ff25d9401ef2cac0c885287d2
Antti Nuortimo <antti.nuortimo@gmail.com>
2026-05-31 21:32:45 +0300

    Tuple templates: reformat code

--- code/ylikuutio/data/tuple_templates.hpp
+++ code/ylikuutio/data/tuple_templates.hpp
@@ -26,51 +26,53 @@
 namespace yli::data
 {
     template<typename T1, typename... Types>
-        auto first(std::tuple<T1, Types...>& my_tuple)
-        {
-            // Inspired by https://stackoverflow.com/questions/10626856/how-to-split-a-tuple
-            return std::get<0>(my_tuple);
-        }
+    auto first(std::tuple<T1, Types...>& my_tuple)
+    {
+        // Inspired by https://stackoverflow.com/questions/10626856/how-to-split-a-tuple
+        return std::get<0>(my_tuple);
+    }
 
     template<std::size_t... Numbers, typename... Types>
-        auto rest_impl(std::index_sequence<Numbers...>, std::tuple<Types...>& my_tuple)
-        {
-            // Inspired by https://stackoverflow.com/questions/10626856/how-to-split-a-tuple
-            return std::make_tuple(std::get<Numbers + 1u>(my_tuple)...);
-        }
+    auto rest_impl(std::index_sequence<Numbers...>, std::tuple<Types...>& my_tuple)
+    {
+        // Inspired by https://stackoverflow.com/questions/10626856/how-to-split-a-tuple
+        return std::make_tuple(std::get<Numbers + 1u>(my_tuple)...);
+    }
 
     template<typename... Types>
-        auto rest(std::tuple<Types...>& my_tuple)
-        {
-            // Inspired by https://stackoverflow.com/questions/10626856/how-to-split-a-tuple
-            return rest_impl(std::make_index_sequence<sizeof...(Types) - 1u>(), my_tuple);
-        }
+    auto rest(std::tuple<Types...>& my_tuple)
+    {
+        // Inspired by https://stackoverflow.com/questions/10626856/how-to-split-a-tuple
+        return rest_impl(std::make_index_sequence<sizeof...(Types) - 1u>(), my_tuple);
+    }
 
     template<typename TupleType, typename FunctionType, std::size_t... Is>
-        auto visit_tuple_helper(TupleType&& tuple, FunctionType&& func, std::index_sequence<Is...>)
-        {
-            return std::tuple(func(Is, std::get<Is>(std::forward<TupleType>(tuple)))...);
-        }
+    auto visit_tuple_helper(TupleType&& tuple, FunctionType&& func, std::index_sequence<Is...>)
+    {
+        return std::tuple(func(Is, std::get<Is>(std::forward<TupleType>(tuple)))...);
+    }
 
     template<typename TupleType, typename FunctionType>
-        auto visit_tuple(TupleType&& tuple, FunctionType&& func)
-        {
-            using index_seq = std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<TupleType>>>;
-            return visit_tuple_helper<TupleType, FunctionType>(std::forward<TupleType>(tuple), std::forward<FunctionType>(func), index_seq());
-        }
+    auto visit_tuple(TupleType&& tuple, FunctionType&& func)
+    {
+        using index_seq = std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<TupleType>>>;
+        return visit_tuple_helper<TupleType, FunctionType>(std::forward<TupleType>(tuple),
+                                                           std::forward<FunctionType>(func), index_seq());
+    }
 
     template<typename Type, typename TupleType, typename FunctionType, std::size_t... Is>
-        auto visit_tuple_with_type_helper(TupleType&& tuple, FunctionType&& func, std::index_sequence<Is...>)
-        {
-            return std::tuple(func<Type>(Is, std::get<Is>(std::forward<TupleType>(tuple)))...);
-        }
+    auto visit_tuple_with_type_helper(TupleType&& tuple, FunctionType&& func, std::index_sequence<Is...>)
+    {
+        return std::tuple(func<Type>(Is, std::get<Is>(std::forward<TupleType>(tuple)))...);
+    }
 
     template<typename Type, typename TupleType, typename FunctionType>
-        auto visit_tuple_with_type(TupleType&& tuple, FunctionType&& func)
-        {
-            using index_seq = std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<TupleType>>>;
-            return visit_tuple_with_type_helper<Type, TupleType, FunctionType>(std::forward<TupleType>(tuple), std::forward<FunctionType>(func), index_seq());
-        }
+    auto visit_tuple_with_type(TupleType&& tuple, FunctionType&& func)
+    {
+        using index_seq = std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<TupleType>>>;
+        return visit_tuple_with_type_helper<Type, TupleType, FunctionType>(
+            std::forward<TupleType>(tuple), std::forward<FunctionType>(func), index_seq());
+    }
 }
 
 #endif
