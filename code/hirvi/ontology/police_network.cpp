a902f42cb1bb133a8a050d6419e3a83dce89dba8
Antti Nuortimo <antti.nuortimo@gmail.com>
2026-06-03 22:12:12 +0300

    Hirvi: `PoliceNetwork` placeholder class

---
+++ code/hirvi/ontology/police_network.cpp
@@ -0,0 +1,49 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2026 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "police_network.hpp"
+#include "code/ylikuutio/ontology/scene.hpp"
+
+// Include standard headers
+#include <string> // std::string
+
+namespace yli::ontology
+{
+    class Registry;
+}
+
+namespace hirvi
+{
+    using namespace yli::ontology;
+
+    PoliceNetwork::PoliceNetwork(
+        Scene& master_scene,
+        Registry* const registry,
+        const std::string& name)
+        : GenericMasterModule(master_scene, registry, name)
+    { }
+
+    void PoliceNetwork::store_data()
+    {
+        // TODO: implement!
+    }
+
+    const SpatialData& PoliceNetwork::get_spatial_data() const
+    {
+        return this->spatial_data;
+    }
+}
---
+++ code/hirvi/ontology/police_network.hpp
@@ -0,0 +1,54 @@
+// Ylikuutio - A 3D game and simulation engine.
+//
+// Copyright (C) 2015-2026 Antti Nuortimo.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef HIRVI_ONTOLOGY_POLICE_NETWORK_HPP_INCLUDED
+#define HIRVI_ONTOLOGY_POLICE_NETWORK_HPP_INCLUDED
+
+#include "code/ylikuutio/ontology/generic_master_module.hpp"
+#include "spatial_data.hpp"
+
+// Include standard headers
+#include <string> // std::string
+
+namespace yli::ontology
+{
+    class Scene;
+    class Registry;
+}
+
+namespace hirvi
+{
+    using namespace yli::ontology;
+
+    class PoliceNetwork final : GenericMasterModule
+    {
+    public:
+        PoliceNetwork(
+            Scene& master_scene,
+            Registry* registry,
+            const std::string& name);
+
+        void store_data();
+
+        [[nodiscard]] const SpatialData& get_spatial_data() const;
+
+    private:
+        SpatialData spatial_data;
+    };
+}
+
+#endif
